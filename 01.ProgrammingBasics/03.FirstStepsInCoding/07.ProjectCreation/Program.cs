192647038a9ead966c707f257892229356450e0f
Georgi <bulbrainn@gmail.com>
2022-05-24 23:56:14 +0300

    Added Project creation

---
+++ 01.ProgrammingBasics/03.FirstStepsInCoding/07.ProjectCreation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _07.ProjectCreation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int projects = int.Parse(Console.ReadLine());
+            int totalTime = projects * 3;
+            Console.WriteLine($"The architect {name} " +
+                $"will need {totalTime} hours to " +
+                $"complete {projects} project/s.");
+        }
+    }
+}
