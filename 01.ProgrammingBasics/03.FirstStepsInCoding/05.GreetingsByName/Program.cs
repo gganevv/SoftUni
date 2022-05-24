a4dd3d272e338686bd7d57af22b60632929fe746
Georgi <bulbrainn@gmail.com>
2022-05-24 23:54:58 +0300

    Added Greetings by name

---
+++ 01.ProgrammingBasics/03.FirstStepsInCoding/05.GreetingsByName/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _05.GreetingsByName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
