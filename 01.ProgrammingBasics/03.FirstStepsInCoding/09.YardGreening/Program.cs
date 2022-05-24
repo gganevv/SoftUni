043fd71cf71f45a68eb736d86a867f2b5499e0a0
Georgi <bulbrainn@gmail.com>
2022-05-24 23:57:03 +0300

    Added yard greening

---
+++ 01.ProgrammingBasics/03.FirstStepsInCoding/09.YardGreening/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _09.YardGreening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double greeningMs = double.Parse(Console.ReadLine());
+            double price = greeningMs * 7.61;
+            double discount = price * 0.18;
+            double priceAfterDiscount = price - discount;
+            Console.WriteLine($"The final price is: {priceAfterDiscount} lv.");
+            Console.WriteLine($"The discount is: {discount} lv.");
+        }
+    }
+}
