3e74146d6110b9e02577f4d6c116eb4d4ce0cb16
Georgi <bulbrainn@gmail.com>
2022-05-24 23:56:34 +0300

    Added Pet Shop

---
+++ 01.ProgrammingBasics/03.FirstStepsInCoding/08.PetShop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _08.PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogFood = int.Parse(Console.ReadLine());
+            int catFood = int.Parse(Console.ReadLine());
+
+            double finalPrice = (dogFood * 2.5) + (catFood * 4);
+            Console.WriteLine($"{finalPrice} lv.");
+        }
+    }
+}
