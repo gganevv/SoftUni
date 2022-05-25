b40adc00dafc5aef288acfda49851c8913d19fa4
Georgi <bulbrainn@gmail.com>
2022-05-25 22:02:24 +0300

    Added Fish Tank

---
+++ 01.ProgrammingBasics/04.FirstStepsInCodingExercises/09.FishTank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _09.FishTank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double sandPersentage = double.Parse(Console.ReadLine());
+
+            double tankArea = length * width * height;
+            double litters = tankArea / 1000;
+            double littersFilled = litters * (1 - sandPersentage / 100);
+            Console.WriteLine(littersFilled);
+        }
+    }
+}
