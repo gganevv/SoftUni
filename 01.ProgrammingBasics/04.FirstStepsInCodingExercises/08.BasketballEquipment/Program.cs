8fb75c3c8b3cb5f2311a532faf766a6a2d5cc1f4
Georgi <bulbrainn@gmail.com>
2022-05-25 22:02:02 +0300

    Added Basketball Equipment

---
+++ 01.ProgrammingBasics/04.FirstStepsInCodingExercises/08.BasketballEquipment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _08.BasketballEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yearFee = int.Parse(Console.ReadLine());
+            double shoesPrice = yearFee * 0.6;
+            double clothesPrice = shoesPrice * 0.8;
+            double ball = clothesPrice * 0.25;
+            double accesoaries = ball * 0.2;
+            double totalPrice = yearFee + shoesPrice + clothesPrice + ball + accesoaries;
+            Console.WriteLine(totalPrice);
+        }
+    }
+}
