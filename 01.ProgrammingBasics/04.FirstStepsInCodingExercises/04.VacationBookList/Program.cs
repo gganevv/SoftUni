717ccd930cf9cd9d1de6456a5487a46ca886e613
Georgi <bulbrainn@gmail.com>
2022-05-25 22:00:22 +0300

    Added Vacation Book List

---
+++ 01.ProgrammingBasics/04.FirstStepsInCodingExercises/04.VacationBookList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _04.VacationBookList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pages = int.Parse(Console.ReadLine());
+            int pagesPerHour = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+            int totalHours = pages / pagesPerHour;
+            int hoursPerDay = totalHours / days;
+            Console.WriteLine(hoursPerDay);
+        }
+    }
+}
