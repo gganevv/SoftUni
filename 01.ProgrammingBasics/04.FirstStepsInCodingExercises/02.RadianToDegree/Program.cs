b73511a4c5f58263fd5c5d4fb6c49452bab3b7d0
Georgi <bulbrainn@gmail.com>
2022-05-25 21:58:30 +0300

    Added Radian to Degree

---
+++ 01.ProgrammingBasics/04.FirstStepsInCodingExercises/02.RadianToDegree/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _02.RadianToDegree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radians = double.Parse(Console.ReadLine());
+            double degree = radians * 180 / Math.PI;
+            Console.WriteLine(degree);
+        }
+    }
+}
