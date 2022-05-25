c9a8e32cc9921c106f7ecfd2b8f0bf820b67bd00
Georgi <bulbrainn@gmail.com>
2022-05-25 22:01:32 +0300

    Added Food Delivery

---
+++ 01.ProgrammingBasics/04.FirstStepsInCodingExercises/07.FoodDelivery/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _07.FoodDelivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chickenMenus = int.Parse(Console.ReadLine());
+            int fishMenus = int.Parse(Console.ReadLine());
+            int veggMenus = int.Parse(Console.ReadLine());
+            double totalPrice = (chickenMenus * 10.35 + fishMenus * 12.4 + veggMenus * 8.15) * 1.2 + 2.5;
+            Console.WriteLine(totalPrice);
+        }
+    }
+}
