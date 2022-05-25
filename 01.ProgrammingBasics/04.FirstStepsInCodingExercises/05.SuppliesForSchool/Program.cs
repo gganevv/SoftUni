54c74b820cc8a792138fc6daba82714012253e76
Georgi <bulbrainn@gmail.com>
2022-05-25 22:00:50 +0300

    Added Supplies For School

---
+++ 01.ProgrammingBasics/04.FirstStepsInCodingExercises/05.SuppliesForSchool/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _05.SuppliesForSchool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pens = int.Parse(Console.ReadLine());
+            int markers = int.Parse(Console.ReadLine());
+            int cleaningDetergent = int.Parse(Console.ReadLine());
+            int discount = int.Parse(Console.ReadLine());
+            double penPrice = pens * 5.8;
+            double markerPrice = markers * 7.2;
+            double detergentPrice = cleaningDetergent * 1.2;
+            double totalPrice = penPrice + markerPrice + detergentPrice;
+            double priceAfterDiscount = totalPrice - (totalPrice * discount / 100);
+            Console.WriteLine(priceAfterDiscount);
+        }
+    }
+}
