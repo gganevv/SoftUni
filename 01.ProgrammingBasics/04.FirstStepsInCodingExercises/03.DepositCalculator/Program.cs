0afb548f8283ab0e40655ee2c8aa75ce91231da7
Georgi <bulbrainn@gmail.com>
2022-05-25 21:58:59 +0300

    Added Deposit Calculator

---
+++ 01.ProgrammingBasics/04.FirstStepsInCodingExercises/03.DepositCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _03.DepositCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double deposit = double.Parse(Console.ReadLine());
+            int mounts = int.Parse(Console.ReadLine());
+            double interest = double.Parse(Console.ReadLine());
+            double sum = deposit + mounts * ((deposit * interest / 100) / 12);
+            Console.WriteLine(sum);
+        }
+    }
+}
