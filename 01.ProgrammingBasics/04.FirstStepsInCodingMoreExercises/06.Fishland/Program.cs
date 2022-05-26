b66423cabbd5feb9c2d82a69b01e4783a3ed6bfb
Georgi <bulbrainn@gmail.com>
2022-05-26 19:53:20 +0300

    Added FishLand

---
+++ 01.ProgrammingBasics/04.FirstStepsInCodingMoreExercises/06.Fishland/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _06.Fishland
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double mackerelPrice = double.Parse(Console.ReadLine());
+            double spratPrice = double.Parse(Console.ReadLine());
+            double bonitoKgs = double.Parse(Console.ReadLine());
+            double horseMackerelKgs = double.Parse(Console.ReadLine());
+            int musselKgs = int.Parse(Console.ReadLine());
+            double bonitoPrice = bonitoKgs * mackerelPrice * 1.6;
+            double horseMackerelPrice = horseMackerelKgs * spratPrice * 1.8;
+            double musselPrice = musselKgs * 7.5;
+            double total = bonitoPrice + horseMackerelPrice + musselPrice;
+            Console.WriteLine($"{total:f2}");
+        }
+    }
+}
