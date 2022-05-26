54905ce2161033d731d0b91039f4c5cf4c0a2752
Georgi <bulbrainn@gmail.com>
2022-05-26 19:52:32 +0300

    Added Vegetable Market

---
+++ 01.ProgrammingBasics/04.FirstStepsInCodingMoreExercises/04.VegetableMarket/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04.VegetableMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double vegetablesPricePerKg = double.Parse(Console.ReadLine());
+            double fruitsPricePerKg = double.Parse(Console.ReadLine());
+            int vegetablesKgs = int.Parse(Console.ReadLine());
+            int fruitsKgs = int.Parse(Console.ReadLine());
+            double vegetablesPrice = vegetablesPricePerKg * vegetablesKgs;
+            double fruitsPrice = fruitsPricePerKg * fruitsKgs;
+            double totalPrice = vegetablesPrice + fruitsPrice;
+            double totalPriceEur = totalPrice / 1.94;
+            Console.WriteLine($"{totalPriceEur:f2}");
+
+        }
+    }
+}
