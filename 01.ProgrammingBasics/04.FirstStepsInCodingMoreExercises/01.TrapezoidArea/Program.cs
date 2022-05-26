999ddd0aaedc73c1ee4a008796f84deed12fe75d
Georgi <bulbrainn@gmail.com>
2022-05-26 19:49:40 +0300

    Added Trapezoid Area

---
+++ 01.ProgrammingBasics/04.FirstStepsInCodingMoreExercises/01.TrapezoidArea/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01.TrapezoidArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double b1 = double.Parse(Console.ReadLine());
+            double b2 = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            double result = (b1 + b2) * h / 2;
+            Console.WriteLine($"{result:f2}");
+        }
+    }
+}
