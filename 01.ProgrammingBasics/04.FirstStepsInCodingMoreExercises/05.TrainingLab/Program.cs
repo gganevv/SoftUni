3bcc33b8ee58a8ac6d9b9122e4ac87bcac9e0aa1
Georgi <bulbrainn@gmail.com>
2022-05-26 19:52:57 +0300

    Added training Lab

---
+++ 01.ProgrammingBasics/04.FirstStepsInCodingMoreExercises/05.TrainingLab/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _05.TrainingLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double w = double.Parse(Console.ReadLine()) * 100;
+            double h = double.Parse(Console.ReadLine()) * 100 - 100;
+            int rows = (int)(h / 70);
+            int cows = (int)(w / 120);
+            int seats = rows * cows - 3;
+            Console.WriteLine(seats);
+        }
+    }
+}
