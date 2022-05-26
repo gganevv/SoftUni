dcb275e209db787a87a16c3cd2950eb7b5e10d5c
Georgi <bulbrainn@gmail.com>
2022-05-26 19:52:02 +0300

    Added C to F

---
+++ 01.ProgrammingBasics/04.FirstStepsInCodingMoreExercises/03.CTOF/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _03.CTOF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double c = double.Parse(Console.ReadLine());
+            double f = c * 1.8 + 32;
+            Console.WriteLine($"{f:f2}");
+        }
+    }
+}
