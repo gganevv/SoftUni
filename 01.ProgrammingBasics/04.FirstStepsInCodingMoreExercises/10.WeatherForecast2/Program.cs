514ab5359d88f86c2a2804e41db589f50a67bc95
Georgi <bulbrainn@gmail.com>
2022-05-26 19:55:04 +0300

    Added Weather Forecast 2

---
+++ 01.ProgrammingBasics/04.FirstStepsInCodingMoreExercises/10.WeatherForecast2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _10.WeatherForecast2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double degrees = double.Parse(Console.ReadLine());
+            if (degrees >= 5 && degrees < 12)
+            {
+                Console.WriteLine("Cold");
+            }
+            else if (degrees >= 12 && degrees < 15)
+            {
+                Console.WriteLine("Cool");
+            }
+            else if (degrees >= 15 && degrees <= 20)
+            {
+                Console.WriteLine("Mild");
+            }
+            else if (degrees >= 20.1 && degrees <= 25.9)
+            {
+                Console.WriteLine("Warm");
+            }
+            else if (degrees >= 26 && degrees <= 35)
+            {
+                Console.WriteLine("Hot");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
