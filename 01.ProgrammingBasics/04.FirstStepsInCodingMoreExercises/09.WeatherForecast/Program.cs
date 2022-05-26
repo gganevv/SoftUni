452cd66daf30df9dc3418682e99430d3d3caf1cb
Georgi <bulbrainn@gmail.com>
2022-05-26 19:54:38 +0300

    Added Weather Forecast

---
+++ 01.ProgrammingBasics/04.FirstStepsInCodingMoreExercises/09.WeatherForecast/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _09.WeatherForecast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string weather = Console.ReadLine();
+            if (weather == "sunny")
+            {
+                Console.WriteLine("It's warm outside!");
+            }
+            else
+            {
+                Console.WriteLine("It's cold outside!");
+            }
+        }
+    }
+}
