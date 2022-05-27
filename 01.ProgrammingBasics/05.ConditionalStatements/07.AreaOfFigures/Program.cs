484d9d67d205ddf2b429cf758ce5d1a892c9dda3
Georgi <bulbrainn@gmail.com>
2022-05-27 19:34:05 +0300

    Added Area Of Figures

---
+++ 01.ProgrammingBasics/05.ConditionalStatements/07.AreaOfFigures/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _07.AreaOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figureType = Console.ReadLine();
+            double area = 0;
+            double a = double.Parse(Console.ReadLine());
+
+            if (figureType == "square")
+            {
+                area = a * a;
+            }
+            else if (figureType == "rectangle")
+            {
+                double b = double.Parse(Console.ReadLine());
+                area = a * b;
+            }
+            else if (figureType == "circle")
+            {
+                area = Math.PI * a * a;
+            }
+            else if (figureType == "triangle")
+            {
+                double b = double.Parse(Console.ReadLine());
+                area = a * b / 2;
+            }
+
+            Console.WriteLine($"{area:f3}");
+        }
+    }
+}
