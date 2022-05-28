5c93704f8663e7db62f0900d9e0a6cfa3ed2d28f
Georgi <bulbrainn@gmail.com>
2022-05-28 13:29:09 +0300

    Added Bonus Score

---
+++ 01.ProgrammingBasics/06.ConditionalStatementsExercises/02.BonusScore/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _02.BonusScore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            double bonusPoints = 0;
+            if (num <= 100)
+            {
+                bonusPoints += 5;
+            }
+            else if (num > 100 && num <= 1000)
+            {
+                bonusPoints = num * 0.2;
+            }
+            else
+            {
+                bonusPoints = num * 0.1;
+            }
+
+            if (num % 2 == 0)
+            {
+                bonusPoints += 1;
+            }
+
+            if (num % 10 == 5)
+            {
+                bonusPoints += 2;
+            }
+
+            Console.WriteLine(bonusPoints);
+            Console.WriteLine(bonusPoints + num);
+        }
+    }
+}
