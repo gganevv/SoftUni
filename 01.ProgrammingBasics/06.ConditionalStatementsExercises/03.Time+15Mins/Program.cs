7ea0f7d228275be1504fa28a0326cf886749aa0d
Georgi <bulbrainn@gmail.com>
2022-05-28 13:29:31 +0300

    Added Time + 15 mins

---
+++ 01.ProgrammingBasics/06.ConditionalStatementsExercises/03.Time+15Mins/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _03.Time_15Mins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int totalTimeInMins = minutes + hours * 60;
+            int timeResult = totalTimeInMins + 15;
+
+            int resultHours = timeResult / 60;
+            int resultMins = timeResult % 60;
+            if (resultHours > 23)
+            {
+                resultHours = 0;
+            }
+            if (resultMins < 10)
+            {
+                Console.WriteLine($"{resultHours}:0{resultMins}");
+            }
+            else
+            {
+                Console.WriteLine($"{resultHours}:{resultMins}");
+            }
+        }
+    }
+}
