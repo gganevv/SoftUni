b7dbd44e30a0c25426316d410310716fdcd9b6c1
Georgi <bulbrainn@gmail.com>
2022-05-28 13:28:20 +0300

    Added Sum Seconds

---
+++ 01.ProgrammingBasics/06.ConditionalStatementsExercises/01.SumSeconds/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01.SumSeconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstResult = int.Parse(Console.ReadLine());
+            int secondResult = int.Parse(Console.ReadLine());
+            int thirdResult = int.Parse(Console.ReadLine());
+            int totalResult = firstResult + secondResult + thirdResult;
+            int mins = totalResult / 60;
+            int sec = totalResult % 60;
+            if (sec < 10)
+            {
+                Console.WriteLine($"{mins}:0{sec}");
+            }
+            else
+            {
+                Console.WriteLine($"{mins}:{sec}");
+            }
+        }
+    }
+}
