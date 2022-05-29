ef2832f06bea3837ad0370358dc1b3c1cc4ad2e6
Georgi <bulbrainn@gmail.com>
2022-05-30 00:18:10 +0300

    Added Flower Shop

---
+++ 01.ProgrammingBasics/06.ConditionalStatementsMoreExercises/07.FlowerShop/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _07.FlowerShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double magnoliaPrice = 3.25;
+            double hyacinthPrice = 4;
+            double rosePrice = 3.5;
+            double cactusPrice = 8;
+
+            int magnolias = int.Parse(Console.ReadLine());
+            int hyacinths = int.Parse(Console.ReadLine());
+            int roses = int.Parse(Console.ReadLine());
+            int cactus = int.Parse(Console.ReadLine());
+            double giftPrice = double.Parse(Console.ReadLine());
+            double totalProfit = magnolias * magnoliaPrice + hyacinths * hyacinthPrice + roses * rosePrice + cactus * cactusPrice;
+            double profitAfterTax = totalProfit * 0.95;
+            double diff = profitAfterTax - giftPrice;
+            if (profitAfterTax >= giftPrice)
+            {
+                Console.WriteLine($"She is left with {Math.Floor(diff)} leva.");
+            }
+            else
+            {
+                diff = Math.Abs(diff);
+                Console.WriteLine($"She will have to borrow {Math.Ceiling(diff)} leva.");
+            }
+        }
+    }
+}
