4f168014c66f75d5af923bd2fe9e149ff7953d50
Georgi <bulbrainn@gmail.com>
2022-05-30 00:18:37 +0300

    Added Fuel Tank

---
+++ 01.ProgrammingBasics/06.ConditionalStatementsMoreExercises/08.FuelTank/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _08.FuelTank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fuelType = Console.ReadLine();
+            double fuelInTank = double.Parse(Console.ReadLine());
+            switch (fuelType)
+            {
+                case "Diesel":
+                case "Gasoline":
+                case "Gas":
+                    if (fuelInTank >= 25)
+                    {
+                        Console.WriteLine($"You have enough {fuelType.ToLower()}.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Fill your tank with {fuelType.ToLower()}!");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid fuel!");
+                    break;
+            }
+        }
+    }
+}
