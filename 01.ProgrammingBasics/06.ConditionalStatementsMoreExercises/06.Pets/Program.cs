3587c997d154ef023cec755f8acb2195705f894b
Georgi <bulbrainn@gmail.com>
2022-05-30 00:17:46 +0300

    Added Pets

---
+++ 01.ProgrammingBasics/06.ConditionalStatementsMoreExercises/06.Pets/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _06.Pets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int food = int.Parse(Console.ReadLine());
+            double dailyDogFood = double.Parse(Console.ReadLine());
+            double dailyCatFood = double.Parse(Console.ReadLine());
+            double dailyTurtleFood = double.Parse(Console.ReadLine()) / 1000;
+
+            double neededFood = (dailyDogFood + dailyCatFood + dailyTurtleFood) * days;
+            double diff = food - neededFood;
+
+            if (diff >= 0)
+            {
+                Console.WriteLine($"{Math.Floor(diff)} kilos of food left.");
+            }
+            else
+            {
+                diff = Math.Abs(diff);
+                Console.WriteLine($"{Math.Ceiling(diff)} more kilos of food are needed.");
+            }
+        }
+    }
+}
