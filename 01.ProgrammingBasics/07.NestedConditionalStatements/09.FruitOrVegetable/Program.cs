12e4c71d048162c5cf1149d7ea0cf4f9a2f303f2
Georgi <bulbrainn@gmail.com>
2022-06-01 19:03:43 +0300

    Added Fruit Or Vegetable

---
+++ 01.ProgrammingBasics/07.NestedConditionalStatements/09.FruitOrVegetable/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _09.FruitOrVegetable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            string productType;
+            switch (product)
+            {
+                case "banana":
+                case "apple":
+                case "kiwi":
+                case "cherry":
+                case "lemon":
+                case "grapes":
+                    productType = "fruit";
+                    break;
+                case "tomato":
+                case "cucumber":
+                case "pepper":
+                case "carrot":
+                    productType = "vegetable";
+                    break;
+                default:
+                    productType = "unknown";
+                    break;
+            }
+            Console.WriteLine(productType);
+        }
+    }
+}
