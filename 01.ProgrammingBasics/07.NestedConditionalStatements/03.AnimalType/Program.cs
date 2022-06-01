dd3d9896658ce710c9120e053f9a7b14972ba810
Georgi <bulbrainn@gmail.com>
2022-06-01 19:00:40 +0300

    Added Animal Type

---
+++ 01.ProgrammingBasics/07.NestedConditionalStatements/03.AnimalType/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _03.AnimalType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string animalName = Console.ReadLine();
+            string animalType;
+            switch (animalName)
+            {
+                case "dog":
+                    animalType = "mammal";
+                    break;
+                case "crocodile":
+                case "tortoise":
+                case "snake":
+                    animalType = "reptile";
+                    break;
+                default:
+                    animalType = "unknown";
+                    break;
+            }
+            Console.WriteLine(animalType);
+        }
+    }
+}
