af1ea7d2be80c35744e82944c6896722f2e0d7b3
Georgi <bulbrainn@gmail.com>
2022-06-01 18:59:25 +0300

    Added Day of Week

---
+++ 01.ProgrammingBasics/07.NestedConditionalStatements/01.DayOfWeek/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _01.DayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = int.Parse(Console.ReadLine());
+            string dayName;
+            switch (day)
+            {
+                case 1:
+                    dayName = "Monday";
+                    break;
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+                case 4:
+                    dayName = "Thursday";
+                    break;
+                case 5:
+                    dayName = "Friday";
+                    break;
+                case 6:
+                    dayName = "Saturday";
+                    break;
+                case 7:
+                    dayName = "Sunday";
+                    break;
+                default:
+                    dayName = "Error";
+                    break;
+            }
+            Console.WriteLine(dayName);
+        }
+    }
+}
