63c898b27aa3de23050bb2eba12f22d3c985d0b5
Georgi <bulbrainn@gmail.com>
2022-06-01 19:02:47 +0300

    Added Working Hours

---
+++ 01.ProgrammingBasics/07.NestedConditionalStatements/07.WorkigHours/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _07.WorkigHours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            string dayOfWeek = Console.ReadLine();
+            if (hour < 10 || hour > 18 || dayOfWeek == "Sunday")
+            {
+                Console.WriteLine("closed");
+            }
+            else
+            {
+                Console.WriteLine("open");
+            }
+        }
+    }
+}
