da54d0574cb9af68ea00774850a9b0ba3d6cb303
Georgi <bulbrainn@gmail.com>
2022-06-01 19:04:35 +0300

    Added Fruit Shop

---
+++ 01.ProgrammingBasics/07.NestedConditionalStatements/11.FruitShop/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace _11.FruitShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fruit = Console.ReadLine();
+            string dayOfWeek = Console.ReadLine();
+            double quantity = double.Parse(Console.ReadLine());
+            double price = 0;
+
+            switch (dayOfWeek)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Wednesday":
+                case "Thursday":
+                case "Friday":
+                    switch (fruit)
+                    {
+                        case "banana":
+                            price = 2.5;
+                            break;
+                        case "apple":
+                            price = 1.2;
+                            break;
+                        case "orange":
+                            price = 0.85;
+                            break;
+                        case "grapefruit":
+                            price = 1.45;
+                            break;
+                        case "kiwi":
+                            price = 2.7;
+                            break;
+                        case "pineapple":
+                            price = 5.5;
+                            break;
+                        case "grapes":
+                            price = 3.85;
+                            break;
+                        default:
+                            Console.WriteLine("error");
+                            break;
+                    }
+                    break;
+                case "Saturday":
+                case "Sunday":
+                    switch (fruit)
+                    {
+                        case "banana":
+                            price = 2.7;
+                            break;
+                        case "apple":
+                            price = 1.25;
+                            break;
+                        case "orange":
+                            price = 0.9;
+                            break;
+                        case "grapefruit":
+                            price = 1.6;
+                            break;
+                        case "kiwi":
+                            price = 3;
+                            break;
+                        case "pineapple":
+                            price = 5.6;
+                            break;
+                        case "grapes":
+                            price = 4.2;
+                            break;
+                        default:
+                            Console.WriteLine("error");
+                            break;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("error");
+                    break;
+            }
+            double totalPrice = price * quantity;
+            if (totalPrice > 0)
+            {
+                Console.WriteLine($"{totalPrice:f2}");
+            }
+
+        }
+    }
+}
