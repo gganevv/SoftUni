79004d4fa46f1a482a1a0c9f128f532840d4a67e
Georgi <bulbrainn@gmail.com>
2022-06-01 19:01:20 +0300

    Added Personal Titles

---
+++ 01.ProgrammingBasics/07.NestedConditionalStatements/04.PersonalTitles/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _04.PersonalTitles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            char gender = char.Parse(Console.ReadLine());
+            string title;
+            if (gender == 'm')
+            {
+                if (age >= 16)
+                {
+                    title = "Mr.";
+                }
+                else
+                {
+                    title = "Master";
+                }
+            }
+            else
+            {
+                if (age >= 16)
+                {
+                    title = "Ms.";
+                }
+                else
+                {
+                    title = "Miss";
+                }
+            }
+            Console.WriteLine(title);
+        }
+    }
+}
