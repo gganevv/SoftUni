45d7a935cd084557d0e6f09e17e54f3ee6cb050b
Georgi <bulbrainn@gmail.com>
2022-06-01 19:04:09 +0300

    Added Invalid Number

---
+++ 01.ProgrammingBasics/07.NestedConditionalStatements/10.InvalidNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _10.InvalidNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n > 200 || n < 100 && n != 0)
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
