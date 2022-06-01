1b2549e9eb79819d52dbfd613b2b165785aa62d7
Georgi <bulbrainn@gmail.com>
2022-06-01 19:01:47 +0300

    Added Small Shop

---
+++ 01.ProgrammingBasics/07.NestedConditionalStatements/05.SmallShop/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace _05.SmallShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string productName = Console.ReadLine();
+            string city = Console.ReadLine();
+            double quantity = double.Parse(Console.ReadLine());
+            double price = 0;
+
+            if (city == "Sofia")
+            {
+                switch (productName)
+                {
+                    case "coffee":
+                        price = 0.5;
+                        break;
+                    case "water":
+                        price = 0.8;
+                        break;
+                    case "beer":
+                        price = 1.2;
+                        break;
+                    case "sweets":
+                        price = 1.45;
+                        break;
+                    case "peanuts":
+                        price = 1.6;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if(city == "Plovdiv")
+            {
+                switch (productName)
+                {
+                    case "coffee":
+                        price = 0.4;
+                        break;
+                    case "water":
+                        price = 0.7;
+                        break;
+                    case "beer":
+                        price = 1.15;
+                        break;
+                    case "sweets":
+                        price = 1.30;
+                        break;
+                    case "peanuts":
+                        price = 1.5;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if(city == "Varna")
+            {
+                switch (productName)
+                {
+                    case "coffee":
+                        price = 0.45;
+                        break;
+                    case "water":
+                        price = 0.7;
+                        break;
+                    case "beer":
+                        price = 1.1;
+                        break;
+                    case "sweets":
+                        price = 1.35;
+                        break;
+                    case "peanuts":
+                        price = 1.55;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            double totalPrice = price * quantity;
+            Console.WriteLine(totalPrice);
+        }
+    }
+}
