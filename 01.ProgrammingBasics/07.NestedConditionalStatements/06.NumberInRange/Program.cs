63f6832caab7767bb8a4c67f4bd9be44fac73468
Georgi <bulbrainn@gmail.com>
2022-06-01 19:02:15 +0300

    Added Numbers In Range

---
+++ 01.ProgrammingBasics/07.NestedConditionalStatements/06.NumberInRange/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _06.NumberInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (num == 0 || (num < -100 || num > 100))
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+                Console.WriteLine("Yes");
+            }
+        }
+    }
+}
