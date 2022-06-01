80cd93d659161faba2cc2c263a09788bf71ff838
Georgi <bulbrainn@gmail.com>
2022-06-01 19:00:08 +0300

    Weekend Or Working Day

---
+++ 01.ProgrammingBasics/07.NestedConditionalStatements/02.WeekendOrWorkingDay/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _02.WeekendOrWorkingDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string day = Console.ReadLine();
+            string dayType;
+            switch (day)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Wednesday":
+                case "Thursday":
+                case "Friday":
+                    dayType = "Working day";
+                    break;
+                case "Saturday":
+                case "Sunday":
+                    dayType = "Weekend";
+                    break;
+                default:
+                    dayType = "Error";
+                    break;
+            }
+            Console.WriteLine(dayType);
+        }
+    }
+}
