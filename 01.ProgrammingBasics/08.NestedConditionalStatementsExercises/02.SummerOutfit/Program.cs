f28d2b5fb8527d66ce7e6fc8ecb9f9e43ca5fa6c
Georgi <bulbrainn@gmail.com>
2022-06-02 22:47:05 +0300

    Added Summer Outfit

---
+++ 01.ProgrammingBasics/08.NestedConditionalStatementsExercises/02.SummerOutfit/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _02.SummerOutfit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temperature = int.Parse(Console.ReadLine());
+            string hour = Console.ReadLine();
+            string outfit = "Shirt";
+            string shoes = "Moccasins";
+
+            if (hour == "Morning")
+            {
+                if (temperature >= 10 && temperature <= 18)
+                {
+                    outfit = "Sweatshirt";
+                    shoes = "Sneakers";
+                }
+                else if(temperature >= 25)
+                {
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+            }
+            else if (hour == "Afternoon")
+            {
+                if (temperature > 18 && temperature <= 24)
+                {
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+                else if (temperature >= 25)
+                {
+                    outfit = "Swim Suit";
+                    shoes = "Barefoot";
+                }
+            }
+
+            Console.WriteLine($"It's {temperature} degrees, get your {outfit} and {shoes}.");
+        }
+    }
+}
