80865ed40e1d4dd53ee2ea864715ff3e633ff42f
Georgi <bulbrainn@gmail.com>
2022-06-02 22:46:36 +0300

    Added Cinema

---
+++ 01.ProgrammingBasics/08.NestedConditionalStatementsExercises/01.Cinema/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01.Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+            int seats = rows * cols;
+            double price = 0;
+            switch (type)
+            {
+                case "Premiere":
+                    price = 12;
+                    break;
+                case "Normal":
+                    price = 7.5;
+                    break;
+                case "Discount":
+                    price = 5;
+                    break;
+                default:
+                    break;
+            }
+            
+            double totalPrice = price * seats;
+            Console.WriteLine($"{totalPrice:f2} leva");
+        }
+    }
+}
