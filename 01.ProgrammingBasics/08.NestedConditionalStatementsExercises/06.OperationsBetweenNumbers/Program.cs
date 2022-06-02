90063c68a71866ecc7a321d6a11b857a71f3c16d
Georgi <bulbrainn@gmail.com>
2022-06-02 22:49:13 +0300

    Added Operations Between Numbers

---
+++ 01.ProgrammingBasics/08.NestedConditionalStatementsExercises/06.OperationsBetweenNumbers/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _06.OperationsBetweenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            string opr = Console.ReadLine();
+            double result;
+            string oddEven;
+            switch (opr)
+            {
+                case "+":
+                    result = n1 + n2;
+                    oddEven = (result % 2 == 0) ? "even" : "odd";
+                    Console.WriteLine($"{n1} + {n2} = {result} - {oddEven}");
+                    break;
+                case "-":
+                    result = n1 - n2;
+                    oddEven = (result % 2 == 0) ? "even" : "odd";
+                    Console.WriteLine($"{n1} - {n2} = {result} - {oddEven}");
+                    break;
+                case "*":
+                    result = n1 * n2;
+                    oddEven = (result % 2 == 0) ? "even" : "odd";
+                    Console.WriteLine($"{n1} * {n2} = {result} - {oddEven}");
+                    break;
+                case "/":
+                    if (n2 == 0)
+                    {
+                        Console.WriteLine($"Cannot divide {n1} by zero");
+                        break;
+                    }
+                    result = (double)n1 / n2;
+                    Console.WriteLine($"{n1} / {n2} = {result:f2}");
+                    break;
+                case "%":
+                    if (n2 == 0)
+                    {
+                        Console.WriteLine($"Cannot divide {n1} by zero");
+                        break;
+                    }
+                    result = (double)n1 % n2;
+                    Console.WriteLine($"{n1} % {n2} = {result}");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
