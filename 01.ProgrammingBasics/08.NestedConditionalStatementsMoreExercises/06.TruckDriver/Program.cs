ae5b81a67be27a358dae48debf35e6ad7b666714
Georgi <bulbrainn@gmail.com>
2022-06-03 22:24:25 +0300

    Added Truck Driver

---
+++ 01.ProgrammingBasics/08.NestedConditionalStatementsMoreExercises/06.TruckDriver/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _06.TruckDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string season = Console.ReadLine();
+            double kms = double.Parse(Console.ReadLine());
+            double price = 0;
+
+            if (kms <= 5000)
+            {
+                if (season == "Spring" || season == "Autumn")
+                {
+                    price = 0.75 * kms * 4; 
+                }
+                else if (season == "Summer")
+                {
+                    price = 0.9 * kms * 4;
+                }
+                else
+                {
+                    price = 1.05 * kms * 4;
+                }
+            }
+            else if (kms > 5000 && kms <= 10000)
+            {
+                if (season == "Spring" || season == "Autumn")
+                {
+                    price = 0.95 * kms * 4;
+                }
+                else if (season == "Summer")
+                {
+                    price = 1.1 * kms * 4;
+                }
+                else
+                {
+                    price = 1.25 * kms * 4;
+                }
+            }
+            else if (kms > 10000 && kms <= 20000)
+            {
+                price = 1.45 * kms * 4;
+            }
+
+            price *= 0.9;
+            Console.WriteLine($"{price:f2}");
+        }
+    }
+}
