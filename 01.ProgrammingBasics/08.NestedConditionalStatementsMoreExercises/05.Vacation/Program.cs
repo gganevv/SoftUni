ad4fabbd98b5124e3c6c6a124459919c8d3141b4
Georgi <bulbrainn@gmail.com>
2022-06-03 22:24:02 +0300

    Added Vacation

---
+++ 01.ProgrammingBasics/08.NestedConditionalStatementsMoreExercises/05.Vacation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _05.Vacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            string location = season == "Summer" ? "Alaska" : "Morocco";
+            string accomodation = "";
+            double price = 0;
+
+            if (budget <= 1000)
+            {
+                accomodation = "Camp";
+                price = season == "Summer" ? budget * 0.65 : budget * 0.45;
+            }
+            else if (budget > 1000 && budget <= 3000)
+            {
+                accomodation = "Hut";
+                price = season == "Summer" ? budget * 0.8 : budget * 0.6;
+            }
+            else
+            {
+                accomodation = "Hotel";
+                price = budget * 0.9;
+            }
+
+            Console.WriteLine($"{location} - {accomodation} - {price:f2}");
+        }
+    }
+}
