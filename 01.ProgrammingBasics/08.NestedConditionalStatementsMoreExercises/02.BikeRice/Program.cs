8c35c88742d6959eff42e98c51b10cb3068d2b14
Georgi <bulbrainn@gmail.com>
2022-06-03 22:22:56 +0300

    Added Bike Rice

---
+++ 01.ProgrammingBasics/08.NestedConditionalStatementsMoreExercises/02.BikeRice/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _02.BikeRice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int juniorBikers = int.Parse(Console.ReadLine());
+            int seniorBikers = int.Parse(Console.ReadLine());
+            string riceType = Console.ReadLine();
+            double seniorFee = 0;
+            double juniorFee = 0;
+
+            switch (riceType)
+            {
+                case "trail":
+                    juniorFee = 5.5;
+                    seniorFee = 7;
+                    break;
+                case "cross-country":
+                    juniorFee = 8;
+                    seniorFee = 9.5;
+                    break;
+                case "downhill":
+                    juniorFee = 12.25;
+                    seniorFee = 13.75;
+                    break;
+                case "road":
+                    juniorFee = 20;
+                    seniorFee = 21.5;
+                    break;
+                default:
+                    break;
+            }
+            double totalFee = (juniorFee * juniorBikers) + (seniorFee * seniorBikers);
+            if (juniorBikers + seniorBikers >= 50 && riceType == "cross-country")
+            {
+                totalFee *= 0.75;
+            }
+            totalFee *= 0.95;
+            Console.WriteLine($"{totalFee:f2}");
+        }
+    }
+}
