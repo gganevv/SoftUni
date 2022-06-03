50741dc8e9a91dc0ebe8a5d0781536c457180017
Georgi <bulbrainn@gmail.com>
2022-06-03 22:26:25 +0300

    Added Nums 1 To 10

---
+++ 01.ProgrammingBasics/08.NestedConditionalStatementsMoreExercises/09.Nums1to10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _09.Nums1to10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            while (n <= 10)
+            {
+                Console.WriteLine(n);
+                n++;
+            }
+        }
+    }
+}
