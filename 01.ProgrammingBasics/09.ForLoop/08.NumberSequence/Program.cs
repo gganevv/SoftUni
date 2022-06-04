377b862efc985de8bf3bada0038edd1cc9b0263a
Georgi <bulbrainn@gmail.com>
2022-06-04 22:42:54 +0300

    Added Number Sequence

---
+++ 01.ProgrammingBasics/09.ForLoop/08.NumberSequence/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _08.NumberSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int seqLength = int.Parse(Console.ReadLine());
+            int minNum = int.MaxValue;
+            int maxNum = int.MinValue;
+            for (int i = 0; i < seqLength; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (num > maxNum)
+                {
+                    maxNum = num;
+                }
+                if (num < minNum)
+                {
+                    minNum = num;
+                }
+            }
+            Console.WriteLine($"Max number: {maxNum}");
+            Console.WriteLine($"Min number: {minNum}");
+        }
+    }
+}
