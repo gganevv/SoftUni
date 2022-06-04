c15c224ab64a13a97a7e0d1cd367363e96856307
Georgi <bulbrainn@gmail.com>
2022-06-04 22:43:18 +0300

    Added Left And Right Sum

---
+++ 01.ProgrammingBasics/09.ForLoop/09.LeftAndRightSum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _09.LeftAndRightSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int seqLength = int.Parse(Console.ReadLine());
+            int leftSum = 0;
+            int rightSum = 0;
+            for (int i = 0; i < seqLength; i++)
+            {
+                leftSum += int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < seqLength; i++)
+            {
+                rightSum += int.Parse(Console.ReadLine());
+            }
+
+            if (leftSum == rightSum)
+            {
+                Console.WriteLine($"Yes, sum = {leftSum}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {Math.Abs(leftSum - rightSum)}");
+            }
+        }
+    }
+}
