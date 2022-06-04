5a423393006c0603d43299f81618479f2b67ba71
Georgi <bulbrainn@gmail.com>
2022-06-04 22:43:39 +0300

    Added Odd Even Sum

---
+++ 01.ProgrammingBasics/09.ForLoop/10.OddEvenSum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _10.OddEvenSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int seqLength = int.Parse(Console.ReadLine());
+            int oddSum = 0;
+            int evenSum = 0;
+
+            for (int i = 1; i <= seqLength; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (i % 2 != 0)
+                {
+                    oddSum += num;
+                }
+                else
+                {
+                    evenSum += num;
+                }
+            }
+
+            if (oddSum == evenSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {evenSum}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(oddSum - evenSum)}");
+            }
+        }
+    }
+}
