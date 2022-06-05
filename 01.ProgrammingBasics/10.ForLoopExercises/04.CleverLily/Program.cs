a2b83bcf2dda8dffe4d00dbe4432125b581d260b
Georgi <bulbrainn@gmail.com>
2022-06-05 15:25:31 +0300

    Added Clever Lily

---
+++ 01.ProgrammingBasics/10.ForLoopExercises/04.CleverLily/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _04.CleverLily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            double washingMachinePrice = double.Parse(Console.ReadLine());
+            int toyPrice = int.Parse(Console.ReadLine());
+
+            double savedMoney = 0;
+            for (int i = 1; i <= age; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    savedMoney += i * 5 - 1;
+                }
+                else
+                {
+                    savedMoney += toyPrice;
+                }
+            }
+            double diff = Math.Abs(savedMoney - washingMachinePrice);
+            if (savedMoney >= washingMachinePrice)
+            {
+                Console.WriteLine($"Yes! {diff:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"No! {diff:f2}");
+            }
+        }
+    }
+}
