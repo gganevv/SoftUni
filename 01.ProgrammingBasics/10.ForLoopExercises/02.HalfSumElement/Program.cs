ea8fc5f88a9af03f447630616c10b1d1cfc880a8
Georgi <bulbrainn@gmail.com>
2022-06-05 15:24:35 +0300

    Added Half Sum Element

---
+++ 01.ProgrammingBasics/10.ForLoopExercises/02.HalfSumElement/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _02.HalfSumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int maxNum = int.MinValue;
+            for (int i = 0; i < n; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                sum += currentNum;
+                if (currentNum > maxNum)
+                {
+                    maxNum = currentNum;
+                }
+            }
+
+            int diff = Math.Abs(maxNum - (sum - maxNum));
+            if (sum - maxNum == maxNum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {maxNum}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {diff}");
+            }
+        }
+    }
+}
