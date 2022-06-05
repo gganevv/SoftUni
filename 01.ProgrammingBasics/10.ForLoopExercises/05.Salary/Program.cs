47d3396bd40ac3bd3ae27ee72c26f16e2a5619c5
Georgi <bulbrainn@gmail.com>
2022-06-05 15:26:30 +0300

    Added Salary

---
+++ 01.ProgrammingBasics/10.ForLoopExercises/05.Salary/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _05.Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tabs = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+            for (int i = 0; i < tabs; i++)
+            {
+                string tab = Console.ReadLine();
+                switch (tab)
+                {
+                    case "Facebook":
+                        salary -= 150;
+                        break;
+                    case "Instagram":
+                        salary -= 100;
+                        break;
+                    case "Reddit":
+                        salary -= 50;
+                        break;
+                    default:
+                        break;
+                }
+                if (salary <= 0)
+                {
+                    Console.WriteLine("You have lost your salary.");
+                    break;
+                }
+            }
+            if (salary > 0)
+            {
+                Console.WriteLine(salary);
+            }
+        }
+    }
+}
