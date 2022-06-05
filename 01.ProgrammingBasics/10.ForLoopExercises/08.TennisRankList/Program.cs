95d9d45067de1273904f4f9cdf4b342d573b7dbd
Georgi <bulbrainn@gmail.com>
2022-06-05 15:27:35 +0300

    Added Tennis Rank List

---
+++ 01.ProgrammingBasics/10.ForLoopExercises/08.TennisRankList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _08.TennisRankList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfTournaments = int.Parse(Console.ReadLine());
+            int points = int.Parse(Console.ReadLine());
+            double earnedPoints = 0;
+            double wonTournamets = 0;
+            for (int i = 0; i < numberOfTournaments; i++)
+            {
+                string tournamentResult = Console.ReadLine();
+                if (tournamentResult == "W")
+                {
+                    earnedPoints += 2000;
+                    wonTournamets++;
+                }
+                else if (tournamentResult == "F")
+                {
+                    earnedPoints += 1200;
+                }
+                else
+                {
+                    earnedPoints += 720;
+                }
+            }
+            Console.WriteLine($"Final points: {points + earnedPoints}");
+            Console.WriteLine($"Average points: {Math.Floor(earnedPoints / numberOfTournaments)}");
+            Console.WriteLine($"{wonTournamets / numberOfTournaments * 100:f2}%");
+        }
+    }
+}
