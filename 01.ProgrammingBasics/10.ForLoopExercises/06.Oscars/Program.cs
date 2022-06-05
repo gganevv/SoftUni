d9162c7a9d1b0c75d088271d849814537d997bcb
Georgi <bulbrainn@gmail.com>
2022-06-05 15:26:50 +0300

    Added Oscars

---
+++ 01.ProgrammingBasics/10.ForLoopExercises/06.Oscars/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _06.Oscars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string actorName = Console.ReadLine();
+            double totalPoints = double.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double points = double.Parse(Console.ReadLine());
+                double addedPoints = name.Length * points / 2;
+                totalPoints += addedPoints;
+                if (totalPoints > 1250.5)
+                {
+                    Console.WriteLine($"Congratulations, {actorName} got a nominee for leading role with {totalPoints:f1}!");
+                    break;
+                }
+            }
+            if (totalPoints <= 1250.5)
+            {
+                Console.WriteLine($"Sorry, {actorName} you need {1250.5 - totalPoints:f1} more!");
+            }
+        }
+    }
+}
