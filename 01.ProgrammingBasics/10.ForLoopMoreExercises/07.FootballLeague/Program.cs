1d226f1082e36d5fc0ab557d1f509409b65c33a9
Georgi <bulbrainn@gmail.com>
2022-06-06 22:07:05 +0300

    Added Football League

---
+++ 01.ProgrammingBasics/10.ForLoopMoreExercises/07.FootballLeague/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _07.FootballLeague
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int stadiumCapacity = int.Parse(Console.ReadLine());
+            int fansNumber = int.Parse(Console.ReadLine());
+            double a = 0;
+            double b = 0;
+            double v = 0;
+            double g = 0;
+            for (int i = 0; i < fansNumber; i++)
+            {
+                string sector = Console.ReadLine();
+                switch (sector)
+                {
+                    case "A":
+                        a++;
+                        break;
+                    case "B":
+                        b++;
+                        break;
+                    case "V":
+                        v++;
+                        break;
+                    case "G":
+                        g++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine($"{a / fansNumber * 100:f2}%");
+            Console.WriteLine($"{b / fansNumber * 100:f2}%");
+            Console.WriteLine($"{v / fansNumber * 100:f2}%");
+            Console.WriteLine($"{g / fansNumber * 100:f2}%");
+            Console.WriteLine($"{(double)fansNumber / stadiumCapacity * 100:f2}%");
+        }
+    }
+}
