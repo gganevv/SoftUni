512047f00b7f9cb9600202fe53372737392742e4
Georgi <bulbrainn@gmail.com>
2022-06-06 22:08:03 +0300

    Added Equal Pairs

---
+++ 01.ProgrammingBasics/10.ForLoopMoreExercises/08.EqualPairs/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _08.EqualPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pairs = int.Parse(Console.ReadLine());
+            int firstN = int.Parse(Console.ReadLine());
+            int secondN = int.Parse(Console.ReadLine());
+            int pastSum = firstN + secondN;
+            bool same = true;
+            int diff = 0;
+
+            for (int i = 1; i < pairs; i++)
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+                int sum = n1 + n2;
+                if (sum != pastSum)
+                {
+                    same = false;
+                    if (Math.Abs(sum - pastSum) > diff)
+                    {
+                        diff = Math.Abs(sum - pastSum);
+                    }
+                }
+                pastSum = sum;
+            }
+            if (same)
+            {
+                Console.WriteLine($"Yes, value={pastSum}");
+            }
+            else
+            {
+                Console.WriteLine($"No, maxdiff={diff}");
+            }
+        }
+    }
+}
