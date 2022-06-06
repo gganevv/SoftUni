74b1b9fdb0f9dac40def1e3b4e7c4eb153909025
Georgi <bulbrainn@gmail.com>
2022-06-06 22:04:02 +0300

    Added Logistics

---
+++ 01.ProgrammingBasics/10.ForLoopMoreExercises/03.Logistics/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _03.Logistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLoads = int.Parse(Console.ReadLine());
+            double busLoads = 0;
+            double truckLoads = 0;
+            double trainLoads = 0;
+            double totalPrice = 0;
+            int totalLoads = 0;
+
+            for (int i = 0; i < numberOfLoads; i++)
+            {
+                int load = int.Parse(Console.ReadLine());
+                totalLoads += load;
+                if (load <= 3)
+                {
+                    busLoads += load;
+                    totalPrice += load * 200;
+                }
+                else if (load < 12)
+                {
+                    truckLoads += load;
+                    totalPrice += load * 175;
+                }
+                else
+                {
+                    trainLoads += load;
+                    totalPrice += load * 120;
+                }
+            }
+           
+            Console.WriteLine($"{totalPrice / totalLoads:f2}");
+            Console.WriteLine($"{busLoads / totalLoads * 100:f2}%");
+            Console.WriteLine($"{truckLoads / totalLoads * 100:f2}%");
+            Console.WriteLine($"{trainLoads / totalLoads * 100:f2}%");
+        }
+    }
+}
