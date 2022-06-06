709ddf0573132df2bf7054198c38d72de5d17baf
Georgi <bulbrainn@gmail.com>
2022-06-06 22:03:24 +0300

    Added Hospital

---
+++ 01.ProgrammingBasics/10.ForLoopMoreExercises/02.Hospital/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _02.Hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int doctors = 7;
+            int treatedPatients = 0;
+            int untreatedpatients = 0;
+
+            for (int i = 1; i <= days; i++)
+            {
+                if (i % 3 == 0 && untreatedpatients > treatedPatients)
+                {
+                    doctors++;
+                }
+                int daylypatients = int.Parse(Console.ReadLine());
+                if (daylypatients <= doctors)
+                {
+                    treatedPatients += daylypatients;
+                }
+                else
+                {
+                    treatedPatients += doctors;
+                    untreatedpatients += (daylypatients - doctors);
+                }
+                
+            }
+            Console.WriteLine($"Treated patients: {treatedPatients}.");
+            Console.WriteLine($"Untreated patients: {untreatedpatients}.");
+        }
+    }
+}
