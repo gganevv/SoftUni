113250cab1fc3cd14e7379bd94bb3303991a2ed7
Georgi <bulbrainn@gmail.com>
2022-06-07 21:02:16 +0300

    Added Sum Numbers

---
+++ 01.ProgrammingBasics/11.WhileLoop/03.SumNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _03.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int sumNum = 0;
+            while (sumNum < firstNum)
+            {
+                sumNum += int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(sumNum);
+        }
+    }
+}
