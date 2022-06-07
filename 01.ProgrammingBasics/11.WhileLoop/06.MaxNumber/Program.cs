24d16ad107782eabcd837ba2d2a38924b9974a10
Georgi <bulbrainn@gmail.com>
2022-06-07 21:04:14 +0300

    Added Max Number

---
+++ 01.ProgrammingBasics/11.WhileLoop/06.MaxNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _06.MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNum = int.Parse(Console.ReadLine());
+            string currentNum = Console.ReadLine();
+            while (currentNum != "Stop")
+            {
+                int n = int.Parse(currentNum);
+                if (n > maxNum)
+                {
+                    maxNum = n;
+                }
+                currentNum = Console.ReadLine();
+            }
+            Console.WriteLine(maxNum);
+        }
+    }
+}
