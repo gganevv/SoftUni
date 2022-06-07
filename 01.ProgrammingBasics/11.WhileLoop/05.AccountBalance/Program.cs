20ff904cc162c2220f45f905d28d56d70ac5e103
Georgi <bulbrainn@gmail.com>
2022-06-07 21:03:52 +0300

    Added Account Balance

---
+++ 01.ProgrammingBasics/11.WhileLoop/05.AccountBalance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _05.AccountBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string addedMoney = Console.ReadLine();
+            double balance = 0;
+            while (addedMoney != "NoMoreMoney")
+            {
+                double increase = double.Parse(addedMoney);
+                if (increase < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                balance += increase;
+                Console.WriteLine($"Increase: {increase:f2}");
+                addedMoney = Console.ReadLine();
+            }
+            Console.WriteLine($"Total: {balance:f2}");
+        }
+    }
+}
