4a1a8b5a389c3b058559cb9754d506307c54ad6c
Georgi <bulbrainn@gmail.com>
2022-06-07 21:05:12 +0300

    Added Min Num

---
+++ 01.ProgrammingBasics/11.WhileLoop/07.MinNum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _07.MinNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            int minNum = int.MaxValue;
+            while (command != "Stop")
+            {
+                int num = int.Parse(command);
+                if (minNum > num)
+                {
+                    minNum = num;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(minNum);
+        }
+    }
+}
