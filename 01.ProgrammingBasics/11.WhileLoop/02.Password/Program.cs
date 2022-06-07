c51b413b801036b169d820bf5a9d0cd00818bf0d
Georgi <bulbrainn@gmail.com>
2022-06-07 21:01:49 +0300

    Added Password

---
+++ 01.ProgrammingBasics/11.WhileLoop/02.Password/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02.Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string password = Console.ReadLine();
+            string checkPass = "";
+
+            while (checkPass != password)
+            {
+                checkPass = Console.ReadLine();
+            }
+            Console.WriteLine($"Welcome {name}!");
+        }
+    }
+}
