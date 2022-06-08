2666bbc8f503f691f881de3ede1e64942d877dab
Georgi <bulbrainn@gmail.com>
2022-06-08 18:24:59 +0300

    Added Cake

---
+++ 01.ProgrammingBasics/12.WhileLoopExercises/06.Cake/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _06.Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int w = int.Parse(Console.ReadLine());
+            int h = int.Parse(Console.ReadLine());
+
+            int cakePcs = w * h;
+            string command = Console.ReadLine();
+            while (command != "STOP")
+            {
+                int takenPcs = int.Parse(command);
+                cakePcs -= takenPcs;
+                if (cakePcs <= 0)
+                {
+                    break;
+                }
+                command = Console.ReadLine();
+            }
+            if (cakePcs < 0)
+            {
+                Console.WriteLine($"No more cake left! You need {Math.Abs(cakePcs)} pieces more.");
+            }
+            else
+            {
+                Console.WriteLine($"{cakePcs} pieces are left.");
+            }
+        }
+    }
+}
