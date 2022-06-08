71c00ef2ef2c04f581240573dd0db68397a6efa2
Georgi <bulbrainn@gmail.com>
2022-06-08 18:24:18 +0300

    Added Walking

---
+++ 01.ProgrammingBasics/12.WhileLoopExercises/04.Walking/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _04.Walking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int stepsNeeded = 10000;
+            int steps = 0;
+            while (steps < stepsNeeded)
+            {
+                string command = Console.ReadLine();
+                if (command != "Going home")
+                {
+                    steps += int.Parse(command);
+                }
+                else
+                {
+                    steps += int.Parse(Console.ReadLine());
+                    break;
+                }
+            }
+
+            if (steps >= stepsNeeded)
+            {
+            Console.WriteLine("Goal reached! Good job!");
+            Console.WriteLine($"{steps - stepsNeeded} steps over the goal!");
+            }
+            else
+            {
+                Console.WriteLine($"{stepsNeeded - steps} more steps to reach goal.");
+            }
+        }
+    }
+}
