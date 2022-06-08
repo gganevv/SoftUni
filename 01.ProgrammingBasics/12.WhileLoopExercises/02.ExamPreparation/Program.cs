68183263068bb1cd796510849d25786aa7860415
Georgi <bulbrainn@gmail.com>
2022-06-08 18:23:37 +0300

    Added Exam Preparation

---
+++ 01.ProgrammingBasics/12.WhileLoopExercises/02.ExamPreparation/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _02.ExamPreparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxBadGrades = int.Parse(Console.ReadLine());
+            int badGrades = 0;
+            string problemName = string.Empty;
+            double totalGrade = 0;
+            double gradeCounter = 0;
+            string lastProblem = string.Empty;
+            while (problemName != "Enough")
+            {
+                problemName = Console.ReadLine();
+                if (problemName == "Enough")
+                {
+                    Console.WriteLine($"Average score: {totalGrade / gradeCounter:f2}");
+                    Console.WriteLine($"Number of problems: {gradeCounter}");
+                    Console.WriteLine($"Last problem: {lastProblem}");
+                    break;
+                }
+                else
+                {
+                    int grade = int.Parse(Console.ReadLine());
+                    totalGrade += grade;
+                    lastProblem = problemName;
+                    if (grade <= 4)
+                    {
+                        badGrades++;
+                    }
+                    if (badGrades == maxBadGrades)
+                    {
+                        Console.WriteLine($"You need a break, {maxBadGrades} poor grades.");
+                        break;
+                    }
+                }
+                gradeCounter++;
+            }
+        }
+    }
+}
