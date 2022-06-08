7a3106ba4e5fe2ab21e5d4de82dada3bcddabadb
Georgi <bulbrainn@gmail.com>
2022-06-08 18:23:10 +0300

    Added Old Books

---
+++ 01.ProgrammingBasics/12.WhileLoopExercises/01.OldBooks/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01.OldBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string book = Console.ReadLine();
+            int bookCounter = 0;
+            string currentBook = null;
+            while (book != currentBook)
+            {
+                currentBook = Console.ReadLine();
+                if (currentBook == book)
+                {
+                    Console.WriteLine($"You checked {bookCounter} books and found it.");
+                    break;
+                }
+                
+                if (currentBook == "No More Books")
+                {
+                    Console.WriteLine("The book you search is not here!");
+                    Console.WriteLine($"You checked {bookCounter} books.");
+                    break;
+                }
+
+                bookCounter++;
+            }
+        }
+    }
+}
