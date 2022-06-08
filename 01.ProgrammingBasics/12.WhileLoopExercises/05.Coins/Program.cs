95b02ec7c22b1c6ba2f7b8313da3e9f6f35512a4
Georgi <bulbrainn@gmail.com>
2022-06-08 18:24:39 +0300

    Added Coins

---
+++ 01.ProgrammingBasics/12.WhileLoopExercises/05.Coins/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _05.Coins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal change = decimal.Parse(Console.ReadLine());
+            int counter = 0;
+            while (change > 0)
+            {
+                if (change >= 2)
+                {
+                    change -= 2;
+                }
+                else if(change >= 1)
+                {
+                    change -= 1;
+                }
+                else if (change >= 0.5M)
+                {
+                    change -= 0.5M;
+                }
+                else if (change >= 0.2M)
+                {
+                    change -= 0.2M;
+                }
+                else if (change >= 0.1M)
+                {
+                    change -= 0.1M;
+                }
+                else if (change >= 0.05M)
+                {
+                    change -= 0.05M;
+                }
+                else if (change >= 0.02M)
+                {
+                    change -= 0.02M;
+                }
+                else if (change >= 0.01M)
+                {
+                    change -= 0.01M;
+                }
+                counter++;
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
