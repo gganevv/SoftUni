651445ce54be3391cb9926d467140725f50e43d4
Georgi <bulbrainn@gmail.com>
2022-06-08 18:23:58 +0300

    Added Vacation

---
+++ 01.ProgrammingBasics/12.WhileLoopExercises/03.Vacation/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _03.Vacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double neededMoney = double.Parse(Console.ReadLine());
+            double savedMoney = double.Parse(Console.ReadLine());
+            int days = 0;
+            int daysSpending = 0;
+            while (true)
+            {
+                days++;
+                string action = Console.ReadLine();
+                double money = double.Parse(Console.ReadLine());
+                if (action == "spend")
+                {
+                    daysSpending++;
+                    savedMoney -= money;
+                    if (savedMoney < 0)
+                    {
+                        savedMoney = 0;
+                    }
+                    if (daysSpending == 5)
+                    {
+                        Console.WriteLine("You can't save the money.");
+                        Console.WriteLine(days);
+                        break;
+                    }
+                }
+                else
+                {
+                    savedMoney += money;
+                    if (savedMoney >= neededMoney)
+                    {
+                        Console.WriteLine($"You saved the money for {days} days.");
+                        break;
+                    }
+                    daysSpending = 0;
+                }
+            }
+        }
+    }
+}
