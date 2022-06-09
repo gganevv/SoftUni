788c5b85174c766d3044ed46bc5c4c6d8e164646
Georgi <bulbrainn@gmail.com>
2022-06-09 19:50:41 +0300

    Added Report System

---
+++ 01.ProgrammingBasics/12.WhileLoopMoreExercises/02.ReportSystem/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace _02.ReportSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int neededMoney = int.Parse(Console.ReadLine());
+            int collectedMoney = 0;
+            string command = Console.ReadLine();
+            int paimentN = 0;
+            double cs = 0;
+            double csTotal = 0;
+            double cc = 0;
+            double ccTotal = 0;
+            while (command != "End")
+            {
+                paimentN++;
+                int payment = int.Parse(command);
+                if (paimentN % 2 != 0)
+                {
+                    if (payment > 100)
+                    {
+                        Console.WriteLine("Error in transaction!");
+                        command = Console.ReadLine();
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Product sold!");
+                        collectedMoney += payment;
+                        cs++;
+                        csTotal += payment;
+                    }
+                }
+                else
+                {
+                    if (payment < 10)
+                    {
+                        Console.WriteLine("Error in transaction!");
+                        command = Console.ReadLine();
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Product sold!");
+                        collectedMoney += payment;
+                        cc++;
+                        ccTotal += payment;
+                    }
+                }
+                if (collectedMoney >= neededMoney)
+                {
+                    break;
+                }
+                command = Console.ReadLine();
+            }
+            if (collectedMoney < neededMoney)
+            {
+                Console.WriteLine($"Failed to collect required money for charity.");
+            }
+            else
+            {
+                Console.WriteLine($"Average CS: {csTotal/cs:f2}");
+                Console.WriteLine($"Average CC: {ccTotal/cc:f2}");
+            }
+        }
+    }
+}
