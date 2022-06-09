af2d4cfe541276640ac59da9275e3ff6ac31acb8
Georgi <bulbrainn@gmail.com>
2022-06-09 19:50:15 +0300

    Added Dish Washer

---
+++ 01.ProgrammingBasics/12.WhileLoopMoreExercises/01.DishWasher/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _01.DishWasher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bottles = int.Parse(Console.ReadLine());
+            int detergent = bottles * 750;
+            string command = Console.ReadLine();
+            int washCounter = 1;
+            int dishes = 0;
+            int pots = 0;
+
+            while (command != "End")
+            {
+                int dishesOrPots = int.Parse(command);
+                if (washCounter % 3 == 0)
+                {
+                    detergent -= dishesOrPots * 15;
+                    pots += dishesOrPots;
+                }
+                else
+                {
+                    detergent -= dishesOrPots * 5;
+                    dishes += dishesOrPots;
+                }
+                if (detergent < 0)
+                {
+                    Console.WriteLine($"Not enough detergent, {Math.Abs(detergent)} ml. more necessary!");
+                    break;
+                }
+                washCounter++;
+                command = Console.ReadLine();
+            }
+            if (detergent >= 0)
+            {
+                Console.WriteLine("Detergent was enough!");
+                Console.WriteLine($"{dishes} dishes and {pots} pots were washed.");
+                Console.WriteLine($"Leftover detergent {detergent} ml.");
+            }
+        }
+    }
+}
