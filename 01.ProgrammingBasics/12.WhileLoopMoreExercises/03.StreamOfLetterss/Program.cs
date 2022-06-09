5a26e37f8022c220f4889c60af83b9fd1d0000c3
Georgi <bulbrainn@gmail.com>
2022-06-09 19:51:04 +0300

    Added Steam Of Letters

---
+++ 01.ProgrammingBasics/12.WhileLoopMoreExercises/03.StreamOfLetterss/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _03.StreamOfLetterss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool c = false;
+            bool o = false;
+            bool n = false;
+            string command = Console.ReadLine();
+            string word = string.Empty;
+            while (command != "End")
+            {
+                char com = char.Parse(command);
+                if (command == "c" && c == false)
+                {
+                    c = true;
+                }
+                else if (command == "o" && o == false)
+                {
+                    o = true;
+                }
+                else if (command == "n" && n == false)
+                {
+                    n = true;
+                }
+                else if ((com > 64 && com < 91) || (com > 96 && com < 123))
+                {
+                    word += command;
+                }
+
+                if (c && o && n)
+                {
+                    Console.Write(word + " ");
+                    c = false;
+                    o = false;
+                    n = false;
+                    word = string.Empty;
+                }
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
