12e507c25419c45881960639bfc9e2523ab14723
Georgi <bulbrainn@gmail.com>
2022-06-09 19:52:05 +0300

    Added Average Number

---
+++ 01.ProgrammingBasics/12.WhileLoopMoreExercises/05.AveregeNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _05.AveregeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"{sum / n:f2}");
+        }
+    }
+}
