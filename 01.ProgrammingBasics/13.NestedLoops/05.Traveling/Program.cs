b8ad86f08a40633be8bd860f808fcf74b4e2f88b
Georgi <bulbrainn@gmail.com>
2022-06-10 19:56:53 +0300

    Added Traveling

---
+++ 01.ProgrammingBasics/13.NestedLoops/05.Traveling/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _05.Traveling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+            while (destination != "End")
+            {
+                double neededMoney = double.Parse(Console.ReadLine());
+                double savedMoney = 0;
+                while (savedMoney < neededMoney)
+                {
+                    savedMoney += double.Parse(Console.ReadLine());
+                }
+                Console.WriteLine($"Going to {destination}!");
+                destination = Console.ReadLine();
+            }
+        }
+    }
+}
