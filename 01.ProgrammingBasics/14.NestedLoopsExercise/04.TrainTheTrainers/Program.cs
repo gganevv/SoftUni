6920420f07b7f306dce7edf9c6127a549e5bb03b
Georgi <bulbrainn@gmail.com>
2022-06-11 16:57:30 +0300

    Added Train The Trainers

---
+++ 01.ProgrammingBasics/14.NestedLoopsExercise/04.TrainTheTrainers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _04.TrainTheTrainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int trainers = int.Parse(Console.ReadLine());
+            double totalpoints = 0;
+            string command = Console.ReadLine();
+            int presentationCounter = 0;
+            while (command != "Finish")
+            {
+                string currentPresentation = command;
+                double currentGrade = 0;
+                for (int i = 0; i < trainers; i++)
+                {
+                    currentGrade += double.Parse(Console.ReadLine());
+                }
+                totalpoints += currentGrade;
+                Console.WriteLine($"{currentPresentation} - {currentGrade / trainers:f2}.");
+                presentationCounter++;
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Student's final assessment is {totalpoints / (presentationCounter * trainers):f2}.");
+        }
+    }
+}
