4cdfb5004208961b5fd8e6807f5a08a6f541d945
Georgi <bulbrainn@gmail.com>
2022-06-11 16:57:06 +0300

    Added Sum Prime And Non Prime

---
+++ 01.ProgrammingBasics/14.NestedLoopsExercise/03.SumPrimeNonPrime/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _03.SumPrimeNonPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            int primeSum = 0;
+            int nonPrimeSum = 0;
+            int divCounter = 0;
+
+            while (command != "stop")
+            {
+                int n = int.Parse(command);
+                divCounter = 0;
+
+                if (n < 0)
+                {
+                    Console.WriteLine("Number is negative.");
+                    command = Console.ReadLine();
+                    continue;
+                }
+
+                for (int i = 1; i <= n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        divCounter++;
+                    }
+                }
+
+                if (divCounter == 2)
+                {
+                    primeSum += n;
+                }
+                else
+                {
+                    nonPrimeSum += n;
+                }
+                command = Console.ReadLine();
+                
+            }
+
+            Console.WriteLine($"Sum of all prime numbers is: {primeSum}");
+            Console.WriteLine($"Sum of all non prime numbers is: {nonPrimeSum}");
+        }
+    }
+}
