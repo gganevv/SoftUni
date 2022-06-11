06e65ce02feb48cdd29476d61615c1b989f91f93
Georgi <bulbrainn@gmail.com>
2022-06-11 16:56:27 +0300

    Added Equal Sum Even And Odd Position

---
+++ 01.ProgrammingBasics/14.NestedLoopsExercise/02.EqualSumEvenOddPosition/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _02.EqualSumEvenOddPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            for (int i = n1; i <= n2; i++)
+            {
+                string num = i.ToString();
+                int oddSum = 0;
+                int evenSum = 0;
+                for (int j = 0; j < num.Length; j++)
+                {
+                    if (j % 2 == 0)
+                    {
+                        oddSum += num[j];
+                    }
+                    else
+                    {
+                        evenSum += num[j];
+                    }
+                }
+                if (oddSum == evenSum)
+                {
+                    Console.Write(num + " ");
+                }
+            }
+        }
+    }
+}
