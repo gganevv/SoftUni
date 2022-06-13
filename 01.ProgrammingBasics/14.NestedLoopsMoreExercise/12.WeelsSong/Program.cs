a90360f216103f18bd61f0f0b550e5b4bee3db77
Georgi <bulbrainn@gmail.com>
2022-06-13 19:27:08 +0300

    Added Weels Song

---
+++ 01.ProgrammingBasics/14.NestedLoopsMoreExercise/12.WeelsSong/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _12.WeelsSong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = int.Parse(Console.ReadLine());
+            int counter = 0;
+            string password = string.Empty;
+
+            for (int a = 1; a <= 9; a++)
+            {
+                for (int b = 1; b <= 9; b++)
+                {
+                    for (int c = 1; c <= 9; c++)
+                    {
+                        for (int d = 1; d <= 9; d++)
+                        {
+                            if (((a * b) + (c * d) == m) 
+                                && (a < b) && (c > d))
+                            {
+                                Console.Write($"{a}{b}{c}{d} ");
+                                counter++;
+                                if (counter == 4)
+                                {
+                                    password = $"{a}{b}{c}{d}";
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (counter >= 4)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Password: {password}");
+            }
+            else if (counter > 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("No!");
+            }
+            else
+            {
+                Console.WriteLine("No!");
+            }
+        }
+    }
+}
