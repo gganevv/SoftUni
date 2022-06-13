743b3bf2e22aa483ed019f304848745893df4e7b
Georgi <bulbrainn@gmail.com>
2022-06-13 19:24:54 +0300

    Added Safe Password Generator

---
+++ 01.ProgrammingBasics/14.NestedLoopsMoreExercise/07.SafePasswordGenerator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _07.SafePasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int maxPass = int.Parse(Console.ReadLine());
+            char j = '@';
+            char i = '#';
+            int counter = 0;
+
+            for (int k = 1; k <= a; k++)
+            {
+                for (int l = 1; l <= b; l++, j++, i++)
+                {
+                    if (i == '8')
+                    {
+                        i = '#';
+                    }
+                    if (j == 'a')
+                    {
+                        j = '@';
+                    }
+                    Console.Write($"{i}{j}{k}{l}{j}{i}|");
+                    counter++;
+                    if (counter == maxPass)
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
