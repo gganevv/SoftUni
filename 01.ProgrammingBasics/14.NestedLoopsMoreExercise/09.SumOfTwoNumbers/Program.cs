f41b06331864a38c6da2f913a193b8756da78efe
Georgi <bulbrainn@gmail.com>
2022-06-13 19:25:46 +0300

    Added Sum Of Two Numbers

---
+++ 01.ProgrammingBasics/14.NestedLoopsMoreExercise/09.SumOfTwoNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _09.SumOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            for (int i = n1; i <= n2; i++)
+            {
+                for (int j = n1; j <= n2; j++)
+                {
+                    counter++;
+                    if (i + j == magicNumber)
+                    {
+                        Console.WriteLine($"Combination N:{counter} ({i} + {j} = {magicNumber})");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine($"{counter} combinations - neither equals {magicNumber}");
+        }
+    }
+}
