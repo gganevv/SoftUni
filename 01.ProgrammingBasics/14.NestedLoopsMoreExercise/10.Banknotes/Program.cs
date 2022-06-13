1382e829dc0e8aa9aa3137cf5c5a2cfd4c17c1a3
Georgi <bulbrainn@gmail.com>
2022-06-13 19:26:09 +0300

    Added Banknotes

---
+++ 01.ProgrammingBasics/14.NestedLoopsMoreExercise/10.Banknotes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _10.Banknotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int coinsOne = int.Parse(Console.ReadLine());
+            int coinsTwo = int.Parse(Console.ReadLine());
+            int coinsFive = int.Parse(Console.ReadLine());
+            int desiredMoney = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= coinsOne; i++)
+            {
+                for (int j = 0; j <= coinsTwo; j++)
+                {
+                    for (int k = 0; k <= coinsFive; k++)
+                    {
+                        if (1 * i + 2 * j + 5 * k == desiredMoney)
+                        {
+                            Console.WriteLine($"{i} * 1 lv. + {j} * 2 lv. + {k} * 5 lv. = {desiredMoney} lv.");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
