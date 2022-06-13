7a0791b10648c481dba7b53fb290d6f560780a17
Georgi <bulbrainn@gmail.com>
2022-06-13 19:23:54 +0300

    Added Challenge The Wedding

---
+++ 01.ProgrammingBasics/14.NestedLoopsMoreExercise/05.ChallangeTheWedding/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _05.ChallangeTheWedding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int man = int.Parse(Console.ReadLine());
+            int woman = int.Parse(Console.ReadLine());
+            int tables = int.Parse(Console.ReadLine());
+            int tablesCounter = 0;
+            for (int i = 1; i <= man; i++)
+            {
+                for (int j = 1; j <= woman; j++)
+                {
+                    Console.Write($"({i} <-> {j}) ");
+                    tablesCounter++;
+                    if (tablesCounter >= tables)
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
