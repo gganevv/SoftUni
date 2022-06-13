c6ff403893e14d140c80cca436d0a717ccc8d222
Georgi <bulbrainn@gmail.com>
2022-06-13 19:25:18 +0300

    Added Secret Door

---
+++ 01.ProgrammingBasics/14.NestedLoopsMoreExercise/08.SecretDoor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _08.SecretDoor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= n1; i+=2)
+            {
+                for (int j = 2; j <= n2; j++)
+                {
+                    for (int k = 2; k <= n3; k+=2)
+                    {
+                        if (j == 2 || j == 3 || j == 5 || j == 7)
+                        {
+                            Console.WriteLine($"{i} {j} {k}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
