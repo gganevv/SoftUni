e7cd8ca53e6718dc461bb51a9c7639776e265d64
Georgi <bulbrainn@gmail.com>
2022-06-13 19:22:09 +0300

    Added Letters Combination

---
+++ 01.ProgrammingBasics/14.NestedLoopsMoreExercise/02.LettersCombination/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _02.LettersCombination
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char combinationStart = char.Parse(Console.ReadLine());
+            char combinationEnd = char.Parse(Console.ReadLine());
+            char ignoredChar = char.Parse(Console.ReadLine());
+            int counter = 0;
+            for (int i = combinationStart; i <= combinationEnd; i++)
+            {
+                for (int j = combinationStart; j <= combinationEnd; j++)
+                {
+                    for (int k = combinationStart; k <= combinationEnd; k++)
+                    {
+                        if (i != ignoredChar && j != ignoredChar && k != ignoredChar)
+                        {
+                            Console.Write($"{(char)i}{(char)j}{(char)k} ");
+                            counter++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
