57cca59a3a89f166e19f94cd794bd2795e54cd0c
Georgi <bulbrainn@gmail.com>
2022-06-13 19:24:21 +0300

    Added Wedding Seats

---
+++ 01.ProgrammingBasics/14.NestedLoopsMoreExercise/06.WeddingSeats/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _06.WeddingSeats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char lastSector = char.Parse(Console.ReadLine());
+            int rows = int.Parse(Console.ReadLine());
+            int seats = int.Parse(Console.ReadLine());
+            int rowCounter = 0;
+            int totalSeatsCounter = 0;
+            for (char i = 'A'; i <= lastSector; i++)
+            {
+                for (int r = 1; r <= rows + rowCounter; r++)
+                {
+                    int seatsCounter = r % 2 == 0 ? 2 : 0;
+                    for (char s = 'a'; s < seats + 97 + seatsCounter; s++)
+                    {
+                        Console.WriteLine($"{(char)i}{r}{(char)s}");
+                        totalSeatsCounter++;
+                    }
+                }
+                rowCounter++;
+            }
+            Console.WriteLine(totalSeatsCounter);
+        }
+    }
+}
