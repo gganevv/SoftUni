363ece3a1ecd5fe84d74d8ed3514e7bff4d4f0ad
Georgi <bulbrainn@gmail.com>
2022-06-13 19:23:19 +0300

    Added Car Numbers

---
+++ 01.ProgrammingBasics/14.NestedLoopsMoreExercise/04.CarNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _04.CarNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingNum = int.Parse(Console.ReadLine());
+            int endingNum = int.Parse(Console.ReadLine());
+
+            for (int i = startingNum; i <= endingNum; i++)
+            {
+                for (int j = startingNum; j <= endingNum; j++)
+                {
+                    for (int k = startingNum; k <= endingNum; k++)
+                    {
+                        for (int l = startingNum; l <= endingNum; l++)
+                        {
+                            bool oddEven = (i % 2 == 0 && l % 2 != 0) || (i % 2 != 0 && l % 2 == 0);
+                            bool firstBigger = i > l;
+                            bool evenSum = (j + k) % 2 == 0;
+                            if (oddEven && firstBigger && evenSum)
+                            {
+                                Console.Write($"{i}{j}{k}{l} ");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
