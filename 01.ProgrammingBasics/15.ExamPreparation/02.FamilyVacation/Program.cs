5df12727738cb52e20e0a480bc0f7b48dae85e49
Georgi <bulbrainn@gmail.com>
2022-06-14 18:26:21 +0300

    Added Family Vacation

---
+++ 01.ProgrammingBasics/15.ExamPreparation/02.FamilyVacation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _02.FamilyVacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int nights = int.Parse(Console.ReadLine());
+            double nightPrice = double.Parse(Console.ReadLine());
+            double increasePercent = int.Parse(Console.ReadLine());
+
+            if (nights > 7)
+            {
+                nightPrice *= 0.95;
+            }
+
+            double neededMoney = nights * nightPrice + (budget * increasePercent / 100);
+            double diff = Math.Abs(budget - neededMoney);
+
+            if (budget >= neededMoney)
+            {
+                Console.WriteLine($"Ivanovi will be left with {diff:f2} leva after vacation.");
+            }
+            else
+            {
+                Console.WriteLine($"{diff:f2} leva needed.");
+            }
+            
+        }
+    }
+}
