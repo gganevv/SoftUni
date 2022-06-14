0fc57236ca12f939e1b5a01ac1421f9dc5330c54
Georgi <bulbrainn@gmail.com>
2022-06-14 18:25:55 +0300

    Added Fruit Market

---
+++ 01.ProgrammingBasics/15.ExamPreparation/01.FruitMarket/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01.FruitMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double strawberriesPrice = double.Parse(Console.ReadLine());
+            double bananasKgs = double.Parse(Console.ReadLine());
+            double orangeKgs = double.Parse(Console.ReadLine());
+            double raspberriesKgs = double.Parse(Console.ReadLine());
+            double strawberriesKgs = double.Parse(Console.ReadLine());
+
+            double raspberriesPrice = strawberriesPrice * 0.5;
+            double orangePrice = raspberriesPrice * 0.6;
+            double bananaPrice = raspberriesPrice * 0.2;
+
+            double total = strawberriesPrice * strawberriesKgs + bananaPrice * bananasKgs + orangeKgs * orangePrice + raspberriesKgs * raspberriesPrice;
+
+            Console.WriteLine($"{total:f2}");
+        }
+    }
+}
