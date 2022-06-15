d2a369d3ed6f9060adb10463e5c17c8eea155bc3
Georgi <bulbrainn@gmail.com>
2022-06-15 19:24:53 +0300

    Added Excursion

---
+++ 01.ProgrammingBasics/16.Exam/01.Excursion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01.Excursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int nights = int.Parse(Console.ReadLine());
+            int cards = int.Parse(Console.ReadLine());
+            int museum = int.Parse(Console.ReadLine());
+
+            double totalPrice = (people * nights * 20.0) + (people * cards * 1.6) + (people * museum * 6.0);
+            totalPrice *= 1.25;
+            Console.WriteLine($"{totalPrice:f2}");
+        }
+    }
+}
