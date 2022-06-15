d0593cb71499d93daba5cd2f63484302a9048c6f
Georgi <bulbrainn@gmail.com>
2022-06-15 19:27:44 +0300

    Added Workout

---
+++ 01.ProgrammingBasics/16.Exam/04.Workout/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _04.Workout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            double runKms = double.Parse(Console.ReadLine());
+            double totalKms = runKms;
+            double increase = 0;
+
+            for (int i = 0; i < days; i++)
+            {
+                increase = 1 + (double.Parse(Console.ReadLine()) / 100);
+                runKms *= increase;
+                totalKms += runKms;
+            }
+            double diff = Math.Ceiling(Math.Abs(1000 - totalKms));
+            if (totalKms >= 1000)
+            {
+                Console.WriteLine($"You've done a great job running {diff} more kilometers!");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry Mrs. Ivanova, you need to run {diff} more kilometers");
+            }
+        }
+    }
+}
