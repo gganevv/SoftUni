f0111201264e6cc4a57e83fa17828c5a9b863bbc
Georgi <bulbrainn@gmail.com>
2022-06-15 19:26:55 +0300

    Added Dear Of Santa

---
+++ 01.ProgrammingBasics/16.Exam/02.DearOfSanta/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02.DearOfSanta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int daysAway = int.Parse(Console.ReadLine());
+            int foodLeft = int.Parse(Console.ReadLine());
+            double firstDeerFood = double.Parse(Console.ReadLine());
+            double secondDeerFood = double.Parse(Console.ReadLine());
+            double thirdDeerFood = double.Parse(Console.ReadLine());
+
+            double foodNeeded = (firstDeerFood * daysAway) + (secondDeerFood * daysAway) + (thirdDeerFood * daysAway);
+            double diff = Math.Abs(foodNeeded - foodLeft);
+            if (foodLeft >= foodNeeded)
+            {
+                Console.WriteLine($"{Math.Floor(diff)} kilos of food left.");
+            }
+            else
+            {
+                Console.WriteLine($"{Math.Ceiling(diff)} more kilos of food are needed.");
+            }
+        }
+    }
+}
