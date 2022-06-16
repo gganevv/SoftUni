06a352f1810a42806797ae87294bd4e947e4c5ad
Georgi <bulbrainn@gmail.com>
2022-06-16 18:42:39 +0300

    Added Theatre Promotions

---
+++ 02.ProgrammingFundamentals/05.BasicsRecap/07.TheatrePromotions/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _07.TheatrePromotions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dayType = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            int price = 0;
+
+            if (age < 0 || age > 122)
+            {
+                Console.WriteLine("Error!");
+                return;
+            }
+
+            if (dayType == "Weekday")
+            {
+                if ((age >= 0 && age <= 18) || (age > 64 && age <= 122))
+                {
+                    price = 12;
+                }
+                else
+                {
+                    price = 18;
+                }
+            }
+            else if (dayType == "Weekend")
+            {
+                if ((age >= 0 && age <= 18) || (age > 64 && age <= 122))
+                {
+                    price = 15;
+                }
+                else
+                {
+                    price = 20;
+                }
+            }
+            else
+            {
+                if (age >= 0 && age <= 18)
+                {
+                    price = 5;
+                }
+                else if (age <= 64)
+                {
+                    price = 12;
+                }
+                else
+                {
+                    price = 10;
+                }
+            }
+
+            Console.WriteLine(price + "$");
+        }
+    }
+}
