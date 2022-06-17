b7b974229061f052ca9f14ef40e66515c9e370d2
Georgi <bulbrainn@gmail.com>
2022-06-17 19:40:35 +0300

    Added Triangle Of Numbers

---
+++ 02.ProgrammingFundamentals/06.BasicsRecapExercise/08.TriangleOfNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _08.TriangleOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program, which receives a number – n and prints a triangle from 1 to n.
+            
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
