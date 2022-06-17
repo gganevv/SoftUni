43b43bbda9bff305bf2150c5394e98c53da07e7d
Georgi <bulbrainn@gmail.com>
2022-06-17 19:41:36 +0300

    Added Orders

---
+++ 02.ProgrammingFundamentals/06.BasicsRecapExercise/11.Orders/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _11.Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int orders = int.Parse(Console.ReadLine());
+            double totalPrice = 0;
+            for (int i = 0; i < orders; i++)
+            {
+                double capsulePrice = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                int capsulesCount = int.Parse(Console.ReadLine());
+
+                double dayPrice = days * capsulesCount * capsulePrice;
+                Console.WriteLine($"The price for the coffee is: ${dayPrice:f2}");
+                totalPrice += dayPrice;
+            }
+
+            Console.WriteLine($"Total: ${totalPrice:f2}");
+        }
+    }
+}
