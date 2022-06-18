3b3727af5fa5c3d3b382ce767541b941f94dcdec
Georgi <bulbrainn@gmail.com>
2022-06-18 11:20:36 +0300

    Added Messages

---
+++ 02.ProgrammingFundamentals/06.BasicsRecapMoreExercise/05.Messages/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _05.Messages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            string message = "";
+
+            for (int i = 0; i < lines; i++)
+            {
+                string command = Console.ReadLine();
+                if (command == "0")
+                {
+                    message += " ";
+                    continue;
+                }
+                int numberOfDigits = command.Length;
+                int mainDigit = int.Parse(command[0].ToString());
+                int offset = (mainDigit - 2) * 3;
+                if (mainDigit == 8 || mainDigit == 9)
+                {
+                    offset++;
+                }
+                int letterIndex = offset + numberOfDigits - 1;
+                message += (char)('a' + letterIndex);
+            }
+
+            Console.WriteLine(message);
+        }
+    }
+}
