9f4c6c3c4a36015f6d5e6e77c1042fb1792d62f4
Georgi <bulbrainn@gmail.com>
2022-06-18 11:19:54 +0300

    Added Gaming Store

---
+++ 02.ProgrammingFundamentals/06.BasicsRecapMoreExercise/03.GamingStore/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _03.GamingStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            double startingBudget = budget;
+
+            string game;
+            while ((game = Console.ReadLine()) != "Game Time")
+            {
+                double gamePrice = 0;
+                switch (game)
+                {
+                    case "OutFall 4":
+                        gamePrice = 39.99;
+                        break;
+                    case "CS: OG":
+                        gamePrice = 15.99;
+                        break;
+                    case "Zplinter Zell":
+                        gamePrice = 19.99;
+                        break;
+                    case "Honored 2":
+                        gamePrice = 59.99;
+                        break;
+                    case "RoverWatch":
+                        gamePrice = 29.99;
+                        break;
+                    case "RoverWatch Origins Edition":
+                        gamePrice = 39.99;
+                        break;
+                    default:
+                        Console.WriteLine("Not Found");
+                        continue;
+                }
+
+                if (budget < gamePrice)
+                {
+                    Console.WriteLine("Too Expensive");
+                }
+                else
+                {
+                    Console.WriteLine($"Bought {game}");
+                    budget -= gamePrice;
+                }
+
+                if (budget == 0)
+                {
+                    Console.WriteLine("Out of money!");
+                    return;
+                }
+            }
+
+            Console.WriteLine($"Total spent: ${startingBudget - budget:f2}. Remaining: ${budget:f2}");
+        }
+    }
+}
