785c48588b7da873a8fd6a4bb8cc652d75d45e76
Georgi <bulbrainn@gmail.com>
2022-06-18 11:20:16 +0300

    Added Reverse String

---
+++ 02.ProgrammingFundamentals/06.BasicsRecapMoreExercise/04.ReverseString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _04.ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string reversedStr = "";
+            for (int i = str.Length -1; i >= 0; i--)
+            {
+                reversedStr += str[i];
+            }
+
+            Console.WriteLine(reversedStr);
+        }
+    }
+}
