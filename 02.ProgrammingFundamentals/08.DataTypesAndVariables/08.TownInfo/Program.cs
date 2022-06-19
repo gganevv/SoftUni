a9abb0c10577c06102befb4856a277743be7426d
Georgi <bulbrainn@gmail.com>
2022-06-19 10:13:40 +0300

    Added Town Info

---
+++ 02.ProgrammingFundamentals/08.DataTypesAndVariables/08.TownInfo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _08.TownInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string townName = Console.ReadLine();
+            int townPopulation = int.Parse(Console.ReadLine());
+            int townArea = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Town {townName} has population of {townPopulation} and area {townArea} square km.");
+        }
+    }
+}
