432fb2f26986216b9ed3284490d5649ac16dc223
Georgi <bulbrainn@gmail.com>
2022-06-19 10:14:27 +0300

    Added Lower Or Upper

---
+++ 02.ProgrammingFundamentals/08.DataTypesAndVariables/10.LowerOrUpper/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _10.LowerOrUpper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ch = Console.ReadLine();
+            if (ch.ToLower() == ch)
+            {
+                Console.WriteLine("lower-case");
+            }
+            else
+            {
+                Console.WriteLine("upper-case");
+            }
+        }
+    }
+}
