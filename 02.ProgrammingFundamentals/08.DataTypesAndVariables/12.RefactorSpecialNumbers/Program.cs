96654d5be22d6d60b4c55388385092fd56e1ced3
Georgi <bulbrainn@gmail.com>
2022-06-19 10:15:16 +0300

    Added Refactor Special Numbers

---
+++ 02.ProgrammingFundamentals/08.DataTypesAndVariables/12.RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _12.RefactorSpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            for (int n = 1; n <= count; n++)
+            {
+                int sum = 0;
+                int currentNum = n;
+                while (currentNum > 0)
+                {
+                    sum += currentNum % 10;
+                    currentNum /= 10;
+                }
+                bool isSpecial = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine("{0} -> {1}", n, isSpecial);
+            }
+
+        }
+    }
+}
