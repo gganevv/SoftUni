3950476494fdaa326bec3615a60c6fb9f290a3bd
Georgi <bulbrainn@gmail.com>
2022-06-19 10:11:09 +0300

    Added Pound To Dollar

---
+++ 02.ProgrammingFundamentals/08.DataTypesAndVariables/02.PoundToDollars/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _02.PoundToDollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pounds = double.Parse(Console.ReadLine());
+            double dollars = pounds * 1.31;
+            Console.WriteLine($"{dollars:f3}");
+        }
+    }
+}
