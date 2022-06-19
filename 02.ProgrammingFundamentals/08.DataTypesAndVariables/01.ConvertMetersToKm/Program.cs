8359e2b218844d74587be5f0c63dfe3ddd4ab744
Georgi <bulbrainn@gmail.com>
2022-06-19 10:10:42 +0300

    Added Convert Meters To Km

---
+++ 02.ProgrammingFundamentals/08.DataTypesAndVariables/01.ConvertMetersToKm/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01.ConvertMetersToKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int meters = int.Parse(Console.ReadLine());
+            double kms = meters / 1000.0;
+            Console.WriteLine($"{kms:f2}");
+        }
+    }
+}
