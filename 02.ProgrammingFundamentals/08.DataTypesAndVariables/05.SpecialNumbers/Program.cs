9dcd2c0f458477a10eeff0488170b52f1746eaee
Georgi <bulbrainn@gmail.com>
2022-06-19 10:12:33 +0300

    Added Special Number

---
+++ 02.ProgrammingFundamentals/08.DataTypesAndVariables/05.SpecialNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _05.SpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                int currentNum = i;
+                int sum = 0;
+                while (currentNum > 0)
+                {
+                    sum += currentNum % 10;
+                    currentNum /= 10;
+                }
+                bool checkNum = sum == 5 || sum == 7 || sum == 11;
+                Console.WriteLine($"{i} -> {checkNum}");
+            }
+        }
+    }
+}
