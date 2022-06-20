fa47e0ee02eb6a3e8f17ee72bf2599e738deaf65
Georgi <bulbrainn@gmail.com>
2022-06-20 19:33:48 +0300

    Added PokeMon

---
+++ 02.ProgrammingFundamentals/09.DataTypesAndVariablesExercise/10.PokeMon/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _10.PokeMon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pokePower = int.Parse(Console.ReadLine());
+            double halfStartingPokePower = pokePower / 2.0;
+            int targetDistance = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+            int pokesCount = 0;
+
+            while (pokePower >= targetDistance)
+            {
+                pokePower -= targetDistance;
+                pokesCount++;
+                if (pokePower == halfStartingPokePower && exhaustionFactor > 0)
+                {
+                    pokePower /= exhaustionFactor;
+                }
+            }
+
+            Console.WriteLine(pokePower);
+            Console.WriteLine(pokesCount);
+        }
+    }
+}
