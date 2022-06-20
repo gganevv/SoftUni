646a437f59cbdaa9a4b03451a3196ec4805fdbb8
Georgi <bulbrainn@gmail.com>
2022-06-20 19:30:02 +0300

    Added Elevator

---
+++ 02.ProgrammingFundamentals/09.DataTypesAndVariablesExercise/03.Elevator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _03.Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            int elevatorCapacity = int.Parse(Console.ReadLine());
+            double courses = Math.Ceiling((double)numberOfPeople / elevatorCapacity);
+            Console.WriteLine(courses);
+        }
+    }
+}
