a70d3694f7c9925c4cd2ab4e3800d06a5ecc5c9a
Georgi <bulbrainn@gmail.com>
2022-06-20 19:29:40 +0300

    Added Sum Digits

---
+++ 02.ProgrammingFundamentals/09.DataTypesAndVariablesExercise/02.SumDigits/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _02.SumDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            int sum = 0;
+            for (int i = 0; i < n.Length; i++)
+            {
+                sum += int.Parse(n[i].ToString());
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
