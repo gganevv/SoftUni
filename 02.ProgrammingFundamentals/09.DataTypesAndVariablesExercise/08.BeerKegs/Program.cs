99e075ce46def5f8986fb3a6ad17afd727cf2af1
Georgi <bulbrainn@gmail.com>
2022-06-20 19:33:01 +0300

    Added Beer Kegs

---
+++ 02.ProgrammingFundamentals/09.DataTypesAndVariablesExercise/08.BeerKegs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _08.BeerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfKegs = int.Parse(Console.ReadLine());
+            double bestKegVolume = int.MinValue;
+            string bestKegModel = "";
+
+            for (int i = 0; i < numberOfKegs; i++)
+            {
+                string currentKegModel = Console.ReadLine();
+                double currentKegRadius = double.Parse(Console.ReadLine());
+                int currentKegHeight = int.Parse(Console.ReadLine());
+
+                double currentKegVolume = Math.PI * Math.Pow(currentKegRadius, 2) * currentKegHeight;
+                if (currentKegVolume > bestKegVolume)
+                {
+                    bestKegVolume = currentKegVolume;
+                    bestKegModel = currentKegModel;
+                }
+            }
+
+            Console.WriteLine(bestKegModel);
+        }
+    }
+}
