6ffbecebb7384bbad6ebdc2bad7967623b1dec4c
Georgi <bulbrainn@gmail.com>
2022-06-20 19:30:27 +0300

    Added Sum Of Chars

---
+++ 02.ProgrammingFundamentals/09.DataTypesAndVariablesExercise/04.SumOfChars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04.SumOfChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+            int digitsSum = 0;
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                char c = char.Parse(Console.ReadLine());
+                digitsSum += c;
+            }
+
+            Console.WriteLine($"The sum equals: {digitsSum}");
+        }
+    }
+}
