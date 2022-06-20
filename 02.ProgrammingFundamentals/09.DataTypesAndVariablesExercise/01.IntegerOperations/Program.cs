3d77773e078e68497934843a7f4f1d22c3711770
Georgi <bulbrainn@gmail.com>
2022-06-20 19:29:01 +0300

    Added Integer Operations

---
+++ 02.ProgrammingFundamentals/09.DataTypesAndVariablesExercise/01.IntegerOperations/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01.IntegerOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            int fourtNum = int.Parse(Console.ReadLine());
+
+            int result = (firstNum + secondNum) / thirdNum * fourtNum;
+            Console.WriteLine(result);
+        }
+    }
+}
