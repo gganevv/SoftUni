a786e4c1bf2f271bbc78fc44814f1009dbd0090c
Georgi <bulbrainn@gmail.com>
2022-06-20 19:32:33 +0300

    Added Water Overflow

---
+++ 02.ProgrammingFundamentals/09.DataTypesAndVariablesExercise/07.WaterOverFlow/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _07.WaterOverFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int TANK_CAPACITY = 255;
+
+            int filledLiters = 0;
+            int numberOfLines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                int currentFill = int.Parse(Console.ReadLine());
+                if (currentFill + filledLiters > TANK_CAPACITY)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+                else
+                {
+                    filledLiters += currentFill;
+                }
+            }
+
+            Console.WriteLine(filledLiters);
+        }
+    }
+}
