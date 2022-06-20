e39c899795e8070bac8c9c28ac8e7a965c72c17d
Georgi <bulbrainn@gmail.com>
2022-06-20 19:33:26 +0300

    Added Spice Must Flow

---
+++ 02.ProgrammingFundamentals/09.DataTypesAndVariablesExercise/09.SpiceMustFlow/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _09.SpiceMustFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MINNIMUM_SPICE = 100;
+            const int SPICE_DECREESE = 10;
+            const int CONSUMED_SPICE_BY_WORKERS = 26;
+
+            int mineSpice = int.Parse(Console.ReadLine());
+            int gatheredSpice = 0;
+            int daysCount = 0;
+
+            while (mineSpice >= MINNIMUM_SPICE)
+            {
+                gatheredSpice += mineSpice - CONSUMED_SPICE_BY_WORKERS;
+                daysCount++;
+                mineSpice -= SPICE_DECREESE;
+            }
+
+            if (gatheredSpice >= CONSUMED_SPICE_BY_WORKERS)
+            {
+                gatheredSpice -= CONSUMED_SPICE_BY_WORKERS;
+            }
+
+            Console.WriteLine(daysCount);
+            Console.WriteLine(gatheredSpice);
+        }
+    }
+}
