50c86e67958770d249387cdb9b1a513c55ed64a1
Georgi <bulbrainn@gmail.com>
2022-06-21 20:05:25 +0300

    Added Data Type Finder

---
+++ 02.ProgrammingFundamentals/09.DataTypesAndVariablesMoreExercise/01.DataTypeFinder/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _01.DataTypeFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                if (int.TryParse(input, out int intValue))
+                {
+                    Console.WriteLine($"{input} is integer type");
+                }
+                else if (double.TryParse(input, out double doubleValue))
+                {
+                    Console.WriteLine($"{input} is floating point type");
+                }
+                else if (char.TryParse(input, out char charValue))
+                {
+                    Console.WriteLine($"{input} is character type");
+                }
+                else if (bool.TryParse(input, out bool boolValue))
+                {
+                    Console.WriteLine($"{input} is boolean type");
+                }
+                else
+                {
+                    Console.WriteLine($"{input} is string type");
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
