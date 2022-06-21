aeb737b73f97712e1f31227be2c63b72147edeb2
Georgi <bulbrainn@gmail.com>
2022-06-21 20:06:37 +0300

    Added Refactor Prime Checker

---
+++ 02.ProgrammingFundamentals/09.DataTypesAndVariablesMoreExercise/04.RefactorPrimeChecker/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _04.RefactorPrimeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lastNum = int.Parse(Console.ReadLine());
+
+            for (int num = 2; num <= lastNum; num++)
+            {
+                bool isPrime = true;
+                for (int divisible = 2; divisible < num; divisible++)
+                {
+                    if (num % divisible == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                Console.WriteLine($"{num} -> {isPrime.ToString().ToLower()}");
+            }
+        }
+    }
+}
