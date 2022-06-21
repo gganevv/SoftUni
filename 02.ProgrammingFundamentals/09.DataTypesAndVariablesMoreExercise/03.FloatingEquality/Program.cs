1e8e6d026d6b5f61f3d509a9eba19f1dab9ee2f6
Georgi <bulbrainn@gmail.com>
2022-06-21 20:06:14 +0300

    Added Floating Equality

---
+++ 02.ProgrammingFundamentals/09.DataTypesAndVariablesMoreExercise/03.FloatingEquality/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _03.FloatingEquality
+{
+    class Program
+    {
+        private const decimal EPS = 0.000001M;
+        static void Main(string[] args)
+        {
+            decimal firstNum = decimal.Parse(Console.ReadLine());
+            decimal secondNum = decimal.Parse(Console.ReadLine());
+
+            decimal diff = Math.Abs(firstNum - secondNum);
+            if (diff >= EPS)
+            {
+                Console.WriteLine(false);
+            }
+            else
+            {
+                Console.WriteLine(true);
+            }
+        }
+    }
+}
