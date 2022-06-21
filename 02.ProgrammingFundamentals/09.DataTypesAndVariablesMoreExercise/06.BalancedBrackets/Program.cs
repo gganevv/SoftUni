6361bec94b30722d82cd12ec05ead8aaf3b27d34
Georgi <bulbrainn@gmail.com>
2022-06-21 20:07:26 +0300

    Added Balanced Brackets

---
+++ 02.ProgrammingFundamentals/09.DataTypesAndVariablesMoreExercise/06.BalancedBrackets/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _06.BalancedBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+            int openCount = 0;
+            int closeCount = 0;
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                string line = Console.ReadLine();
+                if (line == "(")
+                {
+                    openCount++;
+                }
+                else if (line == ")")
+                {
+                    closeCount++;
+                    if (openCount - closeCount != 0)
+                    {
+                        Console.WriteLine("UNBALANCED");
+                        return;
+                    }
+                }
+            }
+
+            if (openCount == closeCount)
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+        }
+    }
+}
