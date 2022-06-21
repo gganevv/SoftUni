0379c19fe85515404ddfc4a3f6f2bf6d6b13dd12
Georgi <bulbrainn@gmail.com>
2022-06-21 20:07:00 +0300

    Added Decrypting Messages

---
+++ 02.ProgrammingFundamentals/09.DataTypesAndVariablesMoreExercise/05.DecryptingMessages/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _05.DecryptingMessages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            int numberOfCharacters = int.Parse(Console.ReadLine());
+            string message = "";
+            for (int i = 0; i < numberOfCharacters; i++)
+            {
+                message += (char)(char.Parse(Console.ReadLine()) + key);
+            }
+
+            Console.WriteLine(message);
+        }
+    }
+}
