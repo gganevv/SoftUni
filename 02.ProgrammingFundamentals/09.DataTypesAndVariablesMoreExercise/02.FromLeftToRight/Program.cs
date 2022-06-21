3c078f0c84353256395d5a9306fcaaf3e17f9b0d
Georgi <bulbrainn@gmail.com>
2022-06-21 20:05:51 +0300

    Added From Left To Right

---
+++ 02.ProgrammingFundamentals/09.DataTypesAndVariablesMoreExercise/02.FromLeftToRight/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _02.FromLeftToRight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                string[] numbers = Console.ReadLine().Split(" ");
+                long firstNum = long.Parse(numbers[0]);
+                long secondNum = long.Parse(numbers[1]);
+                long sum = 0;
+                long biggerNum = Math.Abs(firstNum > secondNum ? firstNum : secondNum);
+                while (biggerNum > 0)
+                {
+                    sum += biggerNum % 10;
+                    biggerNum /= 10;
+                }
+
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
