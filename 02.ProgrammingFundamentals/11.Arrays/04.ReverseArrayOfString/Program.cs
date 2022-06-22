a958a33321af76980a98b266eb385bdd3ece3fdc
Georgi <bulbrainn@gmail.com>
2022-06-22 19:18:27 +0300

    Added Reverse Array Of String

---
+++ 02.ProgrammingFundamentals/11.Arrays/04.ReverseArrayOfString/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.ReverseArrayOfString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split().ToArray();
+            for (int i = str.Length -1; i >= 0; i--)
+            {
+                Console.Write(str[i] + " ");
+            }
+        }
+    }
+}
