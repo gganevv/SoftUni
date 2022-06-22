580c6d98a7857a1bc8abd441e1e269910a9780e8
Georgi <bulbrainn@gmail.com>
2022-06-22 19:17:11 +0300

    Added Day Of Week

---
+++ 02.ProgrammingFundamentals/11.Arrays/01.DayOfWeek/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01.DayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+            int day = int.Parse(Console.ReadLine());
+
+            if (day < 1 || day > 7)
+            {
+                Console.WriteLine("Invalid day!");
+            }
+            else
+            {
+                Console.WriteLine(days[day -1]);
+            }
+        }
+    }
+}
