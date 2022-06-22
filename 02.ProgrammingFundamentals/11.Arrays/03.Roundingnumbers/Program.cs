7fce795c167aaf6c1a6b5d0c9febc3640fa9eec1
Georgi <bulbrainn@gmail.com>
2022-06-22 19:18:03 +0300

    Added Rounding Numbers

---
+++ 02.ProgrammingFundamentals/11.Arrays/03.Roundingnumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Roundingnumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nums = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            foreach (double num in nums)
+            {
+                Console.WriteLine($"{num} => {(int)Math.Round(num, MidpointRounding.AwayFromZero)}");
+            }
+        }
+    }
+}
