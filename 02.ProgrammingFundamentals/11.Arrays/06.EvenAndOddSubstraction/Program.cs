8fda8d30099e2fdc9732a3a2067e0ea15b850795
Georgi <bulbrainn@gmail.com>
2022-06-22 19:19:19 +0300

    Added Even And Odd Substraction

---
+++ 02.ProgrammingFundamentals/11.Arrays/06.EvenAndOddSubstraction/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.EvenAndOddSubstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int evenSum = 0;
+            int oddSum = 0;
+
+            foreach (int n in nums)
+            {
+                if (n % 2 == 0)
+                {
+                    evenSum += n;
+                }
+                else
+                {
+                    oddSum += n;
+                }
+            }
+
+            Console.WriteLine(evenSum - oddSum);
+        }
+    }
+}
