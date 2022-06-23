0ef2bb66763d847c3cade1c1eff27455f1642322
Georgi <bulbrainn@gmail.com>
2022-06-23 21:08:17 +0300

    Added Max Sequence Of Equal Elements

---
+++ 02.ProgrammingFundamentals/12.ArraysExercise/07.MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.MaxSequenceOfEqualElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int maxSecuence = 0;
+            int sequenceName = 0;
+            int counter = 1;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    counter++;
+                }
+                else
+                {
+                    if (counter > maxSecuence)
+                    {
+                        maxSecuence = counter;
+                        sequenceName = numbers[i];
+                    }
+                    counter = 1;
+                }
+            }
+            if (counter > maxSecuence)
+            {
+                maxSecuence = counter;
+                sequenceName = numbers[numbers.Length - 1];
+            }
+
+            for (int i = 0; i < maxSecuence; i++)
+            {
+                Console.Write(sequenceName + " ");
+            }
+        }
+    }
+}
