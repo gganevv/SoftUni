229852a2a06a558abcbd63a10ca1881e16f30866
Georgi <bulbrainn@gmail.com>
2022-06-23 21:05:59 +0300

    Added Train

---
+++ 02.ProgrammingFundamentals/12.ArraysExercise/01.Train/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] asd = { 1, 5325, 7 };
+
+            Console.WriteLine(asd.Min());
+
+
+
+
+
+            int numberOfWagons = int.Parse(Console.ReadLine());
+            int[] wagons = new int[numberOfWagons];
+
+            
+            for (int i = 0; i < numberOfWagons; i++)
+            {
+                wagons[i] = int.Parse(Console.ReadLine());
+            }
+
+            foreach (int passangers in wagons)
+            {
+                Console.Write(passangers + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(wagons.Sum());
+        }
+    }
+}
