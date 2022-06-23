2d9e92c66a1680d213890ee6b7bcb50a1848c27d
Georgi <bulbrainn@gmail.com>
2022-06-23 21:07:29 +0300

    Added Top Integers

---
+++ 02.ProgrammingFundamentals/12.ArraysExercise/05.TopIntegers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.TopIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                bool topInt = true;
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] <= nums[j])
+                    {
+                        topInt = false;
+                        break;
+                    }
+                }
+                if (topInt)
+                {
+                    Console.Write(nums[i] + " ");
+                }
+            }
+        }
+    }
+}
