08357e3f0329dac5663b262a3487bef68c34bff3
Georgi <bulbrainn@gmail.com>
2022-06-23 21:08:36 +0300

    Added Magic Sum

---
+++ 02.ProgrammingFundamentals/12.ArraysExercise/08.MagicSum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.MagicSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int magicSum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    int firstNum = nums[i];
+                    int secondNum = nums[j];
+                    if (firstNum + secondNum == magicSum)
+                    {
+                        Console.WriteLine($"{firstNum} {secondNum}");
+                    }
+                }
+            }
+        }
+    }
+}
