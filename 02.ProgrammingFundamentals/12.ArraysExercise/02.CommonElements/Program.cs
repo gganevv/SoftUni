21b1f2bee89d03f8efda1530f3e126b5e7849bb6
Georgi <bulbrainn@gmail.com>
2022-06-23 21:06:22 +0300

    Added Common Elements

---
+++ 02.ProgrammingFundamentals/12.ArraysExercise/02.CommonElements/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _02.CommonElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArr = Console.ReadLine().Split();
+            string[] secondArr = Console.ReadLine().Split();
+
+
+            for (int i = 0; i < secondArr.Length; i++)
+            {
+                for (int j = 0; j < firstArr.Length; j++)
+                {
+                    if (secondArr[i] == firstArr[j])
+                    {
+                        Console.Write(secondArr[i] + " ");
+                    }
+                }
+            }
+        }
+    }
+}
