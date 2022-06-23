83ec4ffcb3078a9667748a0d0f086a26e07f7d10
Georgi <bulbrainn@gmail.com>
2022-06-23 21:06:47 +0300

    Added Zig Zag Arrays

---
+++ 02.ProgrammingFundamentals/12.ArraysExercise/03.ZigZagArrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.ZigZagArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+            int[] firstArr = new int[numberOfLines];
+            int[] secondArr = new int[numberOfLines];
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                int[] tempArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                if (i % 2 == 0)
+                {
+                    firstArr[i] = tempArray[0];
+                    secondArr[i] = tempArray[1];
+                }
+                else
+                {
+                    firstArr[i] = tempArray[1];
+                    secondArr[i] = tempArray[0];
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", firstArr));
+            Console.WriteLine(String.Join(" ", secondArr));
+        }
+    }
+}
