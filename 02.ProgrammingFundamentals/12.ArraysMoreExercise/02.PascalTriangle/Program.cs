b0bd17f2773996962e52b051e2c5277d688183d7
Georgi <bulbrainn@gmail.com>
2022-06-24 20:02:50 +0300

    Added Pascal Triangle

---
+++ 02.ProgrammingFundamentals/12.ArraysMoreExercise/02.PascalTriangle/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.PascalTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            List<int> prevNums = new List<int>();
+            List<int> currentNums = new List<int>();
+            for (int line = 1; line <= lines; line++)
+            {
+                
+                for (int n = 1; n <= line; n++)
+                {
+                    if (n > 1 && n <= prevNums.Count)
+                    {
+                        int currentNum = prevNums[n - 2] + prevNums[n - 1];
+                        currentNums.Add(currentNum);
+                        Console.Write(currentNum + " ");
+                    }
+                    else
+                    {
+                        currentNums.Add(1);
+                        Console.Write(1 + " ");
+                    }
+                    
+                }
+                prevNums.Clear();
+                prevNums.AddRange(currentNums);
+                currentNums.Clear();
+                Console.WriteLine();
+            }
+        }
+    }
+}
