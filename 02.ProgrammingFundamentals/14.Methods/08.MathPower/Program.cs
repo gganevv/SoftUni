792b74ace5d8cbcb3ccfda82a47be4d7a689fedb
Georgi <bulbrainn@gmail.com>
2022-06-29 00:07:32 +0300

    Added Math Power

---
+++ 02.ProgrammingFundamentals/14.Methods/08.MathPower/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _08.MathPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseNum = double.Parse(Console.ReadLine());
+            double powerNum = double.Parse(Console.ReadLine());
+
+            double result = CalculatePower(baseNum, powerNum);
+            Console.WriteLine(result);
+        }
+
+        private static double CalculatePower(double baseNum, double powerNum)
+        {
+            double sum = 1;
+            for (int i = 0; i < powerNum; i++)
+            {
+                sum *= baseNum;
+            }
+
+            return sum;
+        }
+    }
+}
