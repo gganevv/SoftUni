169917a65129f861f1fd61a18aff35af67c461f3
Georgi <bulbrainn@gmail.com>
2022-06-29 00:05:57 +0300

    Added Printing Triangle

---
+++ 02.ProgrammingFundamentals/14.Methods/04.PrintingTriangle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _04.PrintingTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < max; i++)
+            {
+                PrintLine(1, i);
+            }
+
+            for (int i = max; i > 0; i--)
+            {
+                PrintLine(1, i);
+            }
+        }
+
+        private static void PrintLine(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
