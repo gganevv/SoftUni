e9b449b966537e992a2b8217de4f45ab04e21315
Georgi <bulbrainn@gmail.com>
2022-06-28 23:44:01 +0300

    Added Sign Of Integer Numbers

---
+++ 02.ProgrammingFundamentals/14.Methods/01.SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01.SignOfIntegerNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            string result = CheckNumber(number);
+            Console.WriteLine(result);
+        }
+
+        private static string CheckNumber(int number)
+        {
+            if (number > 0)
+            {
+                return $"The number {number} is positive. ";
+            }
+            else if (number < 0)
+            {
+                return $"The number {number} is negative. ";
+            }
+            else
+            {
+                return $"The number {number} is zero. ";
+            }
+        }
+    }
+}
