2d3c2cf7975ab9a7b4b8b619d0ba175145217ae5
Georgi <bulbrainn@gmail.com>
2022-06-29 00:08:54 +0300

    Added Math Operations

---
+++ 02.ProgrammingFundamentals/14.Methods/11.MathOperations/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _11.MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNum = int.Parse(Console.ReadLine());
+            string operat = Console.ReadLine();
+            double secondNum = int.Parse(Console.ReadLine());
+
+            double result = Calculate(firstNum, operat, secondNum);
+            Console.WriteLine(result);
+        }
+
+        private static double Calculate(double firstNum, string operat, double secondNum)
+        {
+            double result = 0;
+            switch (operat)
+            {
+                case "/":
+                    result = firstNum / secondNum;
+                    break;
+                case "*":
+                    result = firstNum * secondNum;
+                    break;
+                case "+":
+                    result = firstNum + secondNum;
+                    break;
+                case "-":
+                    result = firstNum - secondNum;
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
