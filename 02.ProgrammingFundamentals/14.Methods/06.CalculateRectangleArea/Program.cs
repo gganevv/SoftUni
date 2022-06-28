5d35b45cb591c3668c1f0d68c28a1934ee20dcee
Georgi <bulbrainn@gmail.com>
2022-06-29 00:06:37 +0300

    Added Calculate Recatangle Area

---
+++ 02.ProgrammingFundamentals/14.Methods/06.CalculateRectangleArea/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _06.CalculateRectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double w = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            double result = CalculateArea(w, h);
+            Console.WriteLine(result);
+        }
+
+        public static double CalculateArea(double w, double h) => w * h;
+    }
+}
