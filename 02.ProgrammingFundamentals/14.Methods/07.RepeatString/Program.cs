c3edf5cd16a1397ac40334c30414db5c86c640ad
Georgi <bulbrainn@gmail.com>
2022-06-29 00:07:06 +0300

    Added Repeat String

---
+++ 02.ProgrammingFundamentals/14.Methods/07.RepeatString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace _07.RepeatString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            string result = RepeatStr(str, n);
+            Console.WriteLine(result);
+        }
+
+        private static string RepeatStr(string str, int n)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < n; i++)
+            {
+                sb.Append(str);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
