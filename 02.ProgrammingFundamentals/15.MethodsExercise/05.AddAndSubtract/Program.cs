ca7ad7282c466a4cbe099148648f08a37ca41c31
Georgi <bulbrainn@gmail.com>
2022-06-29 22:59:59 +0300

    Added Add And Subtract

---
+++ 02.ProgrammingFundamentals/15.MethodsExercise/05.AddAndSubtract/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _05.AddAndSubtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+
+            int addResult = Add(firstNum, secondNum);
+            int subtractResult = Subtract(addResult, thirdNum);
+            Console.WriteLine(subtractResult);
+        }
+
+        private static int Subtract(int addResult, int thirdNum) => addResult - thirdNum;
+        private static int Add(int firstNum, int secondNum) => firstNum + secondNum;
+    }
+}
