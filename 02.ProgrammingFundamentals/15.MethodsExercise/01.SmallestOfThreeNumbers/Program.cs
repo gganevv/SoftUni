07f2612cc0b22dd3a91cac983e8363010bc830dc
Georgi <bulbrainn@gmail.com>
2022-06-29 22:58:24 +0300

    Added Smallest Of Three Numbers

---
+++ 02.ProgrammingFundamentals/15.MethodsExercise/01.SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01.SmallestOfThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+
+            PrintSmallestNum(firstNum, secondNum, thirdNum);
+        }
+
+        private static void PrintSmallestNum(int firstNum, int secondNum, int thirdNum)
+        {
+            Console.WriteLine(Math.Min(firstNum, Math.Min(secondNum, thirdNum)));
+        }
+    }
+}
