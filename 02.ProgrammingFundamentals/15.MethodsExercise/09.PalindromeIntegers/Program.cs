5ae99e4562f983e74ff49a0c64f4ab1acd1a87ae
Georgi <bulbrainn@gmail.com>
2022-06-29 23:01:31 +0300

    Added Palindrome Integers

---
+++ 02.ProgrammingFundamentals/15.MethodsExercise/09.PalindromeIntegers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _09.PalindromeIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                bool palindrome = CheckPalindrome(command);
+                Console.WriteLine(palindrome.ToString().ToLower());
+
+                command = Console.ReadLine();
+            }
+        }
+
+        private static bool CheckPalindrome(string num)
+        {
+            if (num.Length < 2)
+            {
+                return true;
+            }
+
+            if (num[0] == num[num.Length - 1])
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
