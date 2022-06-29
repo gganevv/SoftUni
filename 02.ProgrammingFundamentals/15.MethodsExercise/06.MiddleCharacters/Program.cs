2736208c567d832d0aa0922fa7341f5d3e0f74c8
Georgi <bulbrainn@gmail.com>
2022-06-29 23:00:21 +0300

    Added Middle Characters

---
+++ 02.ProgrammingFundamentals/15.MethodsExercise/06.MiddleCharacters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _06.MiddleCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            PrintMiddle(str);
+        }
+
+        private static void PrintMiddle(string str)
+        {
+            if (str.Length % 2 == 0)
+            {
+                Console.Write(str[str.Length / 2 - 1]);
+                Console.WriteLine(str[str.Length / 2]);
+            }
+            else
+            {
+                Console.WriteLine(str[str.Length / 2]);
+            }
+        }
+    }
+}
