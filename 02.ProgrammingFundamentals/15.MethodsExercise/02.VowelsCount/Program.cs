53df4cc5f0bd0415a9c7a096accb782b30af9979
Georgi <bulbrainn@gmail.com>
2022-06-29 22:58:49 +0300

    Added Vowels Count

---
+++ 02.ProgrammingFundamentals/15.MethodsExercise/02.VowelsCount/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02.VowelsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine().ToLower();
+            PrintVowels(word);
+        }
+
+        private static void PrintVowels(string word)
+        {
+            int counter = 0;
+            foreach (char ch in word)
+            {
+                if ("aeiou".Contains(ch))
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
