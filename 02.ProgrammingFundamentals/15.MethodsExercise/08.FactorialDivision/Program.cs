f318adc55044170f20127ce53305e4ce7c17bfe9
Georgi <bulbrainn@gmail.com>
2022-06-29 23:01:04 +0300

    Added Factorial Division

---
+++ 02.ProgrammingFundamentals/15.MethodsExercise/08.FactorialDivision/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _08.FactorialDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            double firstResult = GetFactorial(firstNum);
+            double secondResult = GetFactorial(secondNum);
+
+            double result = firstResult / secondResult;
+            Console.WriteLine($"{result:f2}");
+        }
+
+        private static double GetFactorial(int num)
+        {
+            double result = 1;
+            while (num != 1)
+            {
+                result *= num;
+                num--;
+            }
+            return result;
+        }
+    }
+}
