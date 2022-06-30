810ad22231d456adc46e09e736049cd4936219a9
Georgi <bulbrainn@gmail.com>
2022-06-30 21:37:19 +0300

    Added Multiplication Sign

---
+++ 02.ProgrammingFundamentals/15.MethodsMoreExercise/05.MultiplicationSign/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _05.MultiplicationSign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+
+            string result = FindSign(firstNum, secondNum, thirdNum);
+            Console.WriteLine(result);
+        }
+
+        private static string FindSign(int firstNum, int secondNum, int thirdNum)
+        {
+            int result = firstNum * secondNum * thirdNum;
+            if (result < 0)
+            {
+                return "negative";
+            }
+            else if (result > 0)
+            {
+                return "positive";
+            }
+            else
+            {
+                return "zero";
+            }
+        }
+    }
+}
