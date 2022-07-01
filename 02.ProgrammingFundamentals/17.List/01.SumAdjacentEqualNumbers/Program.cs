07ef51442ce83a10ea62caa7a47f1b7cb2a182e5
Georgi <bulbrainn@gmail.com>
2022-07-01 18:29:19 +0300

    Added Sum Adjacent Equal Numbers

---
+++ 02.ProgrammingFundamentals/17.List/01.SumAdjacentEqualNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.SumAdjacentEqualNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<double> numbers = Console.ReadLine().Split().Select(double.Parse).ToList();
+
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    numbers[i] *= 2;
+                    numbers.RemoveAt(i + 1);
+                    i = -1;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
