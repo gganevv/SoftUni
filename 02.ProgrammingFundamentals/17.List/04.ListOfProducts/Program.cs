558f1b0470225650e2d293164bb46f0909f3fccc
Georgi <bulbrainn@gmail.com>
2022-07-01 18:30:23 +0300

    Added List Of Products

---
+++ 02.ProgrammingFundamentals/17.List/04.ListOfProducts/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.ListOfProducts
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> products = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                products.Add(Console.ReadLine());
+            }
+
+            products.Sort();
+
+            int counter = 1;
+            foreach (var product in products)
+            {
+                Console.WriteLine($"{counter}.{product}");
+                counter++;
+            }
+        }
+    }
+}
