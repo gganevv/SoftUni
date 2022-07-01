03b58f3f28c48373d35bcc22fc93ef8bc3d3f50e
Georgi <bulbrainn@gmail.com>
2022-07-01 18:29:43 +0300

    Added Gauss Trick

---
+++ 02.ProgrammingFundamentals/17.List/02.GaussTrick/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.GaussTrick
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int originalCount = numbers.Count;
+
+            for (int i = 0; i < originalCount / 2; i++)
+            {
+                numbers[i] += numbers[originalCount - i - 1];
+                numbers.RemoveAt(originalCount - i - 1);
+
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
