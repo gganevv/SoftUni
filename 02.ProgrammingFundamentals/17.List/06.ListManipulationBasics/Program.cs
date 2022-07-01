82652d828434f3408561e95720841f7b0515fce0
Georgi <bulbrainn@gmail.com>
2022-07-01 18:31:08 +0300

    Added List Manipulation Basics

---
+++ 02.ProgrammingFundamentals/17.List/06.ListManipulationBasics/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.ListManipulationBasics
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] commandArgs = input.Split();
+                string command = commandArgs[0];
+                switch (command)
+                {
+                    case "Add":
+                        int numberToAdd = int.Parse(commandArgs[1]);
+                        numbers.Add(numberToAdd);
+                        break;
+                    case "Remove":
+                        int numberToRemove = int.Parse(commandArgs[1]);
+                        numbers.Remove(numberToRemove);
+                        break;
+                    case "RemoveAt":
+                        int indexToRemove = int.Parse(commandArgs[1]);
+                        numbers.RemoveAt(indexToRemove);
+                        break;
+                    case "Insert":
+                        int numberToInsert = int.Parse(commandArgs[1]);
+                        int indexToInsert = int.Parse(commandArgs[2]);
+                        numbers.Insert(indexToInsert, numberToInsert);
+                        break;
+                    default:
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+
+        }
+    }
+}
