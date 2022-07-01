f381df2d0d8ba67e564a6c21031967891d93db74
Georgi <bulbrainn@gmail.com>
2022-07-01 18:30:03 +0300

    Added Merging Lists

---
+++ 02.ProgrammingFundamentals/17.List/03.MergingLists/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.MergingLists
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> firstNumbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> secondNumbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> finalNumbers = new List<int>();
+
+            int minLength = Math.Min(firstNumbers.Count, secondNumbers.Count);
+            for (int i = 0; i < minLength; i++)
+            {
+                finalNumbers.Add(firstNumbers[i]);
+                finalNumbers.Add(secondNumbers[i]);
+            }
+
+            if (firstNumbers.Count > secondNumbers.Count)
+            {
+                for (int i = minLength; i < firstNumbers.Count; i++)
+                {
+                    finalNumbers.Add(firstNumbers[i]);
+                }
+            }
+            else if (secondNumbers.Count > firstNumbers.Count)
+            {
+                for (int i = minLength; i < secondNumbers.Count; i++)
+                {
+                    finalNumbers.Add(secondNumbers[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", finalNumbers));
+        }
+    }
+}
