ff0d9f771ca6f6589b71b17afb98b591225d9eaf
Georgi <bulbrainn@gmail.com>
2022-07-02 20:57:01 +0300

    Added House Party

---
+++ 02.ProgrammingFundamentals/18.ListExercise/03.HouseParty/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.HouseParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCommands = int.Parse(Console.ReadLine());
+            List<string> guests = new List<string>();
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string name = input[0];
+                string command = input[2];
+                bool isGoing = command == "going!";
+
+                if (isGoing)
+                {
+                    if (guests.Contains(name))
+                    {
+                        Console.WriteLine($"{name} is already in the list!");
+                        continue;
+                    }
+                    guests.Add(name);
+                }
+                else
+                {
+                    if (!guests.Contains(name))
+                    {
+                        Console.WriteLine($"{name} is not in the list!");
+                        continue;
+                    }
+                    guests.Remove(name);
+                }
+            }
+
+            foreach (string guest in guests)
+            {
+                Console.WriteLine(guest);
+            }
+        }
+    }
+}
