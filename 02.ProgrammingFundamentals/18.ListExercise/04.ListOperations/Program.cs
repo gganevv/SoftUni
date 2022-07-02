3a12cf686eab134a08383d57adccd7802a85518d
Georgi <bulbrainn@gmail.com>
2022-07-02 20:57:23 +0300

    Added List Operations

---
+++ 02.ProgrammingFundamentals/18.ListExercise/04.ListOperations/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.ListOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        
+
+        
+    }
+}
