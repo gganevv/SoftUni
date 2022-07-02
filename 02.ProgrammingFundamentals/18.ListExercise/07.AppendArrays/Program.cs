8d1c83482c8099da75bb3937594c96986ed6d679
Georgi <bulbrainn@gmail.com>
2022-07-02 22:22:30 +0300

    Added Append Arrays

---
+++ 02.ProgrammingFundamentals/18.ListExercise/07.AppendArrays/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.AppendArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            List<int> numbers = new List<int>();
+            string[] arrays = input.Split("|");
+            for (int i = arrays.Length - 1; i >= 0; i--)
+            {
+                List<int> arrNums = arrays[i].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+                numbers.AddRange(arrNums);
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
