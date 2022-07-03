e5de8563cef3aaec0c45051f3837077b915dc657
Georgi <bulbrainn@gmail.com>
2022-07-03 10:51:04 +0300

    Added Messaging

---
+++ 02.ProgrammingFundamentals/18.ListMoreExercise/01.Messaging/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Messaging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine().Split();
+            string text = Console.ReadLine();
+            string result = string.Empty;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int index = 0;
+                for (int j = 0; j < numbers[i].Length; j++)
+                {
+                    index += int.Parse(numbers[i][j].ToString());
+                }
+
+                while (index > text.Length)
+                {
+                    index -= text.Length;
+                }
+
+                result += text[index];
+                text = text.Substring(0, index) + text.Substring(index + 1);
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
