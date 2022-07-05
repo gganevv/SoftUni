95e956307bd2fbc4418fb999f17066672384c6fb
Georgi <bulbrainn@gmail.com>
2022-07-05 10:39:27 +0300

    Added SoftUni Reception

---
+++ 02.ProgrammingFundamentals/20.ExamPractice/02.MidExam2/01.SoftUniReception/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01.SoftUniReception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstEmployeeStudents = int.Parse(Console.ReadLine());
+            int secondEmployeeStudents = int.Parse(Console.ReadLine());
+            int thirdEmployeeStudents = int.Parse(Console.ReadLine());
+            int totalStudentsPerHour = firstEmployeeStudents + secondEmployeeStudents + thirdEmployeeStudents;
+
+            int studentsCount = int.Parse(Console.ReadLine());
+            int timeCounter = 0;
+
+            while (studentsCount > 0)
+            {
+                timeCounter++;
+                studentsCount -= totalStudentsPerHour;
+                if (timeCounter % 4 == 0)
+                {
+                    timeCounter++;
+                    continue;
+                }
+            }
+
+            Console.WriteLine($"Time needed: {timeCounter}h.");
+        }
+    }
+}
