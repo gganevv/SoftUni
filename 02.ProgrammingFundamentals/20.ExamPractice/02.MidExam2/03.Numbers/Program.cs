24dd9c21665cbd065748d816ddf0119dd91e1dbb
Georgi <bulbrainn@gmail.com>
2022-07-05 10:40:13 +0300

    Added Numbers

---
+++ 02.ProgrammingFundamentals/20.ExamPractice/02.MidExam2/03.Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> topNumbers = new List<int>();
+            double avg = 1.0 * numbers.Sum() / numbers.Count();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] > avg)
+                {
+                    topNumbers.Add(numbers[i]);
+                }
+            }
+
+            List<int> sortedNumbers = topNumbers.OrderByDescending(i => i).ToList();
+            if (sortedNumbers.Count == 0)
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+            Console.WriteLine(string.Join(" ", sortedNumbers.Take(5)));
+            }
+        }
+    }
+}
