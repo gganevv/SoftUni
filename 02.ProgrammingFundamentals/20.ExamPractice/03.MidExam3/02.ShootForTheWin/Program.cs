330d608303e40b89e8079551fdbe795837d4f371
Georgi <bulbrainn@gmail.com>
2022-07-06 23:19:57 +0300

    Added Shoot For The Win

---
+++ 02.ProgrammingFundamentals/20.ExamPractice/03.MidExam3/02.ShootForTheWin/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.ShootForTheWin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> targets = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string command = Console.ReadLine();
+            int count = 0;
+            int targetValue = 0;
+
+            while (command != "End")
+            {
+                int index = int.Parse(command);
+                if (index < targets.Count && index >= 0)
+                {
+                    targetValue = targets[index];
+                    targets[index] = -1;
+                    count++;
+
+                    for (int i = 0; i < targets.Count; i++)
+                    {
+                        if (targets[i] != -1 && targets[i] > targetValue)
+                        {
+                            targets[i] -= targetValue;
+                        }
+                        else if(targets[i] != -1 && targets[i] <= targetValue)
+                        {
+                            targets[i] += targetValue;
+                        }
+                    }
+                }
+                command = Console.ReadLine();
+            }
+
+            Console.Write($"Shot targets: {count} -> ");
+            Console.WriteLine(string.Join(" ", targets));
+        }
+    }
+}
