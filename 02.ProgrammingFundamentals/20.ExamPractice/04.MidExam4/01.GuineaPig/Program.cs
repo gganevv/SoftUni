593dfdfc617a812cd4bb5ca873e59b6bcac90aa4
Georgi <bulbrainn@gmail.com>
2022-07-06 23:21:36 +0300

    Added Guinea Pig

---
+++ 02.ProgrammingFundamentals/20.ExamPractice/04.MidExam4/01.GuineaPig/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _01.GuineaPig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double food = double.Parse(Console.ReadLine());
+            double hay = double.Parse(Console.ReadLine());
+            double cover = double.Parse(Console.ReadLine());
+            double weight = double.Parse(Console.ReadLine());
+
+            double foodRemain = food * 1000;
+            double hayRemain = hay * 1000;
+            double coverRemain = cover;
+
+            for (int i = 1; i <= 30; i++)
+            {
+                foodRemain -= 300;
+                if (i % 2 == 0)
+                {
+                    hayRemain -= foodRemain * 0.05;
+                }
+
+                if (i % 3 == 0)
+                {
+                    coverRemain -= weight / 3;
+                }
+
+                if (foodRemain <= 0 || hayRemain <= 0 || coverRemain <= 0)
+                {
+                    Console.WriteLine("Merry must go to the pet store!");
+                    return;
+                }
+            }
+
+            Console.WriteLine($"Everything is fine! Puppy is happy! Food: {foodRemain / 1000:f2}, Hay: {hayRemain / 1000:f2}, Cover: {coverRemain:f2}.");
+        }
+    }
+}
