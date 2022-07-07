38e42b84d499c6bc007a9d75b37492b085a3aa2f
Georgi <bulbrainn@gmail.com>
2022-07-07 08:39:02 +0300

    Added Bonus Scoring

---
+++ 02.ProgrammingFundamentals/20.ExamPractice/05.MidExam5/01.BonusScoringProblem/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01.BonusScoringProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfStudents = int.Parse(Console.ReadLine());
+            int numberOfLectures = int.Parse(Console.ReadLine());
+            int bonus = int.Parse(Console.ReadLine());
+            double maxBonus = 0;
+            int bestAttendances = 0;
+
+            for (int i = 0; i < numberOfStudents; i++)
+            {
+                int studentAttendances = int.Parse(Console.ReadLine());
+                double studentBonus = 1.0 * studentAttendances / numberOfLectures * (5 + bonus);
+                if (studentBonus > maxBonus)
+                {
+                    maxBonus = studentBonus;
+                    bestAttendances = studentAttendances;
+                }
+            }
+
+            Console.WriteLine($"Max Bonus: {Math.Ceiling(maxBonus)}.");
+            Console.WriteLine($"The student has attended {bestAttendances} lectures.");
+        }
+    }
+}
