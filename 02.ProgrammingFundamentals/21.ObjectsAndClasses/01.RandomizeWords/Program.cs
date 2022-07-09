a539c6e49bede54e05eede95ed8596d62ef8ff1d
Georgi <bulbrainn@gmail.com>
2022-07-09 20:14:18 +0300

    Added Randomize Words

---
+++ 02.ProgrammingFundamentals/21.ObjectsAndClasses/01.RandomizeWords/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01.RandomizeWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            Random random = new Random();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                int randomIndex = random.Next(0, words.Length);
+                string temp = words[i];
+                words[i] = words[randomIndex];
+                words[randomIndex] = temp;
+            }
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
