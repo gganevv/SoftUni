cb55e7bdd34a88575f0d6278eb1c550493f59350
Georgi <bulbrainn@gmail.com>
2022-07-09 20:15:03 +0300

    Added Songs

---
+++ 02.ProgrammingFundamentals/21.ObjectsAndClasses/03.Songs/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.Songs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfSongs = int.Parse(Console.ReadLine());
+            List<Song> songs = new List<Song>();
+
+            for (int i = 0; i < numberOfSongs; i++)
+            {
+                string[] inputArgs = Console.ReadLine().Split('_');
+                string songTypeList = inputArgs[0];
+                string songName = inputArgs[1];
+                string songTime = inputArgs[2];
+
+                Song currentSong = new Song(songTypeList, songName, songTime);
+
+                songs.Add(currentSong);
+            }
+
+            string input = Console.ReadLine();
+            if (input == "all")
+            {
+                foreach (var song in songs)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+            else
+            {
+                foreach (var song in songs)
+                {
+                    if (song.TypeList == input)
+                    {
+                        Console.WriteLine(song.Name);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 02.ProgrammingFundamentals/21.ObjectsAndClasses/03.Songs/Song.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Songs
+{
+    public class Song
+    {
+
+        public Song(string songTypeList, string songName, string songTime)
+        {
+            this.TypeList = songTypeList;
+            this.Name = songName;
+            this.Time = songTime;
+        }
+
+        public string TypeList { get; set; }
+        public string  Name { get; set; }
+        public string  Time { get; set; }
+    }
+}
