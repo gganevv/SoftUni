e00de96d50cb4bb391c3bab67bb7a5b2b9a01de3
Georgi <bulbrainn@gmail.com>
2022-07-09 20:15:49 +0300

    Added Students 2

---
+++ 02.ProgrammingFundamentals/21.ObjectsAndClasses/05.Students2.0/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.Students2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] inputArgs = input.Split();
+                string firstName = inputArgs[0];
+                string lastName = inputArgs[1];
+                int age = int.Parse(inputArgs[2]);
+                string homeTown = inputArgs[3];
+                Student student = new Student(firstName, lastName, age, homeTown);
+
+                if (students.FirstOrDefault(x => x.FullName == student.FullName) == null)
+                {
+                    students.Add(student);
+                }
+                else
+                {
+                    Student std = students.FirstOrDefault(x => x.FullName == student.FullName);
+                    std.Age = age;
+                    std.Town = homeTown;
+                }
+
+
+                input = Console.ReadLine();
+            }
+
+            string town = Console.ReadLine();
+
+            foreach (var student in students)
+            {
+                if (student.Town == town)
+                {
+                    Console.WriteLine($"{student.FullName} is {student.Age} years old.");
+                }
+            }
+        }
+    }
+}
---
+++ 02.ProgrammingFundamentals/21.ObjectsAndClasses/05.Students2.0/Student.cs
@@ -0,0 +1,25 @@
+﻿namespace _05.Students2._0
+{
+    public class Student
+    {
+        public Student(string firstName, string lastName, int age, string town)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Town = town;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Town { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return $"{this.FirstName} {this.LastName}";
+            }
+        }
+    }
+}
