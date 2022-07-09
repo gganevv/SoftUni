b2409c58f4b3448a5ecd67a4ec2aa61249751e50
Georgi <bulbrainn@gmail.com>
2022-07-09 20:15:26 +0300

    Added Students

---
+++ 02.ProgrammingFundamentals/21.ObjectsAndClasses/04.Students/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            List<Student> students = new List<Student>();
+
+            while (input != "end")
+            {
+                string[] inputArgs = input.Split();
+                string firstName = inputArgs[0];
+                string lastName = inputArgs[1];
+                int age = int.Parse(inputArgs[2]);
+                string homeTown = inputArgs[3];
+                Student student = new Student(firstName, lastName, age, homeTown);
+                students.Add(student);
+
+                input = Console.ReadLine();
+            }
+
+            string town = Console.ReadLine();
+            foreach (var student in students)
+            {
+                if (student.HomeTown == town)
+                {
+
+                    Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+                }
+            }
+        }
+    }
+}
---
+++ 02.ProgrammingFundamentals/21.ObjectsAndClasses/04.Students/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace _04.Students
+{
+    public class Student
+    {
+        public Student(string firstName, string lastName, int age, string homeTown)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.HomeTown = homeTown;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string HomeTown { get; set; }
+    }
+}
