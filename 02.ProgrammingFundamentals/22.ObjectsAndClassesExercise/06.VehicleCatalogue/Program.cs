a358ba8c03d722a0ef7099cfd2ff45f2f2f7a328
Georgi <bulbrainn@gmail.com>
2022-07-10 21:36:45 +0300

    Added Vehicle Catalogue

---
+++ 02.ProgrammingFundamentals/22.ObjectsAndClassesExercise/06.VehicleCatalogue/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.VehicleCatalogue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Vehicle> vehicles = new List<Vehicle>();
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] inputArgs = input.Split();
+                string type = inputArgs[0];
+                string model = inputArgs[1];
+                string color = inputArgs[2];
+                double horsePower = int.Parse(inputArgs[3]);
+                vehicles.Add(new Vehicle(type, model, color, horsePower));
+
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+
+            while (input != "Close the Catalogue")
+            {
+                Console.WriteLine(vehicles.First(x => x.Model == input));
+
+                input = Console.ReadLine();
+            }
+            double carsAvg = 0;
+            double trucksAvg = 0;
+            if (vehicles.FindAll(x => x.Type == "car").Count > 0)
+            {
+                carsAvg = vehicles.Where(x => x.Type == "car").Average(x => x.HorsePower);
+            }
+
+            if (vehicles.FindAll(x => x.Type == "truck").Count > 0)
+            {
+                trucksAvg = vehicles.Where(x => x.Type == "truck").Average(x => x.HorsePower);
+            }
+           
+            Console.WriteLine($"Cars have average horsepower of: {carsAvg:f2}.");
+            Console.WriteLine($"Trucks have average horsepower of: {trucksAvg:f2}.");
+        }
+    }
+}
---
+++ 02.ProgrammingFundamentals/22.ObjectsAndClassesExercise/06.VehicleCatalogue/Vehicle.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace _06.VehicleCatalogue
+{
+    public class Vehicle
+    {
+        public Vehicle(string type, string model, string color, double horsePower)
+        {
+            Type = type;
+            Model = model;
+            Color = color;
+            HorsePower = horsePower;
+        }
+        public string Type { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public double HorsePower { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Type: {CultureInfo.CurrentCulture.TextInfo.ToTitleCase(Type)}");
+            sb.AppendLine($"Model: {Model}");
+            sb.AppendLine($"Color: {Color}");
+            sb.AppendLine($"Horsepower: {HorsePower}");
+
+            return sb.ToString().Trim();
+        }
+    }
+}
