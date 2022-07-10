8bcb883770bbe0e58310f1993008fdebc9e62741
Georgi <bulbrainn@gmail.com>
2022-07-10 21:35:31 +0300

    Added Articles 2

---
+++ 02.ProgrammingFundamentals/22.ObjectsAndClassesExercise/03.Articles2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.Articles2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Article> articles = new List<Article>();
+            int numberOfArticles = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfArticles; i++)
+            {
+                string[] articleArgs = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string title = articleArgs[0];
+                string content = articleArgs[1];
+                string author = articleArgs[2];
+
+                Article article = new Article(title, content, author);
+                articles.Add(article);
+            }
+            Console.ReadLine();
+            articles.ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
