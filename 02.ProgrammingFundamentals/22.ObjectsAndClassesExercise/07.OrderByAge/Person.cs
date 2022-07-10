5091861de97584bd19e69f2f64beda24cf74fb0e
Georgi <bulbrainn@gmail.com>
2022-07-10 21:37:11 +0300

    Added Order By Age

---
+++ 02.ProgrammingFundamentals/22.ObjectsAndClassesExercise/07.OrderByAge/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace _07.OrderByAge
+{
+    public class Person
+    {
+        public Person(string name, string id, int age)
+        {
+            Name = name;
+            ID = id;
+            Age = age;
+        }
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public int Age { get; set; }
+        public override string ToString()
+        {
+            return $"{Name} with ID: {ID} is {Age} years old.";
+        }
+    }
+}
---
+++ 02.ProgrammingFundamentals/22.ObjectsAndClassesExercise/07.OrderByAge/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.OrderByAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] inputArgs = input.Split();
+                string name = inputArgs[0];
+                string id = inputArgs[1];
+                int age = int.Parse(inputArgs[2]);
+                Person currentPerson = new Person(name, id, age);
+                if (people.Any(x => x.ID == id))
+                {
+                    Person person = people.First(x => x.ID == id);
+                    person.Name = name;
+                    person.Age = age;
+                }
+                else
+                {
+                people.Add(currentPerson);
+
+                }
+
+                input = Console.ReadLine();
+            }
+
+            people = people.OrderBy(x => x.Age).ToList();
+            people.ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
