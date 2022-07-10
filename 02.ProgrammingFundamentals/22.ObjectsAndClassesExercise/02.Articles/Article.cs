9304624adec8cecaa44d9df80cd66fa5a4ead8ca
Georgi <bulbrainn@gmail.com>
2022-07-10 21:35:07 +0300

    Added Articles

---
+++ 02.ProgrammingFundamentals/22.ObjectsAndClassesExercise/02.Articles/Article.cs
@@ -0,0 +1,25 @@
+﻿namespace _02.Articles
+{
+    public class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public void Edit(string content) => Content = content;
+        public void ChangeAuthor(string author) => Author = author;
+        public void Rename(string title) => Title = title;
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+}
---
+++ 02.ProgrammingFundamentals/22.ObjectsAndClassesExercise/02.Articles/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _02.Articles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] articleArgs = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            Article article = new Article(articleArgs[0], articleArgs[1], articleArgs[2]);
+            int numberOfCommands = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string[] commandArgs = Console.ReadLine().Split(": ", StringSplitOptions.RemoveEmptyEntries);
+                string command = commandArgs[0];
+                string change = commandArgs[1];
+                switch (command)
+                {
+                    case "Edit":
+                        article.Edit(change);
+                        break;
+                    case "ChangeAuthor":
+                        article.ChangeAuthor(change);
+                        break;
+                    case "Rename":
+                        article.Rename(change);
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+            Console.WriteLine(article);
+        }
+    }
+}
