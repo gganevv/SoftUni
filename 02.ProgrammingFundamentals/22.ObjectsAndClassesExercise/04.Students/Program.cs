e799d0dad52b6484bbfbff7f1c5e3efc7ccf93fe
Georgi <bulbrainn@gmail.com>
2022-07-10 21:35:51 +0300

    Added Students

---
+++ 02.ProgrammingFundamentals/22.ObjectsAndClassesExercise/04.Students/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfStudents = int.Parse(Console.ReadLine());
+            List<Student> students = new List<Student>();
+
+            for (int i = 0; i < numberOfStudents; i++)
+            {
+                string[] inputArgs = Console.ReadLine().Split();
+                string firstName = inputArgs[0];
+                string lastName = inputArgs[1];
+                double grade = double.Parse(inputArgs[2]);
+
+                Student student = new Student(firstName, lastName, grade);
+                students.Add(student);
+            }
+
+            students = students.OrderByDescending(x => x.Grade).ToList();
+
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+}
---
+++ 02.ProgrammingFundamentals/22.ObjectsAndClassesExercise/04.Students/Student.cs
@@ -0,0 +1,20 @@
+﻿namespace _04.Students
+{
+    public class Student
+    {
+        public Student(string firstName, string lastName, double grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}: {Grade:f2}";
+        }
+    }
+}
