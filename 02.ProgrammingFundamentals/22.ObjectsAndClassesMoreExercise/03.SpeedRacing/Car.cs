26e5bdd9914adc6313928d7ea869b7bc7327c39d
Georgi <bulbrainn@gmail.com>
2022-07-11 21:56:19 +0300

    Added Speed Racing

---
+++ 02.ProgrammingFundamentals/22.ObjectsAndClassesMoreExercise/03.SpeedRacing/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _03.SpeedRacing
+{
+    public class Car
+    {
+        public Car(string model, double fuel, double consumption)
+        {
+            Model = model;
+            Fuel = fuel;
+            Consumption = consumption;
+            TraveledDistance = 0;
+        }
+
+        public string Model { get; set; }
+        public double Fuel { get; set; }
+        public double Consumption { get; set; }
+        public int TraveledDistance { get; set; }
+
+        public void Drive(int distance)
+        {
+            if (Fuel - (distance * Consumption) >= 0)
+            {
+                Fuel -= distance * Consumption;
+                TraveledDistance += distance;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Model} {Fuel:f2} {TraveledDistance}";
+        }
+    }
+}
---
+++ 02.ProgrammingFundamentals/22.ObjectsAndClassesMoreExercise/03.SpeedRacing/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.SpeedRacing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+
+            int numberOfCars = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                Car car = new Car(input[0], double.Parse(input[1]), double.Parse(input[2]));
+                cars.Add(car);
+            }
+
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                string[] commandArgs = command.Split();
+                string carModel = commandArgs[1];
+                int amount = int.Parse(commandArgs[2]);
+                Car currentCar = cars.First(x => x.Model == carModel);
+                currentCar.Drive(amount);
+
+                command = Console.ReadLine();
+            }
+
+            cars.ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
