2631e975c942f97e34452af8adc8d2efa801a01e
Georgi <bulbrainn@gmail.com>
2022-07-11 21:56:36 +0300

    Added Raw Data

---
+++ 02.ProgrammingFundamentals/22.ObjectsAndClassesMoreExercise/04.RawData/Car.cs
@@ -0,0 +1,18 @@
+﻿namespace _04.RawData
+{
+    public class Car
+    {
+        public Car(string model, int engineSpeed, int enginePower, int cargoWeight, string cargoType)
+        {
+            Model = model;
+            Engine engine = new Engine(engineSpeed, enginePower);
+            Engine = engine;
+            Cargo cargo = new Cargo(cargoWeight, cargoType);
+            Cargo = cargo;
+        }
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public Cargo Cargo { get; set; }
+
+    }
+}
---
+++ 02.ProgrammingFundamentals/22.ObjectsAndClassesMoreExercise/04.RawData/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.RawData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+
+            int numberOfCars = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string model = input[0];
+                int engineSpeed = int.Parse(input[1]);
+                int enginePower = int.Parse(input[2]);
+                int cargoWeight = int.Parse(input[3]);
+                string cargoType = input[4];
+                Car car = new Car(model, engineSpeed, enginePower, cargoWeight, cargoType);
+                cars.Add(car);
+            }
+
+            string cargo = Console.ReadLine();
+            if (cargo == "fragile")
+            {
+                foreach (Car car in cars)
+                {
+                    if (car.Cargo.Type == cargo && car.Cargo.Weight < 1000)
+                    {
+                        Console.WriteLine(car.Model);
+                    }
+                }
+            }
+            else
+            {
+                foreach (Car car in cars)
+                {
+                    if (car.Cargo.Type == cargo && car.Engine.Power > 250)
+                    {
+                        Console.WriteLine(car.Model);
+                    }
+                }
+            }
+        }
+    }
+}
