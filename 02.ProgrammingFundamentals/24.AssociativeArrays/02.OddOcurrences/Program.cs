8ebf12c505f6f41ecdb14f02a883b44d4f5bf6c6
Georgi <bulbrainn@gmail.com>
2022-07-12 18:37:01 +0300

    Added Odd Ocurrences

---
+++ 02.ProgrammingFundamentals/24.AssociativeArrays/02.OddOcurrences/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.OddOcurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            Dictionary<string, int> wordsCount = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (!wordsCount.ContainsKey(word.ToLower()))
+                {
+                    wordsCount.Add(word.ToLower(), 0);
+                }
+
+                wordsCount[word.ToLower()]++;
+            }
+
+            List<string> result = wordsCount.Where(x => x.Value % 2 != 0).Select(x => x.Key).ToList();
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
