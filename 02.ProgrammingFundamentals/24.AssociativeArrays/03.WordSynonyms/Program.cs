a4e13552dc59300bf7ed371862fe29144c6a2faf
Georgi <bulbrainn@gmail.com>
2022-07-12 18:37:37 +0300

    Added Word Synonyms

---
+++ 02.ProgrammingFundamentals/24.AssociativeArrays/03.WordSynonyms/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.WordSynonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> synonyms = new Dictionary<string, List<string>>();
+            int countOfWords = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < countOfWords; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (!synonyms.ContainsKey(word))
+                {
+                    synonyms.Add(word, new List<string>());
+                }
+
+                synonyms[word].Add(synonym);
+            }
+
+            foreach (var item in synonyms)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ", item.Value)}");
+            }
+        }
+    }
+}
