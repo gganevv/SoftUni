2315f9a55153a101464bbcc1b6293c5e3a113d49
Georgi <bulbrainn@gmail.com>
2022-07-12 18:37:59 +0300

    Added Word Filter

---
+++ 02.ProgrammingFundamentals/24.AssociativeArrays/04.WordFilter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.WordFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine().Split().ToList();
+
+            List<string> filteredWords = words.Where(x => x.Length % 2 == 0).ToList();
+            filteredWords.ForEach (x => Console.WriteLine(x));
+        }
+    }
+}
