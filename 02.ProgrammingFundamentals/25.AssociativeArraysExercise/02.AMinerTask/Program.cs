c9678ac350afdf684c0a042f3f8db678449235f8
Georgi <bulbrainn@gmail.com>
2022-07-13 17:55:45 +0300

    Added A Miner Task

---
+++ 02.ProgrammingFundamentals/25.AssociativeArraysExercise/02.AMinerTask/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.AMinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> materials = new Dictionary<string, int>();
+
+            string input = Console.ReadLine();
+            while (input != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+                if (!materials.ContainsKey(input))
+                {
+                    materials[input] = 0;
+                }
+
+                materials[input] += quantity;
+
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var m in materials)
+            {
+                Console.WriteLine($"{m.Key} -> {m.Value}");
+            }
+        }
+    }
+}
