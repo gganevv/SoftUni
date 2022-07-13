2e09ddd7fcf208de24537d94c1210c6e3574727a
Georgi <bulbrainn@gmail.com>
2022-07-13 17:57:28 +0300

    Added Courses

---
+++ 02.ProgrammingFundamentals/25.AssociativeArraysExercise/05.Courses/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] inputArgs = input.Split(" : ");
+                string course = inputArgs[0];
+                string student = inputArgs[1];
+
+                if (!courses.ContainsKey(course))
+                {
+                    courses.Add(course, new List<string>());
+                }
+
+                courses[course].Add(student);
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var c in courses)
+            {
+                Console.WriteLine($"{c.Key}: {c.Value.Count}");
+                foreach (var s in c.Value)
+                {
+                    Console.WriteLine($"-- {s}");
+                }
+            }
+        }
+    }
+}
