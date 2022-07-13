d748c6ee8d1a2db68b9a2ce338b11ffe55f97092
Georgi <bulbrainn@gmail.com>
2022-07-13 17:58:11 +0300

    Added Company Users

---
+++ 02.ProgrammingFundamentals/25.AssociativeArraysExercise/07.CompanyUsers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.CompanyUsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> companies = new Dictionary<string, List<string>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] inputArgs = input.Split(" -> ");
+                string company = inputArgs[0];
+                string employee = inputArgs[1];
+
+                if (!companies.ContainsKey(company))
+                {
+                    companies.Add(company, new List<string>());
+                }
+
+                if (!companies[company].Contains(employee))
+                {
+                    companies[company].Add(employee);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var company in companies)
+            {
+                Console.WriteLine(company.Key);
+                foreach (var employee in company.Value)
+                {
+                    Console.WriteLine($"-- {employee}");
+                }
+            }
+        }
+    }
+}
