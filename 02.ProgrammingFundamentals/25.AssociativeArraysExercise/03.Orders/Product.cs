397830312e11db2492deb53a584ee28a2e7ad46d
Georgi <bulbrainn@gmail.com>
2022-07-13 17:56:05 +0300

    Added Orders

---
+++ 02.ProgrammingFundamentals/25.AssociativeArraysExercise/03.Orders/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace _03.Orders
+{
+    public class Product
+    {
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public double TotalPrice => Price * Quantity;
+    }
+}
---
+++ 02.ProgrammingFundamentals/25.AssociativeArraysExercise/03.Orders/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            string input = Console.ReadLine();
+
+            while (input != "buy")
+            {
+                ParseProducts(products, input);
+                input = Console.ReadLine();
+            }
+
+            products.ForEach(x => Console.WriteLine($"{x.Name} -> {x.TotalPrice:f2}"));
+        }
+
+        public static void ParseProducts(List<Product> products, string input)
+        {
+            string[] productArgs = input.Split(" ");
+            string name = productArgs[0];
+            double price = double.Parse(productArgs[1]);
+            int quantity = int.Parse(productArgs[2]);
+            if (!products.Any(x => x.Name == name))
+            {
+                products.Add(new Product(name, price, quantity));
+            }
+            else
+            {
+                Product product = products.Find(x => x.Name == name);
+                product.Quantity += quantity;
+                product.Price = price;
+            }
+        }
+    }
+}
