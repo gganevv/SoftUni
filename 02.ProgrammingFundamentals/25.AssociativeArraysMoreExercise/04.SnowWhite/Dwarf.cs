687b21e5ceb0a9f50e5ed5f5e52422cf09bdb516
Georgi <bulbrainn@gmail.com>
2022-07-14 18:19:47 +0300

    Added Snow White

---
+++ 02.ProgrammingFundamentals/25.AssociativeArraysMoreExercise/04.SnowWhite/Dwarf.cs
@@ -0,0 +1,19 @@
+﻿namespace _04.SnowWhite
+{
+    public class Dwarf
+    {
+        public Dwarf(string name, string hatColor, int phisics)
+        {
+            Name = name;
+            HatColor = hatColor;
+            Phisics = phisics;
+        }
+        public string Name { get; set; }
+        public string HatColor { get; set; }
+        public int Phisics { get; set; }
+        public override string ToString()
+        {
+            return $"({HatColor}) {Name} <-> {Phisics}";
+        }
+    }
+}
---
+++ 02.ProgrammingFundamentals/25.AssociativeArraysMoreExercise/04.SnowWhite/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.SnowWhite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Dwarf> dwarves = new List<Dwarf>();
+            string input = Console.ReadLine();
+            while (input != "Once upon a time")
+            {
+                string[] inputArgs = input.Split(" <:> ");
+                string dwarfName = inputArgs[0];
+                string dwarfHatColor = inputArgs[1];
+                int dwarfPhisics = int.Parse(inputArgs[2]);
+
+                if (!dwarves.Any(x => x.Name == dwarfName && x.HatColor == dwarfHatColor))
+                {
+                    dwarves.Add(new Dwarf(dwarfName, dwarfHatColor, dwarfPhisics));
+                }
+
+                if (dwarves.First(x => x.Name == dwarfName && x.HatColor == dwarfHatColor).Phisics < dwarfPhisics)
+                {
+                    dwarves.First(x => x.Name == dwarfName && x.HatColor == dwarfHatColor).Phisics = dwarfPhisics;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            dwarves = dwarves.OrderByDescending(x => x.Phisics).ThenByDescending(x => dwarves.Where(y => y.HatColor == x.HatColor).Count()).ToList();
+            dwarves.ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
