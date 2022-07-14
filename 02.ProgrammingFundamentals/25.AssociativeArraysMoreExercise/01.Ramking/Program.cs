5eb55407cb998a9100133c1da0f1b654e41f73e9
Georgi <bulbrainn@gmail.com>
2022-07-14 18:18:18 +0300

    Added Ranking

---
+++ 02.ProgrammingFundamentals/25.AssociativeArraysMoreExercise/01.Ramking/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Ramking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> contests = new Dictionary<string, string>();
+            List<Student> students = new List<Student>();
+            Student bestStudent = new Student("");
+
+            string input = Console.ReadLine();
+
+            while (input != "end of contests")
+            {
+                string[] inputArgs = input.Split(":");
+                string contest = inputArgs[0];
+                string password = inputArgs[1];
+                contests.Add(contest, password);
+
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+
+            while (input != "end of submissions")
+            {
+                string[] inputArgs = input.Split("=>");
+                string contest = inputArgs[0];
+                string password = inputArgs[1];
+                string userName = inputArgs[2];
+                int points = int.Parse(inputArgs[3]);
+
+                bool validContest = contests.ContainsKey(contest);
+                bool validPassword = false;
+
+                if (validContest)
+                {
+                    validPassword = contests[contest] == password;
+                }
+
+                if (validContest && validPassword)
+                {
+                    AddStudent(students, userName);
+                    Student student = students.First(x => x.Name == userName);
+                    AddStudentContests(contest, points, student);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            bestStudent = GetBestStudent(students, bestStudent);
+            Console.WriteLine($"Best candidate is {bestStudent.Name} with total {bestStudent.GetTotalPoints()} points.");
+
+            Console.WriteLine("Ranking: ");
+            students = students.OrderBy(x => x.Name).ToList();
+            students.ForEach(x => Console.WriteLine(x));
+        }
+
+        private static void AddStudentContests(string contest, int points, Student student)
+        {
+            if (!student.Contests.ContainsKey(contest))
+            {
+                student.Contests.Add(contest, points);
+            }
+            else if (student.Contests[contest] < points)
+            {
+                student.Contests[contest] = points;
+            }
+        }
+
+        public static Student GetBestStudent(List<Student> students, Student bestStudent)
+        {
+            foreach (var student in students)
+            {
+                if (student.GetTotalPoints() > bestStudent.GetTotalPoints())
+                {
+                    bestStudent = student;
+                }
+            }
+
+            return bestStudent;
+        }
+
+        public static void AddStudent(List<Student> students, string userName)
+        {
+            if (!students.Any(x => x.Name == userName))
+            {
+                Student newStudent = new Student(userName);
+                students.Add(newStudent);
+            }
+        }
+    }
+}
---
+++ 02.ProgrammingFundamentals/25.AssociativeArraysMoreExercise/01.Ramking/Student.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01.Ramking
+{
+    public class Student
+    {
+        public Student(string name)
+        {
+            Name = name;
+            Contests = new Dictionary<string, int>();
+        }
+        public string Name { get; set; }
+        public Dictionary<string, int> Contests { get; set; }
+        public int GetTotalPoints() => Contests.Values.Sum();
+
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Name);
+            Contests = Contests.OrderByDescending(x => x.Value).ToDictionary(k => k.Key, v => v.Value);
+            foreach (var contest in Contests)
+            {
+                sb.AppendLine($"#  {contest.Key} -> {contest.Value}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
