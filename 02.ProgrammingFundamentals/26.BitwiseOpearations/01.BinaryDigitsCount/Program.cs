7818563bdd7aea8fb943a87091a625b585d6122e
Georgi <bulbrainn@gmail.com>
2022-07-16 19:20:19 +0300

    Added Binary Digits Count

---
+++ 02.ProgrammingFundamentals/26.BitwiseOpearations/01.BinaryDigitsCount/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01.BinaryDigitsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            char b = char.Parse(Console.ReadLine());
+            int bCounter = 0;
+            string numAsBinaryString = string.Empty;
+
+            while (number > 0)
+            {
+                numAsBinaryString += number % 2;
+                number /= 2;
+            }
+
+            for (int i = 0; i < numAsBinaryString.Length; i++)
+            {
+                if (numAsBinaryString[i] == b)
+                {
+                    bCounter++;
+                }
+            }
+
+            Console.WriteLine(bCounter);
+        }
+    }
+}
