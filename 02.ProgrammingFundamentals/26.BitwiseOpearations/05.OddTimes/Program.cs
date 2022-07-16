f84f90d398d5af164fea3019142ba30f45ebec48
Georgi <bulbrainn@gmail.com>
2022-07-16 19:21:52 +0300

    Added Odd Times

---
+++ 02.ProgrammingFundamentals/26.BitwiseOpearations/05.OddTimes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.OddTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+            int[] n = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = 0; i < n.Length; i++)
+            {
+                int currentN = n[i];
+                if (!numbers.ContainsKey(currentN))
+                {
+                    numbers.Add(currentN, 0);
+                }
+                numbers[currentN]++;
+            }
+
+            foreach (var d in numbers)
+            {
+                if (d.Value % 2 != 0)
+                {
+                    Console.WriteLine(d.Key);
+                }
+            }
+        }
+    }
+}
