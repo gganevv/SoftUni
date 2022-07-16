9980480b4ab4dd17e287dfb8289c1fb2551ab237
Georgi <bulbrainn@gmail.com>
2022-07-16 19:20:49 +0300

    Added Bit At Position 1

---
+++ 02.ProgrammingFundamentals/26.BitwiseOpearations/02.BitAtPosition1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _02.BitAtPosition1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bitAtPosition1;
+            int input = int.Parse(Console.ReadLine());
+            bitAtPosition1 = input >> 1 & 1;
+            Console.WriteLine(bitAtPosition1);
+        }
+    }
+}
