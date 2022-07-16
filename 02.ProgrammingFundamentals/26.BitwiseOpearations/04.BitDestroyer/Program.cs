a9f2ef05fb8277bad1fb8559873ec5f75c66237b
Georgi <bulbrainn@gmail.com>
2022-07-16 19:21:28 +0300

    Added Bit Destroyer

---
+++ 02.ProgrammingFundamentals/26.BitwiseOpearations/04.BitDestroyer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _04.BitDestroyer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int position = int.Parse(Console.ReadLine());
+            int mask = 1;
+
+            mask = ~(mask << position);
+            int newNumber = mask & number;
+            Console.WriteLine(newNumber);
+        }
+    }
+}
