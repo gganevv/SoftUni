00d8fbc8c9dcbd11f8258c638f159c54962884ad
Georgi <bulbrainn@gmail.com>
2022-07-16 19:21:08 +0300

    Added P-th Bit

---
+++ 02.ProgrammingFundamentals/26.BitwiseOpearations/03.P-thBit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _03.P_thBit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+            int bitAtPosition1 = input >> p & 1;
+            Console.WriteLine(bitAtPosition1);
+        }
+    }
+}
