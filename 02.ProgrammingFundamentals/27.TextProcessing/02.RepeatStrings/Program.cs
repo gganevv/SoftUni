befcbe7ae61da0b884337917fc09dfa22e86a06c
Georgi <bulbrainn@gmail.com>
2022-07-17 21:47:33 +0300

    Added Repeat String

---
+++ 02.ProgrammingFundamentals/27.TextProcessing/02.RepeatStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace _02.RepeatStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            string[] words = Console.ReadLine().Split();
+
+            foreach (string word in words)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    sb.Append(word);
+                }
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
