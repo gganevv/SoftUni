d3aa2cfe4bfee79e6966951b567bb67ed7955e6b
Georgi <bulbrainn@gmail.com>
2022-07-17 21:48:51 +0300

    Added Digits Letters And Others

---
+++ 02.ProgrammingFundamentals/27.TextProcessing/05.DigitsLettersAndOthers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _05.DigitsLettersAndOthers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string digits = string.Empty;
+            string letters = string.Empty;
+            string other = string.Empty;
+
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsDigit(text[i]))
+                {
+                    digits += text[i];
+                }
+                else if (char.IsLetter(text[i]))
+                {
+                    letters += text[i];
+                }
+                else
+                {
+                    other += text[i];
+                }
+            }
+
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(other);
+        }
+    }
+}
