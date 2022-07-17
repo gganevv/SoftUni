25e32288e99252aecbf465aa6ccb17050f7b9e18
Georgi <bulbrainn@gmail.com>
2022-07-17 21:48:23 +0300

    Added Text Filter

---
+++ 02.ProgrammingFundamentals/27.TextProcessing/04.TextFilter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04.TextFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < bannedWords.Length; i++)
+            {
+                text = text.Replace(bannedWords[i], new string('*', bannedWords[i].Length));
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
