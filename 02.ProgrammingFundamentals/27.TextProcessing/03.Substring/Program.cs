b8f303999973f0597079bd97cae3021f62b373ba
Georgi <bulbrainn@gmail.com>
2022-07-17 21:47:51 +0300

    Added Substring

---
+++ 02.ProgrammingFundamentals/27.TextProcessing/03.Substring/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _03.Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strToRemove = Console.ReadLine();
+            string str = Console.ReadLine();
+            while (str.Contains(strToRemove))
+            {
+                int index = str.IndexOf(strToRemove);
+                str = str.Remove(index, strToRemove.Length);
+            }
+            Console.WriteLine(str);
+        }
+    }
+}
