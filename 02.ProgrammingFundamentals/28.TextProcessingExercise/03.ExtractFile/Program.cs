aee37e1dd3ea0d5c1ffbb1b1a5579b3cdc411f16
Georgi <bulbrainn@gmail.com>
2022-07-18 20:31:24 +0300

    Added Extract File

---
+++ 02.ProgrammingFundamentals/28.TextProcessingExercise/03.ExtractFile/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _03.ExtractFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] parts = Console.ReadLine().Split('\\');
+            string[] file = parts[parts.Length - 1].Split('.');
+            string fileName = file[0];
+            string extension = file[1];
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
