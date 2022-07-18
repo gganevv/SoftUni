3c9774a78e6b5d59ee5ad400bb21f591a10f0767
Georgi <bulbrainn@gmail.com>
2022-07-18 20:35:01 +0300

    Added Replace Repeating Chars

---
+++ 02.ProgrammingFundamentals/28.TextProcessingExercise/06.ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace _06.ReplaceRepeatingChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            char lastCh = word[0];
+            StringBuilder sb = new StringBuilder();
+            sb.Append(lastCh);
+
+            for (int i = 1; i < word.Length; i++)
+            {
+                if (word[i] != lastCh)
+                {
+                    sb.Append(word[i]);
+                }
+                lastCh = word[i];
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
