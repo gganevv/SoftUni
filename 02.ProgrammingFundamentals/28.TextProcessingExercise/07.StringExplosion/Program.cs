3b2b49a7b4b45bc0ce8f6bfb50758cbf6bfcf861
Georgi <bulbrainn@gmail.com>
2022-07-18 20:35:19 +0300

    Added String Explosion

---
+++ 02.ProgrammingFundamentals/28.TextProcessingExercise/07.StringExplosion/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _07.StringExplosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string result = string.Empty;
+            int power = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                char currentChar = word[i];
+                if (currentChar == '>')
+                {
+                    power += int.Parse(word[i + 1].ToString()) + 1;
+                    result += '>';
+                }
+
+                if (power == 0)
+                {
+                    result += currentChar;
+                }
+                else
+                {
+                    power--;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
