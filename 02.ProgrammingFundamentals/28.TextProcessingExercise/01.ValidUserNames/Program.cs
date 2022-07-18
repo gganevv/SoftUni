5a7682c2ecab36f855e489d34cccda36ff0ccc4e
Georgi <bulbrainn@gmail.com>
2022-07-18 20:30:34 +0300

    Added Valid User Names

---
+++ 02.ProgrammingFundamentals/28.TextProcessingExercise/01.ValidUserNames/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01.ValidUserNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] userNames = Console.ReadLine().Split(", ");
+
+            foreach (var name in userNames)
+            {
+                if (name.Length >= 3 && name.Length <= 16)
+                {
+                    bool validName = true;
+                    for (int i = 0; i < name.Length; i++)
+                    {
+                        if (!(char.IsLetterOrDigit(name[i]) ||name[i] == '-' || name[i] == '_'))
+                        {
+                            validName = false;
+                            break;
+                        }
+                    }
+
+                    if (validName)
+                    {
+                        Console.WriteLine(name);
+                    }
+                }
+            }
+        }
+    }
+}
