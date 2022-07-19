c2313cdc86902c49cc5fb7054b326ad944ee7059
Georgi <bulbrainn@gmail.com>
2022-07-19 19:04:12 +0300

    Added ASCII Sumator

---
+++ 02.ProgrammingFundamentals/28.TextProcessingMoreExercise/02.ASCIISumator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _02.ASCIISumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstCh = char.Parse(Console.ReadLine());
+            char secondCh = char.Parse(Console.ReadLine());
+            int sum = 0;
+            string randomString = Console.ReadLine();
+
+            for (int i = 0; i < randomString.Length; i++)
+            {
+                if (randomString[i] > firstCh && randomString[i] < secondCh)
+                {
+                    sum += randomString[i];
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
