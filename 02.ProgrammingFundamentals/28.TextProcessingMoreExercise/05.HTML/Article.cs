faba66eb06db137a77db92d534133ef8dab525b8
Georgi <bulbrainn@gmail.com>
2022-07-19 19:05:31 +0300

    Added HTML

---
+++ 02.ProgrammingFundamentals/28.TextProcessingMoreExercise/05.HTML/Article.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace _05.HTML
+{
+    public class Article
+    {
+        public Article(string title, string content)
+        {
+            Title = title;
+            Content = content;
+            Comments = new List<string>();
+        }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public List<string> Comments { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("<h1>");
+            sb.AppendLine($"    {Title}");
+            sb.AppendLine("</h1>");
+            sb.AppendLine("<article>");
+            sb.AppendLine($"    {Content}");
+            sb.AppendLine("</article>");
+            foreach (var comment in Comments)
+            {
+                sb.AppendLine("<div>");
+                sb.AppendLine($"    {comment}");
+                sb.AppendLine("</div>");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ 02.ProgrammingFundamentals/28.TextProcessingMoreExercise/05.HTML/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _05.HTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string title = Console.ReadLine();
+            string content = Console.ReadLine();
+            
+            Article article = new Article(title, content);
+            
+            string comment = Console.ReadLine();
+            while (comment != "end of comments")
+            {
+                article.Comments.Add(comment);
+                comment = Console.ReadLine();
+            }
+
+            Console.WriteLine(article);
+        }
+    }
+}
