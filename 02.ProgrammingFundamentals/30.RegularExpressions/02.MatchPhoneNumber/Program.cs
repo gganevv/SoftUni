b900593be4d92955f8a8491d7e446d48c0e25b57
Georgi <bulbrainn@gmail.com>
2022-07-20 23:58:57 +0300

    Added Match Phone Numbers

---
+++ 02.ProgrammingFundamentals/30.RegularExpressions/02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02.MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"\+359([- ])2\1\d{3}\1\d{4}\b";
+            string phones = Console.ReadLine();
+            MatchCollection phoneMatches = Regex.Matches(phones, regex);
+            string[] matchedPhones = phoneMatches
+                .Cast<Match>()
+                .Select(a => a.Value.Trim())
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ", matchedPhones));
+        }
+    }
+}
