0a118188b5e5ea04dd04960672deded7b1d21b8d
Georgi <bulbrainn@gmail.com>
2022-07-21 22:02:52 +0300

    Added Match Dates

---
+++ 02.ProgrammingFundamentals/30.RegularExpressions/03.MatchDates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03.MatchDates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"\b(?<day>\d{2})(?<separator>[-.\/])(?<month>[A-Z][a-z]{2})\k<separator>(?<year>\d{4})\b";
+            string dateStrings = Console.ReadLine();
+            MatchCollection dates = Regex.Matches(dateStrings, regex);
+            foreach (Match date in dates)
+            {
+                string day = date.Groups["day"].Value;
+                string month = date.Groups["month"].Value;
+                string year = date.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
