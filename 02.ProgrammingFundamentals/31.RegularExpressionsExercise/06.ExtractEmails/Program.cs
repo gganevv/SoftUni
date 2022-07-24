976e385f1f0652d655961cdabe68636f3bd7656b
Georgi <bulbrainn@gmail.com>
2022-07-25 01:13:03 +0300

    Added Extract Emails

---
+++ 02.ProgrammingFundamentals/31.RegularExpressionsExercise/06.ExtractEmails/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _06.ExtractEmails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string emailPattern = @"(^|\s)[A-Za-z0-9][\w*\-\.]*[A-Za-z0-9]@[A-Za-z]+([.-][A-Za-z]+)+\b";
+            string input = Console.ReadLine();
+            var emails = Regex.Matches(input, emailPattern);
+            foreach (Match match in emails)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+    }
+}
