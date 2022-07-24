1e610f26d37181a1cd8d4841a259f3bc6cac9d26
Georgi <bulbrainn@gmail.com>
2022-07-25 00:51:04 +0300

    Added Nether Realms

---
+++ 02.ProgrammingFundamentals/31.RegularExpressionsExercise/05.NetherRealms/Demon.cs
@@ -0,0 +1,62 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _05.NetherRealms
+{
+    internal partial class Program
+    {
+        public class Demon
+        {
+            public Demon(string name)
+            {
+                Name = name;
+                Health = CalculateHealth(name);
+                Damage = CalculateDamage(name);
+            }
+
+            public int CalculateHealth(string name)
+            {
+                string healthPattern = @"[^\+\-\*\/\.,0-9]";
+                int sum = 0;
+                var healthChar = Regex.Matches(name, healthPattern);
+                foreach (Match match in healthChar)
+                {
+                    sum += char.Parse(match.Value);
+                }
+                return sum;
+            }
+            public double CalculateDamage(string name)
+            {
+                double sum = 0;
+                string digitsPattern = @"-?\d+\.?\d*";
+                string multiplyDividePattern = @"[\*\/]";
+                var digits = Regex.Matches(name, digitsPattern);
+                var multiplyDivide = Regex.Matches(name, multiplyDividePattern);
+                foreach (Match match in digits)
+                {
+                    sum += double.Parse(match.Value);
+                }
+                foreach (Match match in multiplyDivide)
+                {
+                    if (match.Value == "*")
+                    {
+                        sum *= 2;
+                    }
+                    else
+                    {
+                        sum /= 2;
+                    }
+                }
+                return sum;
+            }
+
+            public string Name { get; set; }
+            public int Health { get; set; }
+            public double Damage { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Name} - {Health} health, {Damage:f2} damage";
+            }
+        }
+    }
+}
---
+++ 02.ProgrammingFundamentals/31.RegularExpressionsExercise/05.NetherRealms/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _05.NetherRealms
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            string splitPattern = @"[,\s]+";
+            string input = Console.ReadLine();
+            List<Demon> demons = new List<Demon>();
+            string[] inputDemons = Regex.Split(input, splitPattern).OrderBy(x => x).ToArray();
+            for (int i = 0; i < inputDemons.Length; i++)
+            {
+                demons.Add(new Demon(inputDemons[i]));
+            }
+            demons.ForEach(Console.WriteLine);
+        }
+    }
+}
