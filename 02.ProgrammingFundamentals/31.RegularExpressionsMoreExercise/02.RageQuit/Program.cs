3827ce33bf5eb0e00abcbc84ab8e24d875cc8d7e
Georgi <bulbrainn@gmail.com>
2022-07-27 23:39:14 +0300

    Ads

---
+++ 02.ProgrammingFundamentals/31.RegularExpressionsMoreExercise/02.RageQuit/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _02.RageQuit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string currnetChars = string.Empty;
+            string resultingStr = string.Empty;
+            
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentCh = input[i];
+                if (char.IsDigit(currentCh))
+                {
+                    string currChToStr = currentCh.ToString().ToUpper();
+                    int asd = int.Parse(currentCh);
+                }
+                else
+                {
+                    currnetChars += currentCh;
+                }
+            }
+        }
+    }
+}
