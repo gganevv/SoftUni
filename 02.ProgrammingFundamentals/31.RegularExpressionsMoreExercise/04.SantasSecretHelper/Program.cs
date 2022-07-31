525fe7a475eddd7f1f5ecc35c957b0f2b3afe6b2
Georgi <bulbrainn@gmail.com>
2022-07-31 16:42:17 +0300

    Added Santa's Secret Helper

---
+++ 02.ProgrammingFundamentals/31.RegularExpressionsMoreExercise/04.SantasSecretHelper/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _04.SantasSecretHelper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            List<string> names = new List<string>();
+            string message = Console.ReadLine();
+            string pattern = @"@(?<name>[A-Za-z]+)[^@\-!:>]+!(?<behavior>[G])!";
+
+            while (message != "end")
+            {
+                string decryptedMessage = string.Empty;
+                for (int i = 0; i < message.Length; i++)
+                {
+                    decryptedMessage += (char)(message[i] - key);
+                }
+
+                Match namesMatch = Regex.Match(decryptedMessage, pattern);
+                if (namesMatch.Success)
+                {
+                    names.Add(namesMatch.Groups["name"].Value);
+                }
+
+                message = Console.ReadLine();
+            }
+
+            names.ForEach(Console.WriteLine);
+        }
+    }
+}
