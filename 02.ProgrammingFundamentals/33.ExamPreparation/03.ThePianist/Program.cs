e49852ad040968d7f9051c149b1248a7edea24ea
Georgi <bulbrainn@gmail.com>
2022-08-02 21:16:23 +0300

    Moved Class Piece to different File

--- 02.ProgrammingFundamentals/33.ExamPreparation/03.ThePianist/Program.cs
+++ 02.ProgrammingFundamentals/33.ExamPreparation/03.ThePianist/Program.cs
@@ -88,23 +88,4 @@ namespace _03.ThePianist
             }
         }
     }
-
-    public class Piece
-    {
-        public Piece(string name, string composer, string key)
-        {
-            Name = name;
-            Composer = composer;
-            Key = key;
-        }
-
-        public string Name { get; set; }
-        public string Composer { get; set; }
-        public string Key { get; set; }
-
-        public override string ToString()
-        {
-            return $"{Name} -> Composer: {Composer}, Key: {Key}";
-        }
-    }
 }
