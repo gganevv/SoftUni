3eecf6ff7af5beb640b512a788ca74f9c5fa4155
Georgi <bulbrainn@gmail.com>
2022-08-04 02:21:58 +0300

    Added Ad Astra

---
+++ 02.ProgrammingFundamentals/37.ExamPractice/01.FinalExam1/02.AdAstra/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02.AdAstra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalCalories = 0;
+            string input = Console.ReadLine();
+            string productPattern = @"([#|])(?<productName>[A-Za-z ]+)\1(?<day>\d{2})\/(?<month>\d{2})\/(?<year>\d{2})\1(?<calories>\d{1,4})\1";
+            var products = Regex.Matches(input, productPattern);
+
+            foreach (Match product in products)
+            {
+                totalCalories += int.Parse(product.Groups["calories"].Value);
+            }
+
+            Console.WriteLine($"You have food to last you for: {totalCalories / 2000} days!");
+            
+            foreach (Match product in products)
+            {
+                Console.WriteLine($"Item: {product.Groups["productName"].Value}, Best before: {product.Groups["day"].Value}/{product.Groups["month"].Value}/{product.Groups["year"].Value}, Nutrition: {product.Groups["calories"].Value}");
+            }
+
+        }
+    }
+}
