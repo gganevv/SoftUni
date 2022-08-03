73e47228c7b94b9ba2289e8da8cbd6efe358eee9
Georgi <bulbrainn@gmail.com>
2022-08-04 01:06:19 +0300

    Added Parking System

---
+++ 02.ProgrammingFundamentals/36.BasicWebProject/Skeleton/ParkingSystem/Controllers/CarController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParkingSystem.Data;
+using ParkingSystem.Data.Models;
+using System.Linq;
+
+namespace ParkingSystem.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCar(Car car)
+        {
+            DataAccess.Cars.Add(car);
+
+            return Redirect("/");
+        }
+
+        [HttpPost]
+        public IActionResult DeleteCar(string plateNumber)
+        {
+            var car = DataAccess.Cars.FirstOrDefault(x => x.PlateNumber == plateNumber);
+            DataAccess.Cars.Remove(car);
+
+            return Redirect("/");
+        }
+    }
+}
---
+++ 02.ProgrammingFundamentals/36.BasicWebProject/Skeleton/ParkingSystem/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ParkingSystem.Data;
+using ParkingSystem.Data.Models;
+using System.Linq;
+
+namespace ParkingSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(DataAccess.Cars);
+        }
+
+        
+    }
+}
