2cae86b72af675fae3e1d7c12495ce20ae2f435e
Georgi <bulbrainn@gmail.com>
2022-08-04 21:50:15 +0300

    Added Destination Mapper

---
+++ 02.ProgrammingFundamentals/37.ExamPractice/02.FinalExam2/02.DestinationMapper/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02.DestinationMapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"([=\/])(?<destination>[A-Z][A-Za-z]{2,})\1";
+            List<string> destinations = new List<string>();
+            int travelPoints = 0;
+
+            var matches = Regex.Matches(input, pattern);
+            foreach (Match match in matches)
+            {
+                destinations.Add(match.Groups["destination"].Value);
+                travelPoints += match.Groups["destination"].Value.Length;
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ", destinations)}");
+            Console.WriteLine($"Travel Points: {travelPoints}");
+        }
+    }
+}
