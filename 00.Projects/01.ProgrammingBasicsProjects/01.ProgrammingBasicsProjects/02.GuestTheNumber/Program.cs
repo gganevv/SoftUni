54a3e710495f684ffec6d776d4d3c2100af90155
Georgi <bulbrainn@gmail.com>
2022-08-17 23:17:39 +0300

    Added Guess The Nuber

---
+++ 00.Projects/01.ProgrammingBasicsProjects/01.ProgrammingBasicsProjects/02.GuestTheNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _02.GuestTheNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int computerNumber = random.Next(1, 101);
+            Console.WriteLine("Guess a number (1-100):");
+
+            int playerNumber = int.Parse(Console.ReadLine());
+            while (playerNumber != computerNumber)
+            {
+                if (playerNumber < computerNumber)
+                {
+                    Console.WriteLine("Too low");
+                }
+                else if (playerNumber > computerNumber)
+                {
+                    Console.WriteLine("Too high");
+                }
+                Console.WriteLine("Guess a number (1-100):");
+                playerNumber = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("You guessed it!");
+        }
+    }
+}
