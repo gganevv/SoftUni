e42f7c4fd4a085df0be52792034310fa0964d8c5
Georgi <bulbrainn@gmail.com>
2022-08-17 22:43:16 +0300

    Added Boss Rush

---
+++ 02.ProgrammingFundamentals/37.Exam/37.Exam/02.BossRush/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02.BossRush
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfInputs = int.Parse(Console.ReadLine());
+            string validationPattern = @"\|(?<name>[A-Z]{4,})\|:#(?<title>[A-Za-z]+ [A-Za-z]+)#";
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                string currentText = Console.ReadLine();
+                Match match = Regex.Match(currentText, validationPattern);
+                if (match.Success)
+                {
+                    string name = match.Groups["name"].Value;
+                    string title = match.Groups["title"].Value;
+                    Console.WriteLine($"{name}, The {title}");
+                    Console.WriteLine($">> Strength: {name.Length}");
+                    Console.WriteLine($">> Armor: {title.Length}");
+                }
+                else
+                {
+                    Console.WriteLine("Access denied!");
+                }
+            }
+        }
+    }
+}
