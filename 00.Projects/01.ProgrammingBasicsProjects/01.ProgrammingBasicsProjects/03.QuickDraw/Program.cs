5ae57b747e922caf30daa333f732a45d6c2b639e
Georgi <bulbrainn@gmail.com>
2022-08-19 22:34:01 +0300

    Added Quick Draw

--- 00.Projects/01.ProgrammingBasicsProjects/01.ProgrammingBasicsProjects/03.QuickDraw/Program.cs
+++ 00.Projects/01.ProgrammingBasicsProjects/01.ProgrammingBasicsProjects/03.QuickDraw/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace _03.QuickDraw
 {
@@ -6,25 +7,31 @@ namespace _03.QuickDraw
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Quick Draw");
-            Console.WriteLine();
-            Console.WriteLine(@"Face your opponent and wait for the signal.
+            Random random = new Random();
+            string menu = @"Quick Draw
+Face your opponent and wait for the signal.
 Once the signal is given, shoot your opponent by pressong [space]
-before they shoot you. It's all about your reaction time.");
-            Console.WriteLine("Choose Your Opponent:");
-            Console.WriteLine();
-            Console.WriteLine(@"[1] Easy....1000 milliseconds
+before they shoot you. It's all about your reaction time.
+
+Choose Your Opponent:
+[1] Easy....1000 milliseconds
 [2] Medium...500 milliseconds
 [3] Hard.....250 milliseconds
-[4] Harder...125 milliseconds");
-
-            int difficultity = int.Parse(Console.ReadLine());
-            Console.Clear();
+[4] Harder...125 milliseconds";
 
             string wait = @"                                                    
                         _O                          O_            
                        |/|_          wait          _|\|           
                        /\                            /\           
+                      /  |                          |  \          
+            ------------------------------------------------------";
+            string fire = @"
+                                           
+                                   ********                       
+                                   * FIRE *                       
+                        _O         ********         O_            
+                       |/|_                        _|\|           
+                       /\          spacebar          /\           
                       /  |                          |  \          
             ------------------------------------------------------";
             string loseSlow = @"                                         
@@ -51,7 +58,94 @@ before they shoot you. It's all about your reaction time.");
                   /  |                          /             
             ------------------------------------------------------";
 
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine(menu);
+                TimeSpan timeSpan;
 
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        timeSpan = TimeSpan.FromMilliseconds(1000);
+                        break;
+                    case "2":
+                        timeSpan = TimeSpan.FromMilliseconds(500);
+                        break;
+                    case "3":
+                        timeSpan = TimeSpan.FromMilliseconds(250);
+                        break;
+                    case "4":
+                        timeSpan = TimeSpan.FromMilliseconds(125);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input");
+                        continue;
+                }
+                Console.Clear();
+
+                TimeSpan signal = TimeSpan.FromMilliseconds(random.Next(5000, 10000));
+                Console.WriteLine(wait);
+
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Restart();
+
+                bool tooFast = false;
+
+                while (stopwatch.Elapsed < signal && !tooFast)
+                {
+                    if (Console.KeyAvailable && Console.ReadKey().Key == ConsoleKey.Spacebar)
+                    {
+                        tooFast = true;
+                    }
+                }
+
+                Console.Clear();
+
+                if (tooFast)
+                {
+                    Console.WriteLine(loseFast);
+                }
+                else
+                {
+                    Console.WriteLine(fire);
+                    stopwatch.Restart();
+
+                    bool tooSlow = true;
+
+                    TimeSpan reactionTime = default;
+
+                    while (stopwatch.Elapsed < timeSpan && tooSlow)
+                    {
+                        if (Console.KeyAvailable && Console.ReadKey().Key == ConsoleKey.Spacebar)
+                        {
+                            tooSlow = false;
+                            reactionTime = stopwatch.Elapsed;
+                        }
+                    }
+
+                    Console.Clear();
+
+                    if (tooSlow)
+                    {
+                        Console.WriteLine(loseSlow);
+                    }
+                    else
+                    {
+                        Console.WriteLine(win);
+                        Console.WriteLine($"Reaction Time: {reactionTime.TotalMilliseconds} milliseconds");
+                    }
+                }
+
+                Console.Write("Press [1] to Play Again or [2] to quit: ");
+                string playOrQuit = Console.ReadLine();
+
+                if (playOrQuit == "2")
+                {
+                    break;
+                }
+            }            
         }
     }
 }
