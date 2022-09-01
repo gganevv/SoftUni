e032f1a08817e2165e509ea2ac10ae25debb937f
Georgi <bulbrainn@gmail.com>
2022-09-01 19:45:02 +0300

    Added Tic Tac Toe

---
+++ 00.Projects/02.FundamentalsProjects/02.FundamentalsProjects/03.TicTacToe/TicTacToeForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace _03.TicTacToe
+{
+    partial class TicTacToeForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonPanel = new System.Windows.Forms.Panel();
+            this.playerButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(664, 65);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Player";
+            // 
+            // buttonPanel
+            // 
+            this.buttonPanel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonPanel.Location = new System.Drawing.Point(35, 31);
+            this.buttonPanel.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonPanel.Name = "buttonPanel";
+            this.buttonPanel.Size = new System.Drawing.Size(600, 554);
+            this.buttonPanel.TabIndex = 1;
+            // 
+            // playerButton
+            // 
+            this.playerButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.playerButton.Location = new System.Drawing.Point(664, 101);
+            this.playerButton.Name = "playerButton";
+            this.playerButton.Size = new System.Drawing.Size(133, 123);
+            this.playerButton.TabIndex = 0;
+            this.playerButton.Text = "X";
+            this.playerButton.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(699, 240);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TicTacToeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 625);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.playerButton);
+            this.Controls.Add(this.buttonPanel);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "TicTacToeForm";
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel buttonPanel;
+        private System.Windows.Forms.Button playerButton;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ 00.Projects/02.FundamentalsProjects/02.FundamentalsProjects/03.TicTacToe/TicTacToeForm.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _03.TicTacToe
+{
+    public partial class TicTacToeForm : Form
+    {
+        public TicTacToeForm()
+        {
+            InitializeComponent();
+            GenerateButtons();
+        }
+
+        Button[,] buttons = new Button[3, 3];
+
+        private void GenerateButtons()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    buttons[i, j] = new Button();
+                    buttons[i, j].Size = new Size(175, 175);
+                    buttons[i, j].Location = new Point(i * 175, j * 175);
+                    buttons[i, j].FlatStyle = FlatStyle.Flat;
+                    buttons[i, j].Font = new Font(DefaultFont.FontFamily, 80, FontStyle.Bold);
+
+                    buttons[i, j].Click += new EventHandler(OnButtonClick);
+                    this.buttonPanel.Controls.Add(buttons[i, j]);
+                }
+            }
+        }
+
+        private void OnButtonClick(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            if (button.Text != "")
+            {
+                return;
+            }
+
+            button.Text = this.playerButton.Text;
+
+            TogglePlayer();
+        }
+
+        private void TogglePlayer()
+        {
+            if (this.playerButton.Text == "X")
+            {
+                this.playerButton.Text = "O";
+            }
+            else
+            {
+                this.playerButton.Text = "X";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
