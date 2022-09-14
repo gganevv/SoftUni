022c7bd2c0c08a86f70f1e532932bdbb2c251e7d
Georgi <bulbrainn@gmail.com>
2022-09-14 23:52:54 +0300

    Added Traffic Jam

---
+++ 03.C#Advanced/03.StackAndQueues/08.TrafficJam/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08.TrafficJam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int carsToPass = int.Parse(Console.ReadLine());
+            int carsCounter = 0;
+            Queue<string> carsQueue = new Queue<string>();
+            string input = Console.ReadLine();
+            while (input != "end")
+            {
+                if (input == "green")
+                {
+                    for (int i = 0; i < carsToPass; i++)
+                    {
+                        if (carsQueue.Count > 0)
+                        {
+                            Console.WriteLine($"{carsQueue.Dequeue()} passed!");
+                            carsCounter++;
+                        }
+                    }
+                }
+                else
+                {
+                    carsQueue.Enqueue(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{carsCounter} cars passed the crossroads.");
+        }
+    }
+}
