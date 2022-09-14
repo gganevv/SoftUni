d8ad2ba03a5aed5fc1482eb82ce4aba97e8ded72
Georgi <bulbrainn@gmail.com>
2022-09-14 22:50:25 +0300

    Added Matching Brackets

---
+++ 03.C#Advanced/03.StackAndQueues/04.MatchingBrackets/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.MatchingBrackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                if (expression[i] == ')')
+                {
+                    int startIndex = stack.Pop();
+                    Console.WriteLine(expression.Substring(startIndex, i - startIndex + 1));
+                }
+            }
+        }
+    }
+}
