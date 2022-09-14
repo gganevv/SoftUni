9ace4e81b8ea0bc3ef75cdaef264fbb4ece697fb
Georgi <bulbrainn@gmail.com>
2022-09-14 22:30:36 +0300

    Added Simple Calculator

---
+++ 03.C#Advanced/03.StackAndQueues/03.SimpleCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().Reverse().ToArray();
+            Stack<string> expression = new Stack<string>(input);
+
+            while (expression.Count > 1)
+            {
+                int firstNum = int.Parse(expression.Pop());
+                string sign = expression.Pop();
+                int secondNum = int.Parse(expression.Pop());
+                if (sign == "+")
+                {
+                    expression.Push((secondNum + firstNum).ToString());
+                }
+                else if(sign == "-")
+                {
+                    expression.Push((firstNum - secondNum).ToString());
+                }
+            }
+
+            Console.WriteLine(expression.Pop());
+        }
+    }
+}
