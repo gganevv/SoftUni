e56c38c38d0293f55e2476acf939cc8b7d0f0491
Georgi <bulbrainn@gmail.com>
2022-09-14 22:04:09 +0300

    Added Stack Sum

---
+++ 03.C#Advanced/03.StackAndQueues/02.StackSum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.StackSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>(nums);
+
+            string input = Console.ReadLine().ToLower();
+            while (input != "end")
+            {
+                string[] inputArgs = input.Split();
+                if (inputArgs[0] == "add")
+                {
+                    stack.Push(int.Parse(inputArgs[1]));
+                    stack.Push(int.Parse(inputArgs[2]));
+                }
+                else if (inputArgs[0] == "remove")
+                {
+                    int count = int.Parse(inputArgs[1]);
+                    if (stack.Count > count)
+                    {
+                        for (int i = 0; i < count; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+
+                input = Console.ReadLine().ToLower();
+            }
+
+            Console.WriteLine($"Sum: {stack.Sum()}");
+        }
+    }
+}
