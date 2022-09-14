6f95a81279cd4f7e370a9c1b7735dc8977287506
Georgi <bulbrainn@gmail.com>
2022-09-14 23:07:12 +0300

    Added Print Even Numbers

---
+++ 03.C#Advanced/03.StackAndQueues/05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.PrintEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> nums = new Queue<int>(input);
+            List<int> evenNumbers = new List<int>();
+
+            while (nums.Count > 0)
+            {
+                int currentNum = nums.Dequeue();
+                if (currentNum % 2 == 0)
+                {
+                    evenNumbers.Add(currentNum);
+                }
+            }
+
+            Console.WriteLine(String.Join(", ", evenNumbers));
+        }
+    }
+}
