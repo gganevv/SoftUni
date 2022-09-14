a8dc3c8900ab599df80695f772d69123e72267f3
Georgi <bulbrainn@gmail.com>
2022-09-14 21:06:13 +0300

    Added Reverse a String

---
+++ 03.C#Advanced/03.StackAndQueues/01.ReverseAString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.ReverseAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                stack.Push(input[i]);
+            }
+
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
