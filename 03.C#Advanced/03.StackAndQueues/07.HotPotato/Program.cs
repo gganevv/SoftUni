fea920e91e0ff4ece71d97012c32db8f50b57932
Georgi <bulbrainn@gmail.com>
2022-09-14 23:51:46 +0300

    Added Hot Potato

---
+++ 03.C#Advanced/03.StackAndQueues/07.HotPotato/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.HotPotato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kids = Console.ReadLine().Split();
+            Queue<string> queue = new Queue<string>(kids);
+            int count = int.Parse(Console.ReadLine());
+            while (queue.Count > 1)
+            {
+                for (int i = 0; i < count - 1; i++)
+                {
+                    string currentKid = queue.Dequeue();
+                    queue.Enqueue(currentKid);
+                }
+                Console.WriteLine($"Removed {queue.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
