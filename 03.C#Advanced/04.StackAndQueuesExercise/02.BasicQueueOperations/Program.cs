e815e62cae3f2f9a06dbdc95af61b6a8a320ee31
Georgi <bulbrainn@gmail.com>
2022-09-16 22:17:00 +0300

    Added Basic Queue Operations

--- 03.C#Advanced/04.StackAndQueuesExercise/02.BasicQueueOperations/Program.cs
+++ 03.C#Advanced/04.StackAndQueuesExercise/02.BasicQueueOperations/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace _02.BasicQueueOperations
@@ -7,7 +8,35 @@ namespace _02.BasicQueueOperations
     {
         static void Main(string[] args)
         {
+            int[] inputArgs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int elementsToEnqueue = inputArgs[0];
+            int elementsToDequeue = inputArgs[1];
+            int elementToLook = inputArgs[2];
+            int[] startingElements = Console.ReadLine().Split().Select(int.Parse).ToArray();
             
+            Queue<int> nums = new Queue<int>();
+            for (int i = 0; i < elementsToEnqueue; i++)
+            {
+                nums.Enqueue(startingElements[i]);
+            }
+
+            for (int i = 0; i < elementsToDequeue; i++)
+            {
+                nums.Dequeue();
+            }
+
+            if (nums.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (nums.Contains(elementToLook))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(nums.Min());
+            }
         }
     }
 }
