aeee2dc736d35030d7d9b35890346f9e4bfb7027
Georgi <bulbrainn@gmail.com>
2022-09-17 17:04:44 +0300

    Added Balanced Parentheses

---
+++ 03.C#Advanced/04.StackAndQueuesExercise/08.BalancedParwntheses/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08.BalancedParwntheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] parenthesesToStack = Console.ReadLine().ToCharArray();
+            Stack<char> parentheses = new Stack<char>();
+            for (int i = 0; i < parenthesesToStack.Length; i++)
+            {
+                char currentCh = parenthesesToStack[i];
+                if (currentCh == '(' || currentCh == '[' || currentCh == '{')
+                {
+                    parentheses.Push(currentCh);
+                }
+                else
+                {
+                    CheckLastChar(parentheses, currentCh);
+                }
+            }
+            if (parentheses.Count == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+
+        private static void CheckLastChar(Stack<char> parentheses, char currentCh)
+        {
+            if (parentheses.Count > 0)
+            {
+                char lastCh = parentheses.Peek();
+                if (currentCh == ')' && lastCh == '(')
+                {
+                    parentheses.Pop();
+                }
+                else if (currentCh == ']' && lastCh == '[')
+                {
+                    parentheses.Pop();
+                }
+                else if (currentCh == '}' && lastCh == '{')
+                {
+                    parentheses.Pop();
+                }
+                else
+                {
+                    parentheses.Push(currentCh);
+                }
+            }
+            else
+            {
+                parentheses.Push(currentCh);
+            }
+        }
+    }
+}
