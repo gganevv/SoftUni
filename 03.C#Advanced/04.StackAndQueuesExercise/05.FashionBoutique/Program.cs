caf89e6d7707735e3c8513fde846132de98e5b31
Georgi <bulbrainn@gmail.com>
2022-09-17 13:48:56 +0300

    Added Fashion Boutique

---
+++ 03.C#Advanced/04.StackAndQueuesExercise/05.FashionBoutique/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.FashionBoutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothesToStack = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> clothes = new Stack<int>(clothesToStack);
+            int rackCapacity = int.Parse(Console.ReadLine());
+            int rackCounter = 1;
+            int currentRack = 0;
+
+            while (clothes.Count > 0)
+            {
+                int currentCloth = clothes.Peek();
+                if (currentRack + currentCloth <= rackCapacity)
+                {
+                    currentRack += clothes.Pop();
+                }
+                else
+                {
+                    rackCounter++;
+                    currentRack = 0;
+                }
+            }
+
+            Console.WriteLine(rackCounter);
+        }
+    }
+}
