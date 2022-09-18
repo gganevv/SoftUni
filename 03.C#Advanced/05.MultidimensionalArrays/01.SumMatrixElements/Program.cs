1b490b16c13a9ad62973c38a89cfd169470f721c
Georgi <bulbrainn@gmail.com>
2022-09-18 23:35:36 +0300

    Added Sum Matrix Elements

---
+++ 03.C#Advanced/05.MultidimensionalArrays/01.SumMatrixElements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.SumMatrixElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraySize = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = arraySize[0];
+            int cols = arraySize[1];
+            int[,] matrix = new int[rows, cols];
+            int sum = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] colElements = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+
+            foreach (var n in matrix)
+            {
+                sum += n;
+            }
+
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(sum);
+        }
+    }
+}
