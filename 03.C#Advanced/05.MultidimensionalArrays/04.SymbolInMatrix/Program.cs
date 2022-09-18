e317faf10d995f85c897c4829507c6d6cab7b372
Georgi <bulbrainn@gmail.com>
2022-09-19 00:41:25 +0300

    Added Symbol In Matrix

---
+++ 03.C#Advanced/05.MultidimensionalArrays/04.SymbolInMatrix/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _04.SymbolInMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[matrixSize, matrixSize];
+
+            for (int row = 0; row < matrixSize; row++)
+            {
+                char[] colElements = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < matrixSize; col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+
+            char c = char.Parse(Console.ReadLine());
+
+            for (int row = 0; row < matrixSize; row++)
+            {
+                for (int col = 0; col < matrixSize; col++)
+                {
+                    if (c == matrix[row, col])
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{c} does not occur in the matrix");
+        }
+    }
+}
