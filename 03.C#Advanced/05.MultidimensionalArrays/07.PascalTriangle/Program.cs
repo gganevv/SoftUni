91375702307d517509b6bced80fa2390959dfd9d
Georgi <bulbrainn@gmail.com>
2022-09-19 21:03:08 +0300

    Added Pascal Triangle

---
+++ 03.C#Advanced/05.MultidimensionalArrays/07.PascalTriangle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _07.PascalTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            long[][] triangle = new long[rows][];
+            triangle[0] = new long[] { 1 };
+
+            for (int row = 1; row < rows; row++)
+            {
+                triangle[row] = new long[row + 1];
+                triangle[row][0] = 1;
+                for (int col = 1; col < row; col++)
+                {
+                    triangle[row][col] = triangle[row - 1][col - 1] + triangle[row - 1][col];
+                }
+                triangle[row][row] = 1;
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                Console.WriteLine(String.Join(" ", triangle[row]));
+            }
+        }
+    }
+}
