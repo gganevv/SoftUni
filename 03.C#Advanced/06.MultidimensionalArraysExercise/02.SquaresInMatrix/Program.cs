b564c6bedc1b314a759c76a910c886870ca162ae
Georgi <bulbrainn@gmail.com>
2022-09-20 22:10:40 +0300

    Added Squares In Matrix

---
+++ 03.C#Advanced/06.MultidimensionalArraysExercise/02.SquaresInMatrix/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SquaresInMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rows = size[0];
+            int cols = size[1];
+            char[,] matrix = new char[rows, cols];
+            int equalSubSquares = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                char[] colElements = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+
+            if (rows < 2 || cols < 2)
+            {
+                Console.WriteLine(0);
+            }
+
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int col = 0; col < cols - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1]
+                        && matrix[row, col] == matrix[row + 1, col]
+                        && matrix[row, col] == matrix[row + 1, col + 1])
+                    {
+                        equalSubSquares++;
+                    }
+                }
+            }
+
+            Console.WriteLine(equalSubSquares);
+        }
+    }
+}
