19a808e2d17009cafcbf5c68386676cad9ca93df
Georgi <bulbrainn@gmail.com>
2022-09-22 23:57:04 +0300

    Added Count Same Values In Array

---
+++ 03.C#Advanced/07.SetsAndDictionariesAdvanced/01.CountSameValuesInArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.CountSameValuesInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<double, int> numbers = new Dictionary<double, int>();
+            double[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => double.Parse(x)).ToArray();
+            for (int i = 0; i < input.Length; i++)
+            {
+                double currentNumber = input[i];
+                if (!numbers.ContainsKey(currentNumber))
+                {
+                    numbers.Add(currentNumber, 0);
+                }
+
+                numbers[currentNumber]++;
+            }
+
+            foreach (var num in numbers)
+            {
+                Console.WriteLine($"{num.Key} - {num.Value} times");
+            }
+        }
+    }
+}
