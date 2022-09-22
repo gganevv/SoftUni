bb22d403819be0bff6e27bb4875579ae013feb3f
Georgi <bulbrainn@gmail.com>
2022-09-23 00:38:15 +0300

    Added Average Student Grades

---
+++ 03.C#Advanced/07.SetsAndDictionariesAdvanced/02.AverageStudentGrades/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.AverageStudentGrades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfInputs = int.Parse(Console.ReadLine());
+            Dictionary<string, List<decimal>> students = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = input[0];
+                decimal grade = decimal.Parse(input[1]);
+
+                if (!students.ContainsKey(name))
+                {
+                    students.Add(name, new List<decimal>());
+                }
+                students[name].Add(grade);
+            }
+
+            foreach (var student in students)
+            {
+                Console.Write($"{student.Key} -> ");
+                foreach (var grade in student.Value)
+                {
+                    Console.Write($"{grade:f2} ");
+                }
+                Console.WriteLine($"(avg: {student.Value.Average():f2})");
+            }
+        }
+    }
+}
