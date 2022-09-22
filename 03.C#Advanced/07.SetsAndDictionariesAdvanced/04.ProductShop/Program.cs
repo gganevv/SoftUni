d0c0fbad573077bb66c852db3e13f3ae0b23bd87
Georgi <bulbrainn@gmail.com>
2022-09-23 01:02:48 +0300

    Added Product Shop

---
+++ 03.C#Advanced/07.SetsAndDictionariesAdvanced/04.ProductShop/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.ProductShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, double>> shops = new Dictionary<string, Dictionary<string, double>>();
+
+            string input = Console.ReadLine();
+            while (input != "Revision")
+            {
+                string[] inputArgs = input.Split(", ");
+                string shop = inputArgs[0];
+                string product = inputArgs[1];
+                double price = double.Parse(inputArgs[2]);
+
+                if (!shops.ContainsKey(shop))
+                {
+                    shops.Add(shop, new Dictionary<string, double>());
+                }
+
+                shops[shop].Add(product, price);
+
+                input = Console.ReadLine();
+            }
+
+            shops = shops.OrderBy(x => x.Key).ToDictionary(k => k.Key, v => v.Value);
+
+            foreach (var shop in shops)
+            {
+                Console.WriteLine($"{shop.Key}->");
+                foreach (var product in shop.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
+        }
+    }
+}
