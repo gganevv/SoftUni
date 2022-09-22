19718d06c12eb92c0ee0da98d6ab35bae152affc
Georgi <bulbrainn@gmail.com>
2022-09-23 00:45:51 +0300

    Added Larguest 3 Numbers

---
+++ 03.C#Advanced/07.SetsAndDictionariesAdvanced/03.Larguest3Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Larguest3Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToList();
+            nums = nums.OrderByDescending(x => x).Take(3).ToList();
+
+            foreach (var num in nums)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
