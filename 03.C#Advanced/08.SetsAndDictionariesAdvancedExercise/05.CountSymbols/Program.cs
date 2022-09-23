749ebd6e4e8a75c40b06b0a6751a4e9a5801da20
Georgi <bulbrainn@gmail.com>
2022-09-24 02:12:58 +0300

    Added Count Symbols

---
+++ 03.C#Advanced/08.SetsAndDictionariesAdvancedExercise/05.CountSymbols/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.CountSymbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<char, int> chars = new SortedDictionary<char, int>();
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currnetC = text[i];
+                if (!chars.ContainsKey(currnetC))
+                {
+                    chars.Add(currnetC, 0);
+                }
+                chars[currnetC]++;
+            }
+
+            foreach (var c in chars)
+            {
+                Console.WriteLine($"{c.Key}: {c.Value} time/s");
+            }
+        }
+    }
+}
