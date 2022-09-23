d5c3b5b38419825d4c892e866e7b72617c728b38
Georgi <bulbrainn@gmail.com>
2022-09-23 18:46:30 +0300

    Added Parking Lot

---
+++ 03.C#Advanced/07.SetsAndDictionariesAdvanced/07.ParkingLot/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.ParkingLot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> parking = new HashSet<string>();
+
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                string[] commandArgs = input.Split(", ");
+                string command = commandArgs[0];
+                string plate = commandArgs[1];
+
+                if (command == "IN")
+                {
+                    parking.Add(plate);
+                }
+                else
+                {
+                    parking.Remove(plate);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (parking.Count > 0)
+            {
+                foreach (var plate in parking)
+                {
+                    Console.WriteLine(plate);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
