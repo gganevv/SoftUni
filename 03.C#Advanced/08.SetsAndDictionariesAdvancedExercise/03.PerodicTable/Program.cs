7428e89acc7f1cdaaf23f053ff7006e6161aa505
Georgi <bulbrainn@gmail.com>
2022-09-24 01:28:23 +0300

    Added Periodic Table

---
+++ 03.C#Advanced/08.SetsAndDictionariesAdvancedExercise/03.PerodicTable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.PerodicTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfLines = int.Parse(Console.ReadLine());
+            SortedSet<string> elements = new SortedSet<string>();
+
+            for (int i = 0; i < countOfLines; i++)
+            {
+                string[] lineElements = Console.ReadLine().Split(' ');
+                elements.UnionWith(lineElements);
+            }
+
+            Console.WriteLine(string.Join(" ", elements));
+        }
+    }
+}
