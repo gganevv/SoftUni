dca5eb67c316475538be20aaea1a57b639ebf8c3
Georgi <bulbrainn@gmail.com>
2022-09-24 01:15:49 +0300

    Added Unique Usernames

---
+++ 03.C#Advanced/08.SetsAndDictionariesAdvancedExercise/01.UniqueUserNames/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.UniqueUserNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> userNames = new HashSet<string>();
+            int inputNumber = int.Parse(Console.ReadLine());
+            for (int i = 0; i < inputNumber; i++)
+            {
+                userNames.Add(Console.ReadLine());
+            }
+
+            foreach (var userName in userNames)
+            {
+                Console.WriteLine(userName);
+            }
+        }
+    }
+}
