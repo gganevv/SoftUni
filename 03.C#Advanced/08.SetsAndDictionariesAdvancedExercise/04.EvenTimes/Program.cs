39c7b95a938c35d30f007f90fe57329fd6ee342b
Georgi <bulbrainn@gmail.com>
2022-09-24 02:05:12 +0300

    Added Even Times

---
+++ 03.C#Advanced/08.SetsAndDictionariesAdvancedExercise/04.EvenTimes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.EvenTimes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfNumbers = int.Parse(Console.ReadLine());
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+            for (int i = 0; i < countOfNumbers; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                if (!numbers.ContainsKey(currentNum))
+                {
+                    numbers.Add(currentNum, 0);
+                }
+
+                numbers[currentNum]++;
+            }
+
+            Console.WriteLine(numbers.First(x => x.Value % 2 == 0).Key);
+        }
+    }
+}
