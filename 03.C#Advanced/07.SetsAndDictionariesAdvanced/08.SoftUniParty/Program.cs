5039a5b3161137cf914c5b6af628ff157201bf9f
Georgi <bulbrainn@gmail.com>
2022-09-23 19:14:01 +0300

    Added SoftUni Party

---
+++ 03.C#Advanced/07.SetsAndDictionariesAdvanced/08.SoftUniParty/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.SoftUniParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> people = new HashSet<string>();
+
+            HashSet<string> VIPnotComing = new HashSet<string>();
+            HashSet<string> notVIPnotComing = new HashSet<string>();
+
+
+            string invited = Console.ReadLine();
+            while (invited != "PARTY")
+            {
+                people.Add(invited);
+
+                invited = Console.ReadLine();
+            }
+            string coming = Console.ReadLine();
+            while (coming != "END")
+            {
+                people.Remove(coming);
+
+                coming = Console.ReadLine();
+            }
+
+            foreach (var person in people)
+            {
+                if (char.IsDigit(person[0]))
+                {
+                    VIPnotComing.Add(person);
+                }
+                else
+                {
+                    notVIPnotComing.Add(person);
+                }
+            }
+
+            Console.WriteLine(VIPnotComing.Count + notVIPnotComing.Count);
+            foreach (var person in VIPnotComing)
+            {
+                Console.WriteLine(person);
+            }
+            foreach (var person in notVIPnotComing)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
