57e559795a74f79c2113bdd9c12f3a0c29c3f92d
Georgi <bulbrainn@gmail.com>
2022-09-24 12:23:45 +0300

    Added The V-Logger

---
+++ 03.C#Advanced/08.SetsAndDictionariesAdvancedExercise/07.TheV-Logger/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.TheV_Logger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Vlogger> vloggers = new List<Vlogger>();
+            string input = Console.ReadLine();
+
+            while (input != "Statistics")
+            {
+                string[] inputArgs = input.Split(" ");
+                string vloggerName = inputArgs[0];
+                string command = inputArgs[1];
+
+                if (command == "joined")
+                {
+                    AddVlogger(vloggers, vloggerName);
+                }
+                else if (command == "followed")
+                {
+                    string followedVloggerName = inputArgs[2];
+                    FollowVlogger(vloggers, followedVloggerName, vloggerName);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            PrintStatistics(vloggers);
+        }
+
+        private static void AddVlogger(List<Vlogger> vloggers, string vloggerName)
+        {
+            if (!VloggerExists(vloggerName, vloggers))
+            {
+                vloggers.Add(new Vlogger(vloggerName));
+            }
+        }
+
+        private static void FollowVlogger(List<Vlogger> vloggers, string followedVloggerName, string vloggerName)
+        {
+            if (VloggerExists(vloggerName, vloggers)
+                && VloggerExists(followedVloggerName, vloggers)
+                && vloggerName != followedVloggerName)
+            {
+                Vlogger vlogger = vloggers.First(x => x.Name == vloggerName);
+                Vlogger followedVlogger = vloggers.First(x => x.Name == followedVloggerName);
+                if (!vlogger.Following.Contains(followedVloggerName))
+                {
+                    vlogger.Following.Add(followedVloggerName);
+                    followedVlogger.Followers.Add(vloggerName);
+                }
+            }
+        }
+
+        private static void PrintStatistics(List<Vlogger> vloggers)
+        {
+            vloggers = vloggers.OrderByDescending(x => x.Followers.Count).ThenBy(x => x.Following.Count).ToList();
+
+            Console.WriteLine($"The V-Logger has a total of {vloggers.Count} vloggers in its logs.");
+            int counter = 1;
+            foreach (var vlogger in vloggers)
+            {
+                Console.WriteLine($"{counter}. {vlogger}");
+                if (counter == 1)
+                {
+                    foreach (var follower in vlogger.Followers.OrderBy(x => x))
+                    {
+                        Console.WriteLine($"*  {follower}");
+                    }
+                }
+                counter++;
+            }
+        }
+
+        private static bool VloggerExists(string vloggerName, List<Vlogger> vloggers)
+        {
+            return vloggers.Contains(vloggers.FirstOrDefault(x => x.Name == vloggerName));
+        }
+    }
+}
---
+++ 03.C#Advanced/08.SetsAndDictionariesAdvancedExercise/07.TheV-Logger/Vlogger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace _07.TheV_Logger
+{
+    public class Vlogger
+    {
+        public Vlogger(string name)
+        {
+            Name = name;
+            Followers = new List<string>();
+            Following = new List<string>();
+        }
+        public string Name { get; set; }
+        public List<string> Followers { get; set; }
+        public List<string> Following { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} : {Followers.Count} followers, {Following.Count} following";
+        }
+    }
+}
