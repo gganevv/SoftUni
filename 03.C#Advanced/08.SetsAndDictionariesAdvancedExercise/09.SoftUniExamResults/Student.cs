8f82217fa2c0af6c0a56cceb9257d3c49b5af72f
Georgi <bulbrainn@gmail.com>
2022-09-24 17:11:11 +0300

    Refactoring

---
+++ 03.C#Advanced/08.SetsAndDictionariesAdvancedExercise/09.SoftUniExamResults/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace _09.SoftUniExamResults
+{
+    public class Student
+    {
+        public Student(string name, Submission submission)
+        {
+            Name = name;
+            Submission = submission;
+        }
+        public string Name { get; set; }
+        public Submission Submission { get; set; }
+    }
+}
---
+++ 03.C#Advanced/08.SetsAndDictionariesAdvancedExercise/09.SoftUniExamResults/Submission.cs
@@ -0,0 +1,21 @@
+﻿namespace _09.SoftUniExamResults
+{
+    public class Submission
+    {
+        public Submission(string name, int points)
+        {
+            Name = name;
+            Points = points;
+        }
+
+        public Submission(string name)
+        {
+            Name = name;
+            Count = 1;
+        }
+
+        public string Name { get; set; }
+        public int Points { get; set; }
+        public int Count { get; set; }
+    }
+}
