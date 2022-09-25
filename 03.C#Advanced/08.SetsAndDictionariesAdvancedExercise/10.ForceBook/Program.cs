98a06679a99a34322fb89779b7d5be7f79c1566d
Georgi <bulbrainn@gmail.com>
2022-09-26 02:22:26 +0300

    Added Force Book

---
+++ 03.C#Advanced/08.SetsAndDictionariesAdvancedExercise/10.ForceBook/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10.ForceBook
+{
+    internal class Program
+    {
+        //80/100
+        static void Main(string[] args)
+        {
+            HashSet<Side> sides = new HashSet<Side>();
+
+            string input = Console.ReadLine();
+
+            while (input != "Lumpawaroo")
+            {
+                if (input.Contains("|"))
+                {
+                    string forceSide = input.Split(" | ").First();
+                    string user = input.Split(" | ").Last();
+                    if (!sides.Any(x => x.Name == forceSide))
+                    {
+                        sides.Add(new Side(forceSide));
+                    }
+                    Side side = sides.FirstOrDefault(x => x.Name == forceSide);
+
+                    if (!side.Memebers.Contains(user))
+                    {
+                        side.Memebers.Add(user);
+                    }
+                }
+                else if (input.Contains("->"))
+                {
+                    string user = input.Split(" -> ").First();
+                    string forceSide = input.Split(" -> ").Last();
+
+                    if (!sides.Any(x => x.Name == forceSide))
+                    {
+                        sides.Add(new Side(forceSide));
+                    }
+                    Side side = sides.FirstOrDefault(x => x.Name == forceSide);
+
+                    if (UserExist(user, sides))
+                    {
+                        side.Memebers.Add(user);
+                        sides.FirstOrDefault(x => x.Name != forceSide).Memebers.Remove(user);
+                    }
+                    else
+                    {
+                        side.Memebers.Add(user);
+                    }
+                    Console.WriteLine($"{user} joins the {forceSide} side!");
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var side in sides.OrderByDescending(x => x.Memebers.Count).ThenBy(x => x.Name))
+            {
+                if (side.Memebers.Count > 0)
+                {
+                    Console.WriteLine($"Side: {side.Name}, Members: {side.Memebers.Count}");
+                    foreach (var user in side.Memebers.OrderBy(x => x))
+                    {
+                        Console.WriteLine($"! {user}");
+                    }
+                }
+            }
+        }
+
+        private static bool UserExist(string user, HashSet<Side> sides)
+        {
+            foreach (var side in sides)
+            {
+                foreach (var u in side.Memebers)
+                {
+                    if (u == user)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+
+    internal class Side
+    {
+        public Side(string name)
+        {
+            Name = name;
+            Memebers = new List<string>();
+        }
+
+        public string Name { get; set; }
+        public List<String> Memebers { get; set; }
+    }
+}
