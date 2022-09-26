558b5d5f44a3b9e34e0e85d9ac5c74076124e728
Georgi <bulbrainn@gmail.com>
2022-09-26 19:21:34 +0300

    Added Odd Lines

---
+++ 03.C#Advanced/09.StreamsFilesAndDictionaries/01.OddLines/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace OddLines
+{
+    public class OddLines
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            var reader = new StreamReader(inputFilePath);
+            var writer = new StreamWriter(outputFilePath, false);
+
+            using (reader)
+            {
+                using (writer)
+                {
+                    string line = reader.ReadLine();
+                    int counter = 0;
+                    while (line != null)
+                    {
+                        counter++;
+                        if (counter % 2 != 0)
+                        {
+                            writer.WriteLine(line);
+                        }
+
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
