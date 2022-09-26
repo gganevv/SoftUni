e3fccf0c6d49af37007ab002f00db35a8a57d6cb
Georgi <bulbrainn@gmail.com>
2022-09-26 19:31:56 +0300

    Added Line Numbers

---
+++ 03.C#Advanced/09.StreamsFilesAndDictionaries/02.LineNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputPath, string outputPath)
+        {
+            StreamReader reader = new StreamReader(inputPath);
+            StreamWriter writer = new StreamWriter(outputPath);
+
+            using (reader)
+            {
+                using (writer)
+                {
+                    string line = reader.ReadLine();
+                    int counter = 0;
+                    while (line != null)
+                    {
+                        counter++;
+                        writer.WriteLine($"{counter}. {line}");
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
