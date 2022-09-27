5e5d32faaca94086ee906a924c04fcc1c3f27fdc
Georgi <bulbrainn@gmail.com>
2022-09-27 21:47:14 +0300

    Added Even Lines

---
+++ 03.C#Advanced/10.StreamsFilesAndDictionariesExercise/01.EvenLines/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+                char[] separators = new char[] { '-', ',', '.', '!', '?' };
+                string line = reader.ReadLine();
+                int counter = 0;
+                while (line != null)
+                {
+                    counter++;
+                    if (counter % 2 != 0)
+                    {
+                        line = string.Join(" ", line.Split().Reverse().ToArray());
+                        string[] temp = line.Split(separators, StringSplitOptions.None);
+                        stringBuilder.AppendLine(String.Join("@", temp));
+                    }
+
+                    line = reader.ReadLine();
+                }
+
+                return stringBuilder.ToString();
+            }
+        }
+    }
+}
