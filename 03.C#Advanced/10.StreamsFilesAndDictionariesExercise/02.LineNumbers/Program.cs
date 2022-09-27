425effcb6ecfdc9ee47672342212b227a433eed5
Georgi <bulbrainn@gmail.com>
2022-09-27 22:23:40 +0300

    Added Line Numbers

---
+++ 03.C#Advanced/10.StreamsFilesAndDictionariesExercise/02.LineNumbers/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                using (StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                    string line = reader.ReadLine();
+                    int counter = 0;
+                    while (line != null)
+                    {
+                        counter++;
+                        int punctuation = CountPunctuation(line);
+                        int letterrs = CountLetters(line);
+                        line = $"Line {counter}: {line} ({letterrs})({punctuation})";
+                        writer.WriteLine(line);
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+
+        private static int CountPunctuation(string line)
+        {
+            int sum = 0;
+            char[] arr = line.ToCharArray();
+            foreach (char c in arr)
+            {
+                if (char.IsPunctuation(c))
+                {
+                    sum++;
+                }
+            }
+
+            return sum;
+        }
+
+        private static int CountLetters(string line)
+        {
+            int sum = 0;
+            char[] arr = line.ToCharArray();
+            foreach (char c in arr)
+            {
+                if (char.IsLetter(c))
+                {
+                    sum++;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
