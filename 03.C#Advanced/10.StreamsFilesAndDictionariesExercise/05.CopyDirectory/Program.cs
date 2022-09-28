b4044779ea3e968d956f6c7a8f785aa5a321ab0f
Georgi <bulbrainn@gmail.com>
2022-09-28 21:18:13 +0300

    Added Copy Directory

---
+++ 03.C#Advanced/10.StreamsFilesAndDictionariesExercise/05.CopyDirectory/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace CopyDirectory
+{
+    public class CopyDirectory
+    {
+        static void Main()
+        {
+            string inputPath = $@"{Console.ReadLine()}";
+            string outputPath = $@"{Console.ReadLine()}";
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            var files = Directory.GetFiles(inputPath);
+            if (Directory.Exists(outputPath))
+            {
+                Directory.Delete(outputPath, true);
+            }
+            Directory.CreateDirectory(outputPath);
+            foreach (var file in files)
+            {
+                var fi = new FileInfo(file);
+                File.Copy(file, outputPath + "\\" + fi.Name);
+            }
+        }
+    }
+}
