a892a205a793ac5880fd711075fed105d84f03eb
Georgi <bulbrainn@gmail.com>
2022-09-29 23:05:15 +0300

    Added Add Vat

---
+++ 03.C#Advanced/11.FunctionalProgramming/04.AddVat/AddVat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+class AddVat
+{
+    static void Main()
+    {
+        Func<double, double> addVat = x => x * 1.2;
+        double[] nums = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(x => double.Parse(x)).ToArray();
+        double[] numsPlusVat = nums.Select(x => addVat(x)).ToArray();
+
+        Array.ForEach(numsPlusVat, x => Console.WriteLine($"{x:f2}"));
+    }
+}
