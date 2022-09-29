b71d21f704ea1f14a2fdd441cca06e08720e24ba
Georgi <bulbrainn@gmail.com>
2022-09-29 20:43:49 +0300

    Added Sort Even Numbers

---
+++ 03.C#Advanced/11.FunctionalProgramming/01.SortEvenNumbers/SortEvenNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+public class SortEvenNumbers
+{
+    static void Main()
+    {
+        Func<int, bool> filterNums = x => x % 2 == 0;
+
+        int[] nums = Console.ReadLine()
+            .Split(", ")
+            .Select(x => int.Parse(x))
+            .ToArray();
+
+        int[] filteredNums = nums
+            .Where(filterNums)
+            .OrderBy(x => x)
+            .ToArray();
+
+        Console.WriteLine(String.Join(", ", filteredNums));
+    }
+}
+
