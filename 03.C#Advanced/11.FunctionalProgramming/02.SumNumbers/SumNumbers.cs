e493f9b4796c9100c388d42d53d8b21ad2976b33
Georgi <bulbrainn@gmail.com>
2022-09-29 22:03:01 +0300

    Added Sum Numbers

---
+++ 03.C#Advanced/11.FunctionalProgramming/02.SumNumbers/SumNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public class SumNumbers
+{
+    static void Main(string[] args)
+    {
+        Func<int[], int> countNumbers = x => x.Length;
+        
+        int[] nums = Console.ReadLine()
+            .Split(", ")
+            .Select(x => int.Parse(x))
+            .ToArray();
+
+        Console.WriteLine(countNumbers(nums));
+        Console.WriteLine(nums.Sum());
+    }
+}
