262b09db74113e2a88b3a26f36ac188e8e6e2b49
Georgi <bulbrainn@gmail.com>
2022-09-29 22:46:23 +0300

    Added Count Uppercase Words

---
+++ 03.C#Advanced/11.FunctionalProgramming/03.CountUppercaseWords/CountUppercaseWords.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+public class CountUppercaseWords
+{
+    static Func<string, bool> isFirstLetterUppercase =
+        x => x[0] == x.ToUpper()[0];
+    static void Main()
+    {
+        Console.WriteLine(
+            String.Join("\r\n",
+            Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries).
+            Where(isFirstLetterUppercase)));
+    }
+}
