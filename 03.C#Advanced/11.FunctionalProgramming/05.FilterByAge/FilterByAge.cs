99724202f069ca8611011f558f700022b829793e
Georgi <bulbrainn@gmail.com>
2022-09-30 00:15:31 +0300

    Moved class Person to separete file.

--- 03.C#Advanced/11.FunctionalProgramming/05.FilterByAge/FilterByAge.cs
+++ 03.C#Advanced/11.FunctionalProgramming/05.FilterByAge/FilterByAge.cs
@@ -67,14 +67,3 @@ class FilterByAge
         }
     }
 }
-
-class Person
-{
-    public Person(string name, int age)
-    {
-        Name = name;
-        Age = age;
-    } 
-    public string Name { get; set; }
-    public int Age { get; set; }
-}
---
+++ 03.C#Advanced/11.FunctionalProgramming/05.FilterByAge/Person.cs
@@ -0,0 +1,10 @@
+﻿class Person
+{
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    } 
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
