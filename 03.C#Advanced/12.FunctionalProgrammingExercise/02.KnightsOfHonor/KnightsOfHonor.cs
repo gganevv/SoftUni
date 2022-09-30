702b02964d4df9aebf4f649e4be430e658cf00fb
Georgi <bulbrainn@gmail.com>
2022-09-30 22:54:27 +0300

    Added Knights Of Honor

---
+++ 03.C#Advanced/12.FunctionalProgrammingExercise/02.KnightsOfHonor/KnightsOfHonor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class KnightsOfHonor
+{
+    static void Main()
+    {
+        Action<string[]> appendTitle = (names) =>
+        {
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Sir {name}");
+            }
+        };
+
+        string[] names = Console.ReadLine().Split();
+        appendTitle(names);
+    }
+}
