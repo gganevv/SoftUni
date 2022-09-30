7ec914213c03372cf80e977c7272b0764a7a81c2
Georgi <bulbrainn@gmail.com>
2022-09-30 23:06:15 +0300

    Added Custom Min Function

---
+++ 03.C#Advanced/12.FunctionalProgrammingExercise/03.CustomMinFunction/CustomMinFunction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+class CustomMinFunction
+{
+    static void Main()
+    {
+        Func<int[], int> getMinNumber = (nums) =>
+        {
+            int minNum = int.MaxValue;
+            foreach (var n in nums)
+            {
+                if (n < minNum)
+                {
+                    minNum = n;
+                }
+            }
+
+            return minNum;
+        };
+
+        int[] nums = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => int.Parse(x))
+            .ToArray();
+
+        int minNum = getMinNumber(nums);
+        Console.WriteLine(minNum);
+    }
+}
