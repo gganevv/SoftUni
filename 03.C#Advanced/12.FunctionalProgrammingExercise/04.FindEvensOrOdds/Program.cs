b5bbaf6a6ca334c845108def33c678259c971737
Georgi <bulbrainn@gmail.com>
2022-10-01 01:02:14 +0300

    Added Find Evens Or Odds

---
+++ 03.C#Advanced/12.FunctionalProgrammingExercise/04.FindEvensOrOdds/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class Program
+{
+    static void Main()
+    {
+        Predicate<int> isOdd = number => number % 2 != 0;
+        Predicate<int> isEven = number => number % 2 == 0;
+
+        int[] ranges = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => int.Parse(x))
+            .ToArray();
+
+        int lowerRange = ranges[0];
+        int upperRange = ranges[1];
+
+        List<int> nums = Enumerable.Range(lowerRange, upperRange - lowerRange + 1).ToList();
+
+        string condition = Console.ReadLine();
+        List<int> filteredNumbers = new List<int>();
+
+        if (condition == "odd")
+        {
+            filteredNumbers = nums.FindAll(isOdd);
+        }
+        else
+        {
+            filteredNumbers = nums.FindAll(isEven);
+        }
+
+        Console.WriteLine(string.Join(" ", filteredNumbers));
+    }
+}
