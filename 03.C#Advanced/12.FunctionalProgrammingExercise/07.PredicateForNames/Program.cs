3a7d95548f56c2bad2f1d5f8121a2322183f73b7
Georgi <bulbrainn@gmail.com>
2022-10-01 20:06:26 +0300

    Added Predicate for Names

---
+++ 03.C#Advanced/12.FunctionalProgrammingExercise/07.PredicateForNames/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int length = int.Parse(Console.ReadLine());
+        string[] names = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        Predicate<string> checkLength = x => x.Length <= length;
+        List<string> filteredNames = names.Where(x => checkLength(x)).ToList();
+        filteredNames.ForEach(x => Console.WriteLine(x));
+    }
+}
