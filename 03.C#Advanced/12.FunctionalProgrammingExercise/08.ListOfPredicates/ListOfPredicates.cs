d1499699403caa6568ffcba89cbf4257a8733552
Georgi <bulbrainn@gmail.com>
2022-10-01 23:54:06 +0300

    Added List of Predicates

---
+++ 03.C#Advanced/12.FunctionalProgrammingExercise/08.ListOfPredicates/ListOfPredicates.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class ListOfPredicates
+{
+    static void Main()
+    {
+        List<Predicate<int>> predicates = new List<Predicate<int>>();
+
+        int endRange = int.Parse(Console.ReadLine());
+        HashSet<int> dividers = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => int.Parse(x))
+            .ToHashSet();
+
+        int[] nums = Enumerable.Range(1, endRange).ToArray();
+
+        foreach (int div in dividers)
+        {
+            predicates.Add(x => x % div == 0);
+        }
+
+        foreach (var num in nums)
+        {
+            bool isDivisible = true;
+
+            foreach (var match in predicates)
+            {
+                if (!match(num))
+                {
+                    isDivisible = false; 
+                    break;
+                }
+            }
+
+            if (isDivisible)
+            {
+                Console.Write($"{num} ");
+            }
+        }
+    }
+}
