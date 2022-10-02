b2e4910e27ed4e172f5b36eb62e7b971ec5c2d02
Georgi <bulbrainn@gmail.com>
2022-10-02 22:48:16 +0300

    Added TriFunction

---
+++ 03.C#Advanced/12.FunctionalProgrammingExercise/11.Trifunction/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Func<string, int, bool> checkNameSum = (name, sum) =>
+                name.Sum(x => x) >= sum;
+
+        Func<string[], int, Func<string, int, bool>, string> getName = (names, sum, match) =>
+            names.FirstOrDefault(name => match(name, sum));
+
+        int sum = int.Parse(Console.ReadLine());
+        string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        Console.WriteLine(getName(names, sum, checkNameSum));
+    }
+}
