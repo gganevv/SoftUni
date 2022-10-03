acf34e8b2920a63aaae805ec7c371d6597818037
Georgi <bulbrainn@gmail.com>
2022-10-04 01:36:09 +0300

    Refactoring

--- 03.C#Advanced/13.DifiningClasses/01.Car/CarEx.cs
+++ 03.C#Advanced/13.DifiningClasses/01.Car/CarEx.cs
@@ -15,15 +15,4 @@ namespace CarManufacturer
             Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
         }
     }
-
-    public class Car
-    {
-        private string make;
-        private string model;
-        private int year;
-
-        public string Make { get; set; }
-        public string Model { get; set; }
-        public int Year { get; set; }
-    }
 }
