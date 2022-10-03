3240f277a093f1a1eb1decbac69720df63ec5f36
Georgi <bulbrainn@gmail.com>
2022-10-03 23:58:18 +0300

    Added Car Extension

---
+++ 03.C#Advanced/13.DifiningClasses/02.CarExtension/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car();
+
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+            car.FuelQuantity = 200;
+            car.FuelConsumption = 200;
+            car.Drive(2000);
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public string Make;
+        public string Model;
+        public int Year;
+        public double FuelQuantity;
+        public double FuelConsumption;
+
+        public void Drive(double distance)
+        {
+            if (FuelQuantity - (distance * FuelConsumption) > 0)
+            {
+                FuelQuantity -= (distance * FuelConsumption);
+            }
+            else
+            {
+                Console.WriteLine($"Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:f2}";
+        }
+    }
+}
