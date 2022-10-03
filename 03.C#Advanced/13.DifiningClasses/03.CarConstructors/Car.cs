c54d2621fd479d374211b9184bcd833a2db9cc87
Georgi <bulbrainn@gmail.com>
2022-10-04 01:32:58 +0300

    Added Car Constructors

---
+++ 03.C#Advanced/13.DifiningClasses/03.CarConstructors/Car.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year) : this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+            : this(make, model, year)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            if (FuelQuantity - (distance * FuelConsumption) >= 0)
+            {
+                FuelQuantity -= (distance * FuelConsumption);
+            }
+            else
+            {
+                Console.WriteLine($"Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:f2}";
+        }
+    }
+}
