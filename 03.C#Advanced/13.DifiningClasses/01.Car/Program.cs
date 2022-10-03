c2da1550d29f9e49c1aa229938c3c858f0e5e3af
Georgi <bulbrainn@gmail.com>
2022-10-03 23:25:16 +0300

    Added Car

---
+++ 03.C#Advanced/13.DifiningClasses/01.Car/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car();
+
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+
+            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+        }
+    }
+
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
