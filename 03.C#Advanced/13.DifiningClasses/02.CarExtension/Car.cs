722b0f9d67375631301dc3c9637e1c0ba0f536e4
Georgi <bulbrainn@gmail.com>
2022-10-04 01:34:29 +0300

    Refactoring

--- 03.C#Advanced/13.DifiningClasses/02.CarExtension/Car.cs
+++ 03.C#Advanced/13.DifiningClasses/02.CarExtension/Car.cs
@@ -2,22 +2,6 @@
 
 namespace CarManufacturer
 {
-    public class StartUp
-    {
-        static void Main()
-        {
-            Car car = new Car();
-
-            car.Make = "VW";
-            car.Model = "MK3";
-            car.Year = 1992;
-            car.FuelQuantity = 200;
-            car.FuelConsumption = 200;
-            car.Drive(2000);
-            Console.WriteLine(car.WhoAmI());
-        }
-    }
-
     public class Car
     {
         private string make;
