2c454d5fee8ea641992faa55db491c7a3b061461
Georgi <bulbrainn@gmail.com>
2022-10-05 01:01:18 +0300

    Added Creating Constructors

---
+++ 03.C#Advanced/14.DefiningClassesExercise/02.CreatingConstructors/Person.cs
@@ -0,0 +1,28 @@
+﻿namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person()
+        {
+            Name = "No name";
+            Age = 1;
+        }
+
+        public Person(int age) : this()
+        {
+            this.Age = age;
+        }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get { return name; } set { name = value; } }
+        public int Age { get { return age; } set { age = value; } }
+    }
+}
