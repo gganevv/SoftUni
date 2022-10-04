e150a2ae74f761abe2a1ac0dc74c58b5106ee9fa
Georgi <bulbrainn@gmail.com>
2022-10-05 00:48:09 +0300

    Added Define a class Person

---
+++ 03.C#Advanced/14.DefiningClassesExercise/01.DefineAClassPerson/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Person person1 = new Person();
+            person1.Name = "Pesho";
+            person1.Age = 99;
+
+            Person person2 = new Person() { Name = "Ivan", Age = 22 };
+        }
+    }
+}
