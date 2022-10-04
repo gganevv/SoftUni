efa7618671613ad3240b6fb566f64df52560227b
Georgi <bulbrainn@gmail.com>
2022-10-04 19:34:00 +0300

    Fixes

--- 03.C#Advanced/13.DifiningClasses/01.Car/Car.cs
+++ 03.C#Advanced/13.DifiningClasses/01.Car/Car.cs
@@ -6,8 +6,41 @@
         private string model;
         private int year;
 
-        public string Make { get; set; }
-        public string Model { get; set; }
-        public int Year { get; set; }
+        public string Make 
+        { 
+            get 
+            { 
+                return make;
+            } 
+            set 
+            { 
+                make = value; 
+            } 
+        }
+
+        public string Model
+        {
+            get
+            {
+                return model;
+            }
+            set
+            {
+                model = value;
+            }
+        }
+
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+
+            set
+            {
+                year = value;
+            }
+        }
     }
 }
