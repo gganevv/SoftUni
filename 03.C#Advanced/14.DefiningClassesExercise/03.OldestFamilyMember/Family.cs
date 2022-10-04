4e73c5fbf98cbca97d39aa3cdf2bb5ca7f600366
Georgi <bulbrainn@gmail.com>
2022-10-05 01:23:41 +0300

    Added Oldest Family Member

---
+++ 03.C#Advanced/14.DefiningClassesExercise/03.OldestFamilyMember/Family.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> people;
+
+        public Family()
+        {
+            people = new List<Person>();
+        }
+
+        public List<Person> People { get { return people; } set { people = value; } }
+
+        public void AddMember(Person person)
+        {
+            this.People.Add(person);
+        }
+
+        public void GetOldestMember()
+        {
+            Console.WriteLine(People.OrderByDescending(x => x.Age).First());
+        }
+    }
+}
---
+++ 03.C#Advanced/14.DefiningClassesExercise/03.OldestFamilyMember/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int countOfPeople = int.Parse(Console.ReadLine());
+            Family family = new Family();
+
+            for (int i = 0; i < countOfPeople; i++)
+            {
+                string[] args = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = args[0];
+                int age = int.Parse(args[1]);
+                family.AddMember(new Person(name, age));
+            }
+
+            family.GetOldestMember();
+        }
+    }
+}
