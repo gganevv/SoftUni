3b321c6dc9d0d2b1e9e71779cf299d506c40b34a
Georgi <bulbrainn@gmail.com>
2022-10-05 23:17:25 +0300

    Added Car Salesman

---
+++ 03.C#Advanced/14.DefiningClassesExercise/07.RawData/Car.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+
+namespace _08.CarSalesman
+{
+    public class Car
+    {
+        public Car(string model, Engine engine, int weight, string color)
+        {
+            Model = model;
+            Engine = engine;
+            Weight = weight;
+            Color = color;
+        }
+
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public int Weight { get; set; }
+        public string Color { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"{this.Model}:");
+            stringBuilder.AppendLine($"  {this.Engine.Model}:");
+            stringBuilder.AppendLine($"    Power: {this.Engine.Power}");
+            if (this.Engine.Displacement > 0)
+            {
+                stringBuilder.AppendLine($"    Displacement: {this.Engine.Displacement}");
+            }
+            else
+            {
+                stringBuilder.AppendLine($"    Displacement: n/a");
+            }
+            if (this.Engine.Efficiency != null)
+            {
+                stringBuilder.AppendLine($"    Efficiency: {this.Engine.Efficiency}");
+            }
+            else
+            {
+                stringBuilder.AppendLine($"    Efficiency: n/a");
+            }
+            if (this.Weight > 0)
+            {
+                stringBuilder.AppendLine($"  Weight: {this.Weight}");
+            }
+            else
+            {
+                stringBuilder.AppendLine($"  Weight: n/a");
+            }
+            if (this.Color != null)
+            {
+                stringBuilder.AppendLine($"  Color: {this.Color}");
+            }
+            else
+            {
+                stringBuilder.AppendLine($"  Color: n/a");
+            }
+            
+            return stringBuilder.ToString().Trim();
+        }
+    }
+}
---
+++ 03.C#Advanced/14.DefiningClassesExercise/07.RawData/StartUp.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.CarSalesman
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Engine> engines = new List<Engine>();
+            List<Car> cars = new List<Car>();
+
+            int numberOfEngines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfEngines; i++)
+            {
+                string[] inputArgs = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string model = inputArgs[0];
+                int power = int.Parse(inputArgs[1]);
+                int displacement = -1;
+                string efficiency = null;
+
+                if (inputArgs.Length > 2)
+                {
+                    if (char.IsDigit(inputArgs[2][0]))
+                    {
+                        displacement = int.Parse(inputArgs[2]);
+                    }
+                    else
+                    {
+                        efficiency = inputArgs[2];
+                    }
+                }
+                if (inputArgs.Length > 3)
+                {
+                    efficiency = inputArgs[3];
+                }
+                Engine engine = new Engine(model, power, displacement, efficiency);
+                engines.Add(engine);
+            }
+
+            int numberOfCars = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                string[] inputArgs = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string model = inputArgs[0];
+                string engineName = inputArgs[1];
+                Engine engine = engines.FirstOrDefault(x => x.Model == engineName);
+                int weight = -1;
+                string color = null;
+
+                if (inputArgs.Length > 2)
+                {
+                    if (char.IsDigit(inputArgs[2][0]))
+                    {
+                        weight = int.Parse(inputArgs[2]);
+                    }
+                    else
+                    {
+                        color = inputArgs[2];
+                    }
+                }
+                if (inputArgs.Length > 3)
+                {
+                    color = inputArgs[3];
+                }
+                Car car = new Car(model, engine, weight, color);
+                cars.Add(car);
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
