525955d750dc836b8ec883714ffa10ba8aa2205e
Georgi <bulbrainn@gmail.com>
2022-10-05 19:53:04 +0300

    Added Opinion Poll

---
+++ 03.C#Advanced/14.DefiningClassesExercise/04.OpinionPoll/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int numberofPeople = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < numberofPeople; i++)
+            {
+                string[] inputArgs = Console.ReadLine().Split();
+                string name = inputArgs[0];
+                int age = int.Parse(inputArgs[1]);
+                Person person = new Person(name, age);
+                people.Add(person);
+            }
+
+            Console.WriteLine(String.Join("\n", people.Where(x => x.Age > 30).OrderBy(x => x.Name)));
+        }
+    }
+}
