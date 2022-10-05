87c7eed86d860f29068c79f314dd5d4aa0ecbdfd
Georgi <bulbrainn@gmail.com>
2022-10-05 20:01:25 +0300

    Added Date Modifier

---
+++ 03.C#Advanced/14.DefiningClassesExercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+    class DateModifier
+    {
+        static void Main(string[] args)
+        {
+            DateTime startDate = DateTime.Parse(Console.ReadLine());
+            DateTime endDate = DateTime.Parse(Console.ReadLine());
+            TimeSpan timeSpan = startDate - endDate;
+            Console.WriteLine(Math.Abs(timeSpan.Days));
+        }
+    }
