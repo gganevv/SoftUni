6bc946d831c4bd47a476546da7cda85216b86fd6
Georgi <bulbrainn@gmail.com>
2022-10-06 21:18:49 +0300

    Added Pokemon Trainer

---
+++ 03.C#Advanced/14.DefiningClassesExercise/09.PokemonTrainer/PokemonTrainer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.PokemonTrainer
+{
+    public class PokemonTrainer
+    {
+        static void Main()
+        {
+            List<Trainer> trainers = new List<Trainer>();
+            string input = Console.ReadLine();
+            while (input != "Tournament")
+            {
+                AddTrainers(trainers, input);
+
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+            while (input != "End")
+            {
+                CheckElements(trainers, input);
+
+                input = Console.ReadLine();
+            }
+
+            trainers = trainers.OrderByDescending(x => x.Badges).ToList();
+            trainers.ForEach(x => Console.WriteLine(x));
+        }
+
+        private static void CheckElements(List<Trainer> trainers, string input)
+        {
+            string element = input;
+            foreach (Trainer trainer in trainers)
+            {
+                bool playerWon = false;
+                foreach (var pokemon in trainer.Pokemons)
+                {
+                    if (pokemon.Element == element)
+                    {
+                        playerWon = true;
+                        continue;
+                    }
+                }
+                if (playerWon)
+                {
+                    trainer.Badges++;
+                }
+                else
+                {
+                    trainer.Pokemons.ForEach(x => x.Health -= 10);
+                }
+
+                trainer.CheckPokemonsHelth();
+            }
+        }
+
+        private static void AddTrainers(List<Trainer> trainers, string input)
+        {
+            string[] inputArgs = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string trainerName = inputArgs[0];
+            string pokemonName = inputArgs[1];
+            string pokemonElement = inputArgs[2];
+            int pokemonHealth = int.Parse(inputArgs[3]);
+
+            if (!trainers.Any(x => x.Name == trainerName))
+            {
+                trainers.Add(new Trainer(trainerName));
+            }
+            trainers.FirstOrDefault(x => x.Name == trainerName)
+                .Pokemons.Add(new Pokemon(pokemonName, pokemonElement, pokemonHealth));
+        }
+    }
+}
---
+++ 03.C#Advanced/14.DefiningClassesExercise/09.PokemonTrainer/Trainer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace _09.PokemonTrainer
+{
+    public class Trainer
+    {
+        public Trainer(string name)
+        {
+            this.Name = name;
+            this.Badges = 0;
+            this.Pokemons = new List<Pokemon>();
+        }
+        public string Name { get; set; }
+        public int Badges { get; set; }
+        public List<Pokemon> Pokemons { get; set; }
+
+        public void CheckPokemonsHelth()
+        {
+            for (int i = 0; i < Pokemons.Count; i++)
+            {
+                if (Pokemons[i].Health <= 0)
+                {
+                    Pokemons.RemoveAt(i);
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} {this.Badges} {this.Pokemons.Count}";
+        }
+    }
+}
