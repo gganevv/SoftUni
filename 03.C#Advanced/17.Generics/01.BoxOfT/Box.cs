9030291b79f4c98b682d5a5ae96044bb9eabccbb
Georgi <bulbrainn@gmail.com>
2022-10-10 23:21:52 +0300

    Extracted class Box

---
+++ 03.C#Advanced/17.Generics/01.BoxOfT/Box.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        public Stack<T> stack;
+
+        public Box()
+        {
+            this.stack = new Stack<T>();
+        }
+
+        public void Add(T element)
+        {
+            this.stack.Push(element);
+            this.Count++;
+        }
+
+        public T Remove()
+        {
+            this.Count--;
+            return this.stack.Pop();
+        }
+
+        public int Count { get; set; }
+    }
+}
--- 03.C#Advanced/17.Generics/01.BoxOfT/StartUp.cs
+++ 03.C#Advanced/17.Generics/01.BoxOfT/StartUp.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace BoxOfT
 {
@@ -9,29 +8,5 @@ namespace BoxOfT
         {
 
         }
-    }
-
-    public class Box<T>
-    {
-        public Stack<T> stack;
-
-        public Box()
-        {
-            this.stack = new Stack<T>();
-        }
-
-        public void Add(T element)
-        {
-            this.stack.Push(element);
-            this.Count++;
-        }
-
-        public T Remove()
-        {
-            this.Count--;
-            return this.stack.Pop();
-        }
-
-        public int Count { get; set; }
     }
 }
