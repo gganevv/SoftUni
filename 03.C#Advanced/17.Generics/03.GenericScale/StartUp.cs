5d40656403f2202e8ef2c2101ad76fef6e6b7429
Georgi <bulbrainn@gmail.com>
2022-10-11 00:13:20 +0300

    Added Equality Scale

---
+++ 03.C#Advanced/17.Generics/03.GenericScale/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            string a = "a";
+            string b = "b";
+            EqualityScale equalityScale = new GenericScale.EqualityScale(a, b);
+
+        }
+    }
+}
