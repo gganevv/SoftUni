ff0c403f6e0a9128cd1614df132488c2e70f2f51
Georgi <bulbrainn@gmail.com>
2022-10-10 23:20:29 +0300

    Added Box Of T

---
+++ 03.C#Advanced/17.Generics/01.BoxOfT/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+        }
+    }
+
+    public class Box<T>
+    {
+        public Stack<T> stack;
+
+        public Box()
+        {
+            this.stack = new Stack<T>();
+        }
+
+        public void Add(T element)
+        {
+            this.stack.Push(element);
+            this.Count++;
+        }
+
+        public T Remove()
+        {
+            this.Count--;
+            return this.stack.Pop();
+        }
+
+        public int Count { get; set; }
+    }
+}
