fbc6989cfcf8e5b2faa360773311717fa568f834
Georgi <bulbrainn@gmail.com>
2022-10-14 00:26:55 +0300

    Added Generic Swap Method Ints

---
+++ 03.C#Advanced/18.GenericsExercise/04.GenericSwapMethodIntegers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.GenericSwapMethodIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Box<int>> boxes = new List<Box<int>>();
+            int countOfBoxes = int.Parse(Console.ReadLine());
+            for (int i = 0; i < countOfBoxes; i++)
+            {
+                boxes.Add(new Box<int>(int.Parse(Console.ReadLine())));
+            }
+
+            int[] elementsToSwap = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            SwapElemet(elementsToSwap[0], elementsToSwap[1], boxes);
+
+            foreach (var box in boxes)
+            {
+                Console.WriteLine(box);
+            }
+        }
+
+        private static void SwapElemet<T>(int firstIndex, int secondIndex, List<T> boxes)
+        {
+            T temp = boxes[firstIndex];
+            boxes[firstIndex] = boxes[secondIndex];
+            boxes[secondIndex] = temp;
+        }
+    }
+}
