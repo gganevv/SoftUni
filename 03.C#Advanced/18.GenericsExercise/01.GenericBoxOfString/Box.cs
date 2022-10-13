18fb5a9b842b2d1f0ddc7d7014cda7f6b3487cb4
Georgi <bulbrainn@gmail.com>
2022-10-13 23:05:43 +0300

    Added Generic Generic Box Of String

---
+++ 03.C#Advanced/18.GenericsExercise/01.GenericBoxOfString/Box.cs
@@ -0,0 +1,23 @@
+﻿namespace _01.GenericBoxOfString
+{
+    public class Box<T>
+    {
+        private T element;
+
+        public T Element
+        {
+            get { return element; }
+            set { element = value; }
+        }
+
+        public Box(T element)
+        {
+            Element = element;
+        }
+
+        public override string ToString()
+        {
+            return $"{Element.GetType()}: {Element}";
+        }
+    }
+}
---
+++ 03.C#Advanced/18.GenericsExercise/01.GenericBoxOfString/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01.GenericBoxOfString
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int countOfItems = int.Parse(Console.ReadLine());
+            for (int i = 0; i < countOfItems; i++)
+            {
+                string value = Console.ReadLine();
+                Box<string> box = new Box<string>(value);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
