78373a1d913a3cc4bf950de19ff30ff58fc01d7b
Georgi <bulbrainn@gmail.com>
2022-10-13 23:06:15 +0300

    Added Generic Box Of Integer

---
+++ 03.C#Advanced/18.GenericsExercise/02.GenericBoxOfInteger/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _02.GenericBoxOfInteger
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int countOfItems = int.Parse(Console.ReadLine());
+            for (int i = 0; i < countOfItems; i++)
+            {
+                int value = int.Parse(Console.ReadLine());
+                Box<int> box = new Box<int>(value);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
