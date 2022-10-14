e774520206b2780124c08dfdf902957ac28baef0
Georgi <bulbrainn@gmail.com>
2022-10-14 19:25:26 +0300

    Added Tuple

---
+++ 03.C#Advanced/18.GenericsExercise/07.Tuple/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _07.Tuple
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            string[] personDetails = Console.ReadLine().Split();
+            string personFirstName = personDetails[0] + " " + personDetails[1];
+            string personAddres = personDetails[2];
+            Tuple<string, string> personTuple = new Tuple<string, string>(personFirstName, personAddres);
+            Console.WriteLine(personTuple);
+
+            string[] beerDetails = Console.ReadLine().Split();
+            string drinkerName = beerDetails[0];
+            int drinkQuantity = int.Parse(beerDetails[1]);
+            Tuple<string, int> beerTuple = new Tuple<string, int>(drinkerName, drinkQuantity);
+            Console.WriteLine(beerTuple);
+
+            string[] numsDetails = Console.ReadLine().Split();
+            int intNum = int.Parse(numsDetails[0]);
+            double doubleNum = double.Parse(numsDetails[1]);
+            Tuple<int, double> numTuple = new Tuple<int, double>(intNum, doubleNum);
+            Console.WriteLine(numTuple);
+        }
+    }
+}
