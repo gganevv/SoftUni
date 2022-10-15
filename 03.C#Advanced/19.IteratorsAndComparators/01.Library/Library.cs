d234fea177d2ce5c4ad55d50d49b16cc9ce5f259
Georgi <bulbrainn@gmail.com>
2022-10-15 21:03:00 +0300

    Added Library

---
+++ 03.C#Advanced/19.IteratorsAndComparators/01.Library/Library.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            Books = books.ToList();
+        }
+
+        public List<Book> Books
+        {
+            get { return books; }
+            set { books = value; }
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
