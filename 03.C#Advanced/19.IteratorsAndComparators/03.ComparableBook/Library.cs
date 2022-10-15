ee060d1a9ee965a5aeb2df9d27967b1a8e6a67e9
Georgi <bulbrainn@gmail.com>
2022-10-15 22:45:48 +0300

    Added Comparable Book

---
+++ 03.C#Advanced/19.IteratorsAndComparators/03.ComparableBook/Library.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            Books = books.ToList();
+            this.Books.Sort();
+        }
+
+        public List<Book> Books
+        {
+            get { return books; }
+            set { books = value; }
+        }
+
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            for (int i = 0; i < books.Count; i++)
+            {
+                yield return books[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator(); 
+    }
+}
