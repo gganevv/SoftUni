349076eab0eba91626c69669e3b72b142181b38c
Georgi <bulbrainn@gmail.com>
2022-10-16 14:29:43 +0300

    Added Renovators

---
+++ 03.C#Advanced/24.ExamPractice/02.ExamPractice02/03.Renovators/Catalog.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Renovators
+{
+    public class Catalog
+    {
+        public Catalog(string name, int neededRenovators, string project)
+        {
+            Renovators = new List<Renovator>();
+            Name = name;
+            NeededRenovators = neededRenovators;
+            Project = project;
+        }
+
+        public List<Renovator> Renovators { get; set; }
+        public string Name { get; set; }
+        public int NeededRenovators { get; set; }
+        public string Project { get; set; }
+        public int Count => Renovators.Count;
+
+        public string AddRenovator(Renovator renovator)
+        {
+            if (renovator.Name == null || renovator.Name == string.Empty || renovator.Type == null || renovator.Type == string.Empty)
+            {
+                return "Invalid renovator's information.";
+            }
+
+            if (Count == NeededRenovators)
+            {
+                return "Renovators are no more needed.";
+            }
+
+            if (renovator.Rate > 350)
+            {
+                return "Invalid renovator's rate.";
+            }
+
+            Renovators.Add(renovator);
+            return $"Successfully added {renovator.Name} to the catalog.";
+        }
+
+        public bool RemoveRenovator(string name)
+        {
+            if (Renovators.Any(x => x.Name == name))
+            {
+                Renovator renovatorToRemove = Renovators.First(x => x.Name == name);
+                Renovators.Remove(renovatorToRemove);
+                return true;
+            }
+
+            return false;
+        }
+
+        public int RemoveRenovatorBySpecialty(string type)
+        {
+            return Renovators.RemoveAll(x => x.Type == type);
+        }
+
+        public Renovator HireRenovator(string name)
+        {
+            if (!Renovators.Any(x => x.Name == name))
+            {
+                return null;
+            }
+
+            Renovator renovator = Renovators.First(x => x.Name == name);
+            renovator.Hired = true;
+            return renovator;
+        }
+
+        public List<Renovator> PayRenovators(int days)
+        {
+            List<Renovator> renovatorsList = new List<Renovator>();
+            foreach (var renovator in Renovators)
+            {
+                if (renovator.Days >= days)
+                {
+                    renovatorsList.Add(renovator);
+                }
+            }
+
+            return renovatorsList;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Renovators avaible for Project {Project}:");
+            foreach (var renovator in Renovators)
+            {
+                if (renovator.Hired == false)
+                {
+                    sb.AppendLine(renovator.ToString());
+                }
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ 03.C#Advanced/24.ExamPractice/02.ExamPractice02/03.Renovators/Renovator.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace Renovators
+{
+    public class Renovator
+    {
+        private string name;
+        private string type;
+        private double rate;
+        private int days;
+        private bool hired;
+
+        public Renovator(string name, string type, double rate, int days)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Rate = rate;
+            this.Days = days;
+            this.Hired = false;
+        }
+
+        public string Name { get { return name; } set { name = value; } }
+        public string Type { get { return type; } set { type = value; } }
+        public double Rate { get { return rate; } set { rate = value; } }
+        public int Days { get { return days; } set { days = value; } }
+        public bool Hired { get { return hired; } set { hired = value; } }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"-Renovator: {Name}");
+            sb.AppendLine($"--Speciality: {Type}");
+            sb.AppendLine($"--Rate per day: {Rate} BGN");
+
+            return sb.ToString().Trim();
+        }
+    }
+}
