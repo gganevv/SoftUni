d4ebedf67fb166584936b669fae6de550841fc3f
Georgi <bulbrainn@gmail.com>
2022-10-18 01:55:56 +0300

    Added Collection

---
+++ 03.C#Advanced/20.IteratorsAndComparatorsExercise/02.Collection/ListyIterator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02.Collection
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        public ListyIterator(List<T> elements)
+        {
+            Elements = elements;
+            CurrentIndex = 0;
+        }
+
+        public List<T> Elements { get; set; }
+        public int CurrentIndex { get; set; }
+
+        public bool Move()
+        {
+            if (CurrentIndex < Elements.Count - 1)
+            {
+                CurrentIndex++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            return CurrentIndex < Elements.Count - 1;
+        }
+
+        public void Print()
+        {
+            try
+            {
+                Console.WriteLine(Elements[CurrentIndex]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var element in Elements)
+            {
+                yield return element;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() =>  GetEnumerator();
+    }
+}
---
+++ 03.C#Advanced/20.IteratorsAndComparatorsExercise/02.Collection/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Collection
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            List<string> items = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Skip(1)
+                .ToList();
+
+            ListyIterator<string> listyIterator = new ListyIterator<string>(items);
+
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                switch (input)
+                {
+                    case "Move":
+                        Console.WriteLine(listyIterator.Move());
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(listyIterator.HasNext());
+                        break;
+                    case "Print":
+                        listyIterator.Print();
+                        break;
+                    case "PrintAll":
+                        foreach (var element in listyIterator.Elements)
+                        {
+                            Console.Write(element + " ");
+                        }
+                        Console.WriteLine();
+                        break;
+                    default:
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
