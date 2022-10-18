efeb40d8984dfae8449e3a6dfa318394058209c5
Georgi <bulbrainn@gmail.com>
2022-10-18 20:38:43 +0300

    Added Equality Logic

---
+++ 03.C#Advanced/20.IteratorsAndComparatorsExercise/06.EqualityLogic/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace _06.EqualityLogic
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public int CompareTo([AllowNull] Person other)
+        {
+            if (this.Name.CompareTo(other.Name) == 0)
+            {
+                return this.Age.CompareTo(other.Age);
+            }
+            return this.Name.CompareTo(other.Name);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Person other = obj as Person;
+
+            if (other == null)
+            {
+                return false;
+            }
+
+            return Name == other.Name && Age == other.Age;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = Name.GetHashCode() + Age.GetHashCode();
+
+            return hashCode;
+        }
+    }
+}
---
+++ 03.C#Advanced/20.IteratorsAndComparatorsExercise/06.EqualityLogic/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.EqualityLogic
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            SortedSet<Person> peopleSS = new SortedSet<Person>();
+            HashSet<Person> peopleHS = new HashSet<Person>();
+
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfPeople; i++)
+            {
+                string[] personArgs = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                Person person = new Person(personArgs[0], int.Parse(personArgs[1]));
+
+                peopleSS.Add(person);
+                peopleHS.Add(person);
+            }
+
+            Console.WriteLine(peopleSS.Count);
+            Console.WriteLine(peopleHS.Count);
+        }
+    }
+}
