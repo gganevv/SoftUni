030ba7e0cc0f32224d20eec7cd1dede278336c2c
Georgi <bulbrainn@gmail.com>
2022-10-18 19:59:05 +0300

    Added Froggy

---
+++ 03.C#Advanced/20.IteratorsAndComparatorsExercise/04.Froggy/Lake.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace _04.Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private List<int> stones;
+
+        public List<int> Stones
+        {
+            get { return stones; }
+            set { stones = value; }
+        }
+
+        public Lake(List<int> stones)
+        {
+            Stones = stones;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < Stones.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return Stones[i];
+                }
+            }
+            for (int i = Stones.Count - 1; i >= 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    yield return Stones[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ 03.C#Advanced/20.IteratorsAndComparatorsExercise/04.Froggy/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Froggy
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<int> stones = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToList();
+            Lake lake = new Lake(stones);
+            Console.WriteLine(String.Join(", ", lake));
+        }
+    }
+}
