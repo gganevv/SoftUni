cb720806ccffa966c6eb3622ab7819d6897e093b
Georgi <bulbrainn@gmail.com>
2022-10-18 20:45:39 +0300

    Added Custom Comparator

---
+++ 03.C#Advanced/20.IteratorsAndComparatorsExercise/07.CustomComparator/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.CustomComparator
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Func<int, int, int> customComparator = (x, y) =>
+            {
+                if (x % 2 == 0 && y % 2 != 0)
+                {
+                    return -1;
+                }
+
+                if (x % 2 != 0 && y % 2 == 0)
+                {
+                    return 1;
+                }
+
+                return x.CompareTo(y);
+            };
+
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Array.Sort(numbers, new Comparison<int>(customComparator));
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
