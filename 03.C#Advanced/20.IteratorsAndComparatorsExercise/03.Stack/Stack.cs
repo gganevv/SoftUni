41f333e22f5803f456c6572fd51d0c7f426a1145
Georgi <bulbrainn@gmail.com>
2022-10-18 19:42:28 +0300

    Added Stack

---
+++ 03.C#Advanced/20.IteratorsAndComparatorsExercise/03.Stack/Stack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _03.Stack
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        private List<T> elements;
+
+        public List<T> Elements
+        {
+            get { return elements; }
+            set { elements = value; }
+        }
+
+        public Stack()
+        {
+            elements = new List<T>();
+        }
+
+        public void Push(params T[] elements)
+        {
+            foreach (var element in elements)
+            {
+                Elements.Add(element);
+            }
+        }
+
+        public T Pop()
+        {
+            if (Elements.Count == 0)
+            {
+                Console.WriteLine("No elements");
+                return default(T);
+            }
+            else
+            {
+                T element = Elements[Elements.Count - 1];
+                Elements.RemoveAt(Elements.Count - 1);
+                return element;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = elements.Count - 1; i >= 0; i--)
+            {
+                yield return elements[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ 03.C#Advanced/20.IteratorsAndComparatorsExercise/03.Stack/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Stack
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Stack<string> stack = new Stack<string>();
+
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                string[] inputArgs = input.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+                string command = inputArgs[0];
+                if (command == "Push")
+                {
+                    stack.Push(inputArgs.Skip(1).ToArray());
+                }
+                else
+                {
+                    stack.Pop();
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
