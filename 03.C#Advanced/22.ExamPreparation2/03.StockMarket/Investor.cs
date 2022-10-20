0ec4b1323ebc84221217beb47ff6e7f3dbbd54dc
Georgi <bulbrainn@gmail.com>
2022-10-20 23:03:13 +0300

    Added Stock Market

---
+++ 03.C#Advanced/22.ExamPreparation2/03.StockMarket/Investor.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StockMarket
+{
+    public class Investor
+    {
+        public List<Stock> Portfolio { get; set; }
+        public Investor(string fullName, string emailAddress, decimal moneyToInvest, string brokerName)
+        {
+            FullName = fullName;
+            EmailAddress = emailAddress;
+            MoneyToInvest = moneyToInvest;
+            BrokerName = brokerName;
+            Portfolio = new List<Stock>();
+        }
+
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+        public decimal MoneyToInvest { get; set; }
+        public string BrokerName { get; set; }
+        public int Count => Portfolio.Count;
+
+        public void BuyStock(Stock stock)
+        {
+            if (stock.MarketCapitalization > 10000 && MoneyToInvest >= stock.PricePerShare)
+            {
+                Portfolio.Add(stock);
+                this.MoneyToInvest -= stock.PricePerShare;
+            }
+            
+        }
+
+        public string SellStock(string companyName, decimal sellPrice)
+        {
+            Stock stock = Portfolio.FirstOrDefault(x => x.CompanyName == companyName);
+            if (stock == null)
+            {
+                return $"{companyName} does not exist.";
+            }
+
+            if (sellPrice < stock.PricePerShare)
+            {
+                return $"Cannot sell {companyName}.";
+            }
+
+            Portfolio.Remove(stock);
+            MoneyToInvest += sellPrice;
+            return $"{companyName} was sold.";
+        }
+
+        public Stock FindStock(string companyName) =>
+            Portfolio.FirstOrDefault(x => x.CompanyName == companyName);
+
+        public Stock FindBiggestCompany()
+        {
+            if (Portfolio.Count == 0)
+            {
+                return null;
+            }
+            decimal biggestStockCapitalization = decimal.MinValue;
+            Stock biggestStock = null;
+            foreach (var stock in Portfolio)
+            {
+                if (stock.MarketCapitalization > biggestStockCapitalization)
+                {
+                    biggestStock = stock;
+                    biggestStockCapitalization = stock.MarketCapitalization;
+                }
+            }
+
+            return biggestStock;
+        }
+
+        public string InvestorInformation()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The investor {FullName} with a broker {BrokerName} has stocks:");
+            foreach (var stock in Portfolio)
+            {
+                sb.AppendLine(stock.ToString());
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
