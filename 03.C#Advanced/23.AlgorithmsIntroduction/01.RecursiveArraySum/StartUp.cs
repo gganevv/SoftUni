63caa62d77fc92d0e5ae519efd7b16ad4a48ee3d
Georgi <bulbrainn@gmail.com>
2022-10-25 00:04:25 +0300

    Added Recursive Array Sum

---
+++ 03.C#Advanced/23.AlgorithmsIntroduction/01.RecursiveArraySum/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.RecursiveArraySum
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = Sum(nums, 0);
+            Console.WriteLine(sum);
+        }
+
+        static int Sum(int[] array, int index)
+        {
+            if (index == array.Length - 1)
+            {
+                return array[index];
+            }
+
+            return array[index] + Sum(array, index + 1);
+        }
+    }
+}
