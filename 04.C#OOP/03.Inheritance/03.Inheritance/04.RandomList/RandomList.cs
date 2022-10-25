1e248c2e1eb8503091fe1668bfae5ec4b805633d
Georgi <bulbrainn@gmail.com>
2022-10-26 00:15:12 +0300

    Added Random List

---
+++ 04.C#OOP/03.Inheritance/03.Inheritance/04.RandomList/RandomList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random rnd;
+        public RandomList()
+        {
+            rnd = new Random();
+        }
+        public string RandomString()
+        {
+            int index = rnd.Next(0, this.Count);
+            string element = this[index];
+            this.RemoveAt(index);
+            return element;
+        }
+    }
+}
