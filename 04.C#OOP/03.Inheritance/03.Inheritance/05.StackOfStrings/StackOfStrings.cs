46817b4588aa5a5597dee294bed16bce996dd696
Georgi <bulbrainn@gmail.com>
2022-10-26 00:26:11 +0300

    Added Stack Of String

---
+++ 04.C#OOP/03.Inheritance/03.Inheritance/05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty() => this.Count == 0;
+        public Stack<string> AddRange(Stack<string> addedStr)
+        {
+            for (int i = 0; i < addedStr.Count; i++)
+            {
+                this.Push(addedStr.Pop());
+            }
+            return this;
+        }
+    }
+}
