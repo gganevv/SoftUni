d73b3c3294d824b25444b7922cbc03f86db2b341
Georgi <bulbrainn@gmail.com>
2022-10-26 23:01:18 +0300

    Added Need For Speed

---
+++ 04.C#OOP/04.InheritanceExercise/04.NeedForSpeed/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        public Vehicle(int horsePower, double fuel)
+        {
+            HorsePower = horsePower;
+            Fuel = fuel;
+            FuelConsumption = DefaultFuelConsumption;
+        }
+
+        public double FuelConsumption { get; set; }
+        public virtual double DefaultFuelConsumption { get; set; } = 1.25;
+        public double Fuel { get; set; }
+        public int HorsePower { get; set; }
+
+        public virtual void Drive(double kilometers)
+        {
+            Fuel -= FuelConsumption * kilometers;
+        }
+    }
+}
