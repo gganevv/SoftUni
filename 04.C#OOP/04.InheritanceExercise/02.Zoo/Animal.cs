714cfe40a615dba42989fa3900b3b0a97613b24f
Georgi <bulbrainn@gmail.com>
2022-10-26 22:20:30 +0300

    Added Zoo

---
+++ 04.C#OOP/04.InheritanceExercise/02.Zoo/Animal.cs
@@ -0,0 +1,17 @@
+﻿namespace Zoo
+{
+    public class Animal
+    {
+        private string name;
+        public Animal(string name)
+        {
+            Name = name;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
