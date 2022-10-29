09c15d15d16f8ac92adb5e68229319b30af522e3
Georgi <bulbrainn@gmail.com>
2022-10-29 22:42:58 +0300

    Added Animals

---
+++ 04.C#OOP/04.InheritanceExercise/06.Animals/Animal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private int age;
+        private string gender;
+
+        public Animal(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public string Name 
+        { 
+            get => name;
+            set
+            {
+                if (!string.IsNullOrEmpty(value)) name = value;
+                else throw new ArgumentException("Invalid input!");
+            }
+        }
+        public int Age
+        {
+            get => age;
+
+            set
+            {
+                if (value >= 0) age = value;
+                else throw new ArgumentException("Invalid input!");
+            }
+        }
+        public string Gender 
+        {
+            get => gender;
+            set
+            {
+                if (!string.IsNullOrEmpty(value)) gender = value;
+                else throw new ArgumentException("Invalid input!");
+            }
+        }
+
+        public virtual string ProduceSound() => "Make sound.";
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.GetType().Name}");
+            sb.AppendLine($"{Name} {Age} {Gender}");
+            sb.AppendLine($"{ProduceSound()}");
+
+            return sb.ToString().Trim();
+        }
+
+    }
+}
---
+++ 04.C#OOP/04.InheritanceExercise/06.Animals/StartUp.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            string type = Console.ReadLine();
+
+            while (type != "Beast!")
+            {
+                string[] animalProp = Console.ReadLine().Split();
+                string name = animalProp[0];
+                int age = int.Parse(animalProp[1]);
+                string gender = string.Empty;
+                if (animalProp.Length > 2)
+                {
+                    gender = animalProp[2];
+                }
+                
+
+                Animal animal = null;
+
+                try
+                {
+                    switch (type)
+                    {
+                        case "Dog":
+                            animal = new Dog(name, age, gender);
+                            break;
+                        case "Cat":
+                            animal = new Cat(name, age, gender);
+                            break;
+                        case "Frog":
+                            animal = new Frog(name, age, gender);
+                            break;
+                        case "Kitten":
+                            animal = new Kitten(name, age);
+                            break;
+                        case "Tomcat":
+                            animal = new Tomcat(name, age);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                if (animal != null) Console.WriteLine(animal);
+
+                type = Console.ReadLine();
+            }
+        }
+    }
+}
