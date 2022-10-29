0b946a56839982ce741da7335038f3ac71583213
Georgi <bulbrainn@gmail.com>
2022-10-29 23:23:09 +0300

    Added Salary Increase

---
+++ 04.C#OOP/05.Encapsulation/02.SalaryIncrease/Person.cs
@@ -0,0 +1,31 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+        public decimal Salary { get; set; }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (Age < 30)
+            {
+                percentage *= 0.5M;
+            }
+            Salary *= 1 + (percentage / 100);
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:f2} leva.";
+        }
+    }
+}
