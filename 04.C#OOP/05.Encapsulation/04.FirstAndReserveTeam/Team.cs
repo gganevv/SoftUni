1a252739f127e914eed4ae5e169d618612d8803d
Georgi <bulbrainn@gmail.com>
2022-10-30 00:33:49 +0300

    Added First And Reserve Team

---
+++ 04.C#OOP/05.Encapsulation/04.FirstAndReserveTeam/Team.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+        public Team(string name)
+        {
+            Name = name;
+            firstTeam = new List<Person>();
+            reserveTeam = new List<Person>();
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public List<Person> FirstTeam 
+        { 
+            get => firstTeam;
+        }
+
+        public List<Person> ReserveTeam
+        {
+            get => reserveTeam;
+        }
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40) firstTeam.Add(person);
+            else reserveTeam.Add(person);
+        }
+    }
+}
