482eb65ddf66787b41f8ba353f1df2e54a06956c
Georgi <bulbrainn@gmail.com>
2022-11-02 22:32:45 +0200

    Added Animal Farm

---
+++ 04.C#OOP/06.EncapsulationExercise/02.AnimalFarm/ExceptionMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace AnimalFarm
+{
+    public static class ExceptionMessages
+    {
+        public const string INVALID_NAME = "Name cannot be empty.";
+        public const string INVALID_AGE = "Age should be between 0 and 15.";
+    }
+}
