a6fa8cbda02289a7f55800bfee296e3be38f646b
Georgi <bulbrainn@gmail.com>
2022-11-02 20:49:50 +0200

    Added Class Box Data

---
+++ 04.C#OOP/06.EncapsulationExercise/01.ClassBoxData/Box.cs
@@ -0,0 +1,72 @@
+﻿namespace _01.ClassBoxData
+{
+    using System;
+    using System.Text;
+
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double lenght, double width, double height)
+        {
+            Length = lenght;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length
+        {
+            get => length;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(string.Format(ErrorMessages.INVALID_PARAMETHER_MESSAGE, nameof(Length)));
+                }
+                length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => width;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(string.Format(ErrorMessages.INVALID_PARAMETHER_MESSAGE, nameof(Width)));
+                }
+                width = value;
+            }
+        }
+
+        public double Height
+        {
+            get => height;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(string.Format(ErrorMessages.INVALID_PARAMETHER_MESSAGE, nameof(Height)));
+                }
+                height = value;
+            }
+        }
+
+        public double SurfaceArea() => LateralSurfaceArea() + (2 * Length * Width);
+        public double LateralSurfaceArea() => (2 * Length * Height) + (2 * Width * Height);
+        public double Volume() => Length * Width * Height;
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Surface Area - {SurfaceArea():f2}");
+            sb.AppendLine($"Lateral Surface Area - {LateralSurfaceArea():f2}");
+            sb.AppendLine($"Volume - {Volume():f2}");
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ 04.C#OOP/06.EncapsulationExercise/01.ClassBoxData/ErrorMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace _01.ClassBoxData
+{
+    public static class ErrorMessages
+    {
+        public const string INVALID_PARAMETHER_MESSAGE = "{0} cannot be zero or negative.";
+    }
+}
