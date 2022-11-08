f67876d0cf450b843834e9deb243741d4feb41bf
Georgi <bulbrainn@gmail.com>
2022-11-08 17:43:06 +0200

    Added Math Operations

---
+++ 04.C#OOP/09.Polymorphism/01.MathOperations/MathOperations.cs
@@ -0,0 +1,13 @@
+﻿namespace Operations
+{
+    public class MathOperations
+    {
+        public int Add(int v1, int v2) => v1 + v2;
+
+        public double Add(double v1, double v2, double v3)
+        => v1 + v2 + v3;
+
+        public decimal Add(decimal v1, decimal v2, decimal v3)
+        => v1 + v2 + v3;
+    }
+}
