e8878fc7a2e8a94e1c56bb1fba9951fcbae8b46d
Georgi <bulbrainn@gmail.com>
2022-11-12 00:59:14 +0200

    Added Square Root

---
+++ 04.C#OOP/11.ExceptionHandling/01.SquareRoot/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace _01.SquareRoot
+{
+    using System;
+    public class StartUp
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            try
+            {
+                if (num < 0)
+                {
+                    throw new InvalidOperationException("Invalid number.");
+                }
+                Console.WriteLine(Math.Sqrt(num));
+            }
+            catch (InvalidOperationException ioe)
+            {
+                Console.WriteLine(ioe.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
