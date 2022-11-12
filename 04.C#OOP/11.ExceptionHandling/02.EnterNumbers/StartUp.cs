11e0553328a36439b8963ad27ebdc99a841957de
Georgi <bulbrainn@gmail.com>
2022-11-12 02:18:55 +0200

    Added Enter Numbers

---
+++ 04.C#OOP/11.ExceptionHandling/02.EnterNumbers/StartUp.cs
@@ -0,0 +1,59 @@
+﻿namespace _02.EnterNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            List<int> numbers = new List<int>();
+            while (numbers.Count < 10)
+            {
+                try
+                {
+                    int currentNum = int.Parse(Console.ReadLine());
+                    if (!numbers.Any())
+                    {
+                        ReadNumber(1, 100, currentNum, numbers);
+                    }
+                    else
+                    {
+                        ReadNumber(numbers[numbers.Count - 1], 100, currentNum, numbers);
+                    }
+                }
+                catch (ArgumentException ae)
+                {
+                    Console.WriteLine(ae.Message);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid Number!");
+                }
+            }
+
+            Console.WriteLine(String.Join(", ", numbers));
+        }
+
+        public static void ReadNumber(int start, int end, int number, List<int> numbers)
+        {
+            if (number <= start || number >= end)
+            {
+                if (numbers.Any())
+                {
+                    throw new ArgumentException($"Your number is not in range {numbers[numbers.Count - 1]} - 100!");
+                }
+                else
+                {
+                    throw new ArgumentException($"Your number is not in range 1 - 100!");
+                }
+                
+            }
+            else
+            {
+                numbers.Add(number);
+            }
+        }
+    }
+}
