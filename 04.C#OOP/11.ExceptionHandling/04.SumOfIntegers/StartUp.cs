164dbd64c2b504bc198e78821749f6e366b3e6ef
Georgi <bulbrainn@gmail.com>
2022-11-13 14:52:55 +0200

    Added Sum of Integers

---
+++ 04.C#OOP/11.ExceptionHandling/04.SumOfIntegers/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace _04.SumOfIntegers
+{
+    using System;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            int sum = 0;
+            string[] elements = Console.ReadLine().Split();
+            foreach (var element in elements)
+            {
+                try
+                {
+                    sum += int.Parse(element);
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"The element '{element}' is out of range!");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"The element '{element}' is in wrong format!");
+                }
+                finally
+                {
+                    Console.WriteLine($"Element '{element}' processed - current sum: {sum}");
+                }
+            }
+
+            Console.WriteLine($"The total sum of all integers is: {sum}");
+        }
+    }
+}
