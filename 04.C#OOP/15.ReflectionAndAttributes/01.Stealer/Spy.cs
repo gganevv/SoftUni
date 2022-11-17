1f4fa982a4de673af11581e9e8feb0cb02fec66a
Georgi <bulbrainn@gmail.com>
2022-11-17 23:50:27 +0200

    Added Stealer

---
+++ 04.C#OOP/15.ReflectionAndAttributes/01.Stealer/Spy.cs
@@ -0,0 +1,27 @@
+﻿namespace Stealer
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string[] fields)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Class under investigation: {className}");
+            Type classType = Type.GetType(className);
+            FieldInfo[] fieldInfo = classType.GetFields(
+                BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+
+            Object classInstance = Activator.CreateInstance(classType, new object[] { });
+            foreach (FieldInfo field in fieldInfo.Where(x => fields.Contains(x.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+            return sb.ToString().TrimEnd();
+
+        }
+    }
+}
---
+++ 04.C#OOP/15.ReflectionAndAttributes/01.Stealer/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace Stealer
+{
+    using System;
+    public class StartUp
+    {
+        static void Main()
+        {
+            Spy spy = new Spy();
+            string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+            Console.WriteLine(result);
+        }
+    }
+}
