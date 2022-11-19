e48a251c1decb77addc9b70140678bf568dc5c8b
Georgi <bulbrainn@gmail.com>
2022-11-19 15:28:06 +0200

    Added Validation Attributes

---
+++ 04.C#OOP/16.ReflectionAndAttributesExercise/02.ValidationAttributes/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace ValidationAttributes
+{
+    using Attributes;
+    public class Person
+    {
+        private const int MIN_AGE = 12;
+        private const int MAX_AGE = 90;
+        public Person(string fullName, int age)
+        {
+            FullName = fullName;
+            Age = age;
+        }
+        [MyRequired]
+        public string FullName { get; set; }
+        [MyRange(MIN_AGE, MAX_AGE)]
+        public int Age { get; set; }
+    }
+}
---
+++ 04.C#OOP/16.ReflectionAndAttributesExercise/02.ValidationAttributes/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace ValidationAttributes
+{
+    using System;
+    public class StartUp
+    {
+        static void Main()
+        {
+            var person = new Person
+             (
+                 null,
+                 -1
+             );
+
+            bool isValidEntity = Validator.IsValid(person);
+
+            Console.WriteLine(isValidEntity);
+        }
+    }
+}
