5d0b0d7c761ab7fc9a8c4149b17cf6f47cf14746
Georgi <bulbrainn@gmail.com>
2022-11-28 23:47:40 +0200

    Added Singleton

---
+++ 04.C#OOP/21.DesignPatterns/01.Singleton/ISingletonContainer.cs
@@ -0,0 +1,7 @@
+﻿namespace _01.Singleton
+{
+    public interface ISingletonContainer
+    {
+        int GetPopulation(string name);
+    }
+}
---
+++ 04.C#OOP/21.DesignPatterns/01.Singleton/SingletonDataContainer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _01.Singleton
+{
+    public class SingletonDataContainer : ISingletonContainer
+    {
+        private static SingletonDataContainer instance = new SingletonDataContainer();
+        private Dictionary<string, int> capitals = new Dictionary<string, int>();
+        private SingletonDataContainer()
+        {
+            Console.WriteLine("Initializing singleton object");
+
+            var elements = File.ReadAllLines("capitals.txt");
+            for (int i = 0; i < elements.Length; i += 2)
+            {
+                capitals.Add(elements[i], int.Parse(elements[i + 1]));
+            }
+        }
+        public int GetPopulation(string name) => capitals[name];
+        public static SingletonDataContainer Instance => instance;
+    }
+}
