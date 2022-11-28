0638232a436225bcbe2bbdae30f9513db618bfc4
Georgi <bulbrainn@gmail.com>
2022-11-29 00:09:00 +0200

    Added Facade

---
+++ 04.C#OOP/21.DesignPatterns/02.Facade/CarAddressBuilder.cs
@@ -0,0 +1,22 @@
+﻿namespace _02.Facade
+{
+    public class CarAddressBuilder : CarBuilderFacade
+    {
+        public CarAddressBuilder(Car car)
+        {
+            Car = car;
+        }
+
+        public CarAddressBuilder inCity(string city)
+        {
+            Car.City = city;
+            return this;
+        }
+
+        public CarAddressBuilder AtAddress(string addresss)
+        {
+            Car.Address = addresss;
+            return this;
+        }
+    }
+}
--- 04.C#OOP/21.DesignPatterns/02.Facade/StartUp.cs
+++ 04.C#OOP/21.DesignPatterns/02.Facade/StartUp.cs
@@ -5,6 +5,17 @@
     {
         static void Main(string[] args)
         {
+            var car = new CarBuilderFacade()
+                .Info
+                    .WithType("BMW")
+                    .WithColor("Black")
+                    .WithNumberOfDoors(5)
+                .Built
+                    .inCity("Leipzig ")
+                    .AtAddress("Some address 254")
+                    .Build();
+
+            Console.WriteLine(car);
         }
     }
 }
