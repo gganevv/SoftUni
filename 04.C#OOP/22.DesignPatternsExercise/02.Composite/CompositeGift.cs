6f97ad3f531f9a27732d6a0f32b87f4339b55169
Georgi <bulbrainn@gmail.com>
2022-11-29 23:58:23 +0200

    Added Composite

---
+++ 04.C#OOP/22.DesignPatternsExercise/02.Composite/CompositeGift.cs
@@ -0,0 +1,36 @@
+﻿namespace _02.Composite
+{
+    using System;
+    using System.Collections.Generic;
+    public class CompositeGift : GiftBase, IGiftOperations
+    {
+        private List<GiftBase> gifts;
+        public CompositeGift(string name, int price) : base(name, price)
+        {
+            gifts = new List<GiftBase>();
+        }
+
+        public void Add(GiftBase gift)
+        {
+            gifts.Add(gift);
+        }
+
+        public override int CalculateTotalPrice()
+        {
+            int totalPrice = 0;
+
+            Console.WriteLine($"{name} contains the following products with prices:");
+            foreach (var gift in gifts)
+            {
+                totalPrice += gift.CalculateTotalPrice();
+            }
+
+            return totalPrice;
+        }
+
+        public void Remove(GiftBase gift)
+        {
+            gifts.Remove(gift);
+        }
+    }
+}
---
+++ 04.C#OOP/22.DesignPatternsExercise/02.Composite/IGiftOperations.cs
@@ -0,0 +1,8 @@
+﻿namespace _02.Composite
+{
+    public interface IGiftOperations
+    {
+        void Add(GiftBase gift);
+        void Remove(GiftBase gift);
+    }
+}
