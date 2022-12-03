59af0655a7936439f655c665a0db5daed109b407
Georgi <bulbrainn@gmail.com>
2022-12-04 00:16:18 +0200

    Added Exam Preparation 2 Structure

---
+++ 04.C#OOP/24.ExamPreparation2/NavalVessels-Skeleton/NavalVessels/Models/Captain.cs
@@ -0,0 +1,65 @@
+﻿namespace NavalVessels.Models
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Collections.Generic;
+
+    using Contracts;
+    using Utilities.Messages;
+
+    public class Captain : ICaptain
+    {
+        private HashSet<IVessel> vessels;
+        private string fullName;
+
+        public Captain(string fullname)
+        {
+            FullName = fullname;
+            vessels = new HashSet<IVessel>();
+            CombatExperience = 0;
+        }
+        public string FullName
+        {
+            get => fullName;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(ExceptionMessages.InvalidCaptainName);
+                }
+                fullName = value;
+            }
+        }
+
+        public int CombatExperience { get; private set; }
+
+        public ICollection<IVessel> Vessels => vessels;
+
+        public void AddVessel(IVessel vessel)
+        {
+            if (vessel == null)
+            {
+                throw new NullReferenceException(ExceptionMessages.InvalidVesselForCaptain);
+            }
+
+            vessels.Add(vessel);
+        }
+
+        public void IncreaseCombatExperience() => CombatExperience += 10;
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{FullName} has {CombatExperience} combat experience and commands {vessels.Count} vessels.");
+            if (vessels.Any())
+            {
+                foreach (var vessel in vessels)
+                {
+                    Console.WriteLine(vessel);
+                }
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ 04.C#OOP/24.ExamPreparation2/NavalVessels-Skeleton/NavalVessels/Models/Submarine.cs
@@ -0,0 +1,42 @@
+﻿namespace NavalVessels.Models
+{
+    using Contracts;
+    using System;
+
+    public class Submarine : Vessel, ISubmarine
+    {
+        private const double DEFAULT_ARMOR_THICKNES = 200;
+        public Submarine(string name, double mainWeaponCaliber, double speed, double armorThickness) : base(name, mainWeaponCaliber, speed, armorThickness)
+        {
+            SubmergeMode = false;
+        }
+
+        public bool SubmergeMode { get; private set; }
+
+        public override void RepairVessel()
+        {
+            ArmorThickness = DEFAULT_ARMOR_THICKNES;
+        }
+
+        public void ToggleSubmergeMode()
+        {
+            if (SubmergeMode == false)
+            {
+                MainWeaponCaliber += 40;
+                Speed -= 4;
+                SubmergeMode = true;
+            }
+            else
+            {
+                MainWeaponCaliber -= 40;
+                Speed += 4;
+                SubmergeMode = false;
+            }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + Environment.NewLine + $" *Submerge mode: {(SubmergeMode ? "On" : "Off")}";
+        }
+    }
+}
