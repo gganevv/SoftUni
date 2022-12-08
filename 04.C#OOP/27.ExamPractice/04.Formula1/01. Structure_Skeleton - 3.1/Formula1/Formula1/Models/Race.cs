f96e48d61496501c61358dafb315e83e45e91010
Georgi <bulbrainn@gmail.com>
2022-12-08 19:49:45 +0200

    Added Formula 1 Strucure And Buisiness Logic

---
+++ 04.C#OOP/27.ExamPractice/04.Formula1/01. Structure_Skeleton - 3.1/Formula1/Formula1/Models/Race.cs
@@ -0,0 +1,77 @@
+﻿namespace Formula1.Models
+{
+    using System;
+
+    using Formula1.Models.Contracts;
+    using System.Collections.Generic;
+    using Formula1.Utilities;
+    using System.Text;
+
+    public class Race : IRace
+    {
+        private string raceName;
+        private int numberOfLaps;
+        private bool tookPlace;
+        private readonly ICollection<IPilot> pilots;
+
+        public Race(string raceName, int numberOfLaps)
+        {
+            RaceName = raceName;
+            NumberOfLaps = numberOfLaps;
+            pilots = new List<IPilot>();
+        }
+
+        public string RaceName
+        {
+            get => raceName;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 5)
+                {
+                    throw new ArgumentException(String.Format(ExceptionMessages.InvalidRaceName, value));
+                }
+                raceName = value;
+            }
+        }
+
+        public int NumberOfLaps
+        {
+            get => numberOfLaps;
+            private set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException(String.Format(ExceptionMessages.InvalidLapNumbers, value));
+                }
+
+                numberOfLaps = value;
+            }
+        }
+
+        public bool TookPlace { get => tookPlace; set => tookPlace = value; }
+
+        public ICollection<IPilot> Pilots
+        {
+            get
+            {
+                return pilots;
+            }
+        }
+
+        public void AddPilot(IPilot pilot)
+        {
+            Pilots.Add(pilot);
+        }
+
+        public string RaceInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The {RaceName} race has:");
+            sb.AppendLine($"Participants: {Pilots.Count}");
+            sb.AppendLine($"Number of laps: {NumberOfLaps}");
+            sb.AppendLine($"Took place: {(tookPlace ? "Yes" : "No")}");
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ 04.C#OOP/27.ExamPractice/04.Formula1/01. Structure_Skeleton - 3.1/Formula1/Formula1/Models/Williams.cs
@@ -0,0 +1,9 @@
+﻿namespace Formula1.Models
+{
+    public class Williams : FormulaOneCar
+    {
+        public Williams(string model, int horsePower, double enineDisplacement) : base(model, horsePower, enineDisplacement)
+        {
+        }
+    }
+}
