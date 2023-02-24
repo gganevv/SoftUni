ecd9e51180f9566d96d3f5cbb66e9e1ee13bde9b
Georgi <bulbrainn@gmail.com>
2023-02-24 23:12:41 +0200

    Added Villain Names

---
+++ 06.EntityFramework/04.ADO.NETExercise/02.VillainNames/Config.cs
@@ -0,0 +1,8 @@
+﻿namespace _02.VillainNames
+{
+    internal static class Config
+    {
+        public const string ConnectionString =
+            @"Server=GANEV;Database=MinionsDB;Integrated Security=True;Trust Server Certificate=True";
+    }
+}
---
+++ 06.EntityFramework/04.ADO.NETExercise/02.VillainNames/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace _02.VillainNames
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection(Config.ConnectionString);
+            connection.Open();
+
+            SqlCommand command = new SqlCommand(SqlQueries.GetAllVillians, connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            while(reader.Read())
+            {
+                string villainName = reader["Name"].ToString();
+                int numberOfMinions = (int)reader["MinionsCount"];
+                Console.WriteLine($"{villainName} - {numberOfMinions}");
+            }
+
+        }
+    }
+}
