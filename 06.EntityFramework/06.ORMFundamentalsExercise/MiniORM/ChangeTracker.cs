d29cd6ef5388f7ff78c3f63e2d7dad58ffa556e0
Georgi <bulbrainn@gmail.com>
2023-02-25 23:55:29 +0200

    Added MiniORM Workshop

---
+++ 06.EntityFramework/06.ORMFundamentalsExercise/MiniORM/ChangeTracker.cs
@@ -0,0 +1,112 @@
+﻿namespace MiniORM;
+
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+internal class ChangeTracker<T>
+    where T : class, new()
+{
+    private readonly IList<T> allEntities;
+    private readonly IList<T> added;
+    private readonly IList<T> removed;
+
+    private ChangeTracker()
+    {
+        this.added = new List<T>();
+        this.removed = new List<T>();
+    }
+
+    public ChangeTracker(IEnumerable<T> allEntities) 
+        : this()
+    {
+        this.allEntities = this.CloneEntities(allEntities);
+    }
+
+    public IReadOnlyCollection<T> AllEntities
+        => (IReadOnlyCollection<T>)this.allEntities;
+
+    public IReadOnlyCollection<T> Added
+        => (IReadOnlyCollection<T>)this.added;
+
+    public IReadOnlyCollection<T> Removed
+        => (IReadOnlyCollection<T>)this.removed;
+
+    public IEnumerable<T> GetModifiedEntities(DbSet<T> dbSet)
+    {
+        IList<T> modifiedEntities = new List<T>();
+        PropertyInfo[] primaryKeys = typeof(T)
+            .GetProperties()
+            .Where(pi => pi.HasAttribute<KeyAttribute>())
+            .ToArray();
+
+        foreach (T proxyEntity in this.AllEntities)
+        {
+            object[] primaryKeyValues = this.GetPrimaryKeyValues(primaryKeys, proxyEntity)
+                .ToArray();
+            T entity = dbSet.Entities
+                .FirstOrDefault(e => this.GetPrimaryKeyValues(primaryKeys, e)
+                .SequenceEqual(primaryKeyValues));
+            if (entity == null)
+            {
+                continue;
+            }
+
+            bool isModified = this.IsModified(proxyEntity, entity);
+
+            if (isModified)
+            {
+                modifiedEntities.Add(proxyEntity);
+            }
+        }
+
+        return modifiedEntities;
+    }
+
+    private bool IsModified(T proxyEntity, T originalEntity)
+    {
+        PropertyInfo[] monitoredProperties = typeof(T)
+            .GetProperties()
+            .Where(pi => DbContext.AlloledSqlTypes.Contains(pi.PropertyType))
+            .ToArray();
+
+        PropertyInfo[] modifiedProperties = monitoredProperties
+            .Where(pi => !Equals(pi.GetValue(proxyEntity), pi.GetValue(originalEntity)))
+            .ToArray();
+
+        return modifiedProperties.Any();
+    }
+
+    private IEnumerable<object> GetPrimaryKeyValues(IEnumerable<PropertyInfo> primaryKeys, T proxyEntity)
+    {
+        return primaryKeys.Select(pk => pk.GetValue(proxyEntity));
+    }
+
+    public void Add(T entity)
+        => this.added.Add(entity);
+
+    public void Remove(T entity)
+        => this.removed.Add(entity);
+
+    private IList<T> CloneEntities(IEnumerable<T> originalEntities)
+    {
+        IList<T> clonedEntities = new List<T>();
+        PropertyInfo[] propertiesToClone = typeof(T)
+            .GetProperties()
+            .Where(pi => DbContext.AlloledSqlTypes.Contains(pi.PropertyType))
+            .ToArray();
+
+        foreach (T originalEntity in originalEntities)
+        {
+            T entityClone = Activator.CreateInstance<T>();
+            foreach (PropertyInfo property in propertiesToClone)
+            {
+                object originalValue = property.GetValue(originalEntity);
+                property.SetValue(entityClone, originalValue);
+            }
+
+            clonedEntities.Add(entityClone);
+        }
+
+        return clonedEntities;
+    }
+}
---
+++ 06.EntityFramework/06.ORMFundamentalsExercise/MiniORM/ConnectionManager.cs
@@ -0,0 +1,17 @@
+﻿namespace MiniORM;
+
+internal class ConnectionManager : IDisposable
+{
+    private readonly DatabaseConnection connection;
+    public ConnectionManager(DatabaseConnection connection)
+    {
+        this.connection = connection;
+
+        this.connection.Open();
+    }
+
+    public void Dispose()
+    {
+        this.connection.Close();
+    }
+}
