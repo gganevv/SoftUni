a328bf6b3f86cda9d40e00cbd5b8928dfa89937b
Georgi <bulbrainn@gmail.com>
2023-03-01 01:23:59 +0200

    Added Employees With Salary over 50 000

--- 06.EntityFramework/08.EntityFrameworkIntroductionExercise/08.EntityFrameworkIntroductionExercise/StartUp.cs
+++ 06.EntityFramework/08.EntityFrameworkIntroductionExercise/08.EntityFrameworkIntroductionExercise/StartUp.cs
@@ -8,7 +8,12 @@ public class StartUp
     static void Main(string[] args)
     {
         SoftUniContext dBcontext = new SoftUniContext();
-        string result = GetEmployeesFullInformation(dBcontext);
+
+        //Problem 03 Employees Full Information
+        //string result = GetEmployeesFullInformation(dBcontext);
+
+        //Problem 04 Employees With Salary Over 50 000
+        string result = GetEmployeesWithSalaryOver50000(dBcontext);
         Console.WriteLine(result);
     }
 
@@ -32,6 +37,29 @@ public class StartUp
         {
             sb.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}");
         }
+        return sb.ToString().TrimEnd();
+    }
+
+    public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        var employees = context.Employees
+            .Where(e => e.Salary > 50000)
+            .Select(e => new
+            {
+                e.FirstName,
+                e.Salary
+            })
+            .OrderBy(e => e.FirstName)
+            .ToArray();
+
+        foreach (var e in employees)
+        {
+            sb.AppendLine($"{e.FirstName} - {e.Salary:f2}");
+        }
+
+
         return sb.ToString().TrimEnd();
     }
 }
