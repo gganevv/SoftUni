c9d02d3695bef943769cb521a9cd32f695fd099e
Georgi <bulbrainn@gmail.com>
2023-03-01 01:16:13 +0200

    Added Employees Full Information

--- 06.EntityFramework/08.EntityFrameworkIntroductionExercise/08.EntityFrameworkIntroductionExercise/StartUp.cs
+++ 06.EntityFramework/08.EntityFrameworkIntroductionExercise/08.EntityFrameworkIntroductionExercise/StartUp.cs
@@ -1,11 +1,37 @@
 ﻿using SoftUni.Data;
+using System.Text;
 
-namespace EntityFrameworkIntroductionExercise;
+namespace SoftUni;
 
 public class StartUp
 {
     static void Main(string[] args)
     {
         SoftUniContext dBcontext = new SoftUniContext();
+        string result = GetEmployeesFullInformation(dBcontext);
+        Console.WriteLine(result);
+    }
+
+    //Problem 03
+    public static string GetEmployeesFullInformation(SoftUniContext context)
+    {
+        StringBuilder sb = new StringBuilder();
+        var employees = context.Employees
+            .OrderBy(e => e.EmployeeId)
+            .Select(e => new
+            {
+                e.FirstName,
+                e.LastName,
+                e.MiddleName,
+                e.JobTitle,
+                e.Salary
+            })
+            .ToArray();
+
+        foreach (var e in employees)
+        {
+            sb.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}");
+        }
+        return sb.ToString().TrimEnd();
     }
 }
