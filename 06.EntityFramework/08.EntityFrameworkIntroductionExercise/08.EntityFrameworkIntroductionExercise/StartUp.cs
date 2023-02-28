2adf907e045fbeb1bf441762067ff8d754c46725
Georgi <bulbrainn@gmail.com>
2023-03-01 01:42:48 +0200

    Added Employees from Research and Development

--- 06.EntityFramework/08.EntityFrameworkIntroductionExercise/08.EntityFrameworkIntroductionExercise/StartUp.cs
+++ 06.EntityFramework/08.EntityFrameworkIntroductionExercise/08.EntityFrameworkIntroductionExercise/StartUp.cs
@@ -13,11 +13,15 @@ public class StartUp
         //string result = GetEmployeesFullInformation(dBcontext);
 
         //Problem 04 Employees With Salary Over 50 000
-        string result = GetEmployeesWithSalaryOver50000(dBcontext);
+        //string result = GetEmployeesWithSalaryOver50000(dBcontext);
+
+        //Problem 05 Employees from Research and Development
+        string result = GetEmployeesFromResearchAndDevelopment(dBcontext);
+
         Console.WriteLine(result);
     }
 
-    //Problem 03
+    //Problem 03 Employees Full Information
     public static string GetEmployeesFullInformation(SoftUniContext context)
     {
         StringBuilder sb = new StringBuilder();
@@ -40,6 +44,7 @@ public class StartUp
         return sb.ToString().TrimEnd();
     }
 
+    //Problem 04 Employees With Salary Over 50 000
     public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
     {
         StringBuilder sb = new StringBuilder();
@@ -59,6 +64,32 @@ public class StartUp
             sb.AppendLine($"{e.FirstName} - {e.Salary:f2}");
         }
 
+
+        return sb.ToString().TrimEnd();
+    }
+
+    //Problem 05 Employees from Research and Development
+    public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        var empleyees = context.Employees
+            .Where(e => e.Department.Name == "Research and Development")
+            .OrderBy(e => e.Salary)
+            .ThenByDescending(e => e.FirstName)
+            .Select(e => new
+            {
+                e.FirstName,
+                e.LastName,
+                Department = e.Department.Name,
+                e.Salary
+            })
+            .ToArray();
+
+        foreach (var e in empleyees)
+        {
+            sb.AppendLine($"{e.FirstName} {e.LastName} from {e.Department} - ${e.Salary:f2}");
+        }
 
         return sb.ToString().TrimEnd();
     }
