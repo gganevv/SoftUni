78c00cca00e7585c0113d4239f9f4acdb63b43d2
Georgi <bulbrainn@gmail.com>
2023-03-01 21:56:16 +0200

    Added Get Employees In Period

--- 06.EntityFramework/08.EntityFrameworkIntroductionExercise/08.EntityFrameworkIntroductionExercise/StartUp.cs
+++ 06.EntityFramework/08.EntityFrameworkIntroductionExercise/08.EntityFrameworkIntroductionExercise/StartUp.cs
@@ -1,6 +1,7 @@
 ﻿using SoftUni.Data;
 using System.Text;
 using SoftUni.Models;
+using System.Globalization;
 
 namespace SoftUni;
 
@@ -20,7 +21,10 @@ public class StartUp
         //string result = GetEmployeesFromResearchAndDevelopment(dBcontext);
 
         //Problem 06 Adding a New Address and Updating Employee
-        string result = AddNewAddressToEmployee(dBcontext);
+        //string result = AddNewAddressToEmployee(dBcontext);
+
+        //Problem 07 Get Employees In Period
+        string result = GetEmployeesInPeriod(dBcontext);
 
         Console.WriteLine(result);
     }
@@ -111,7 +115,7 @@ public class StartUp
 
         Employee employee = context.Employees
             .FirstOrDefault(e => e.LastName == "Nakov")!;
-        
+
         employee.Address = address;
 
         context.SaveChanges();
@@ -125,6 +129,45 @@ public class StartUp
         foreach (var e in employees)
         {
             sb.AppendLine(e);
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+
+    public static string GetEmployeesInPeriod(SoftUniContext context)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        var employees = context.Employees
+            .Take(10)
+            .Select(e => new
+            {
+                e.FirstName,
+                e.LastName,
+                ManagerFirstName = e.Manager.FirstName,
+                ManagerLastName = e.Manager.LastName,
+                Projects = e.EmployeesProjects
+                .Where(ep => ep.Project.StartDate.Year >= 2001 &&
+                ep.Project.StartDate.Year <= 2003)
+                .Select(ep => new
+                {
+                    ProjectName = ep.Project.Name,
+                    StartDate = ep.Project.StartDate.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture),
+                    EndDate = ep.Project.EndDate.HasValue
+                            ? ep.Project.EndDate.Value.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture)
+                            : "not finished"
+                })
+                .ToArray()
+            })
+            .ToArray();
+
+        foreach (var e in employees)
+        {
+            sb.AppendLine($"{e.FirstName} {e.LastName} - Manager: {e.ManagerFirstName} {e.ManagerLastName}");
+            foreach (var p in e.Projects)
+            {
+                sb.AppendLine($"--{p.ProjectName} - {p.StartDate} - {p.EndDate}");
+            }
         }
 
         return sb.ToString().TrimEnd();
