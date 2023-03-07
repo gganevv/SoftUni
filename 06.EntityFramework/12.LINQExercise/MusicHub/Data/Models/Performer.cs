764e3928fd32f590fdcd5ff52e94666d6df274c0
Georgi <bulbrainn@gmail.com>
2023-03-07 03:08:53 +0200

    Added MusicHub

---
+++ 06.EntityFramework/12.LINQExercise/MusicHub/Data/Models/Performer.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicHub.Data.Models;
+
+public class Performer
+{
+    public Performer()
+    {
+        PerformerSongs = new HashSet<SongPerformer>();
+    }
+
+    [Key]
+    public int Id { get; set; }
+
+    [MaxLength(ValidationConstraints.PerformerFirstNameMaxLength)]
+    public string FirstName { get; set; } = null!;
+
+    [MaxLength(ValidationConstraints.PerformerLastNameMaxLength)]
+    public string LastName { get; set; } = null!;
+
+    public int Age { get; set; }
+
+    public decimal NetWorth { get; set; }
+
+    public ICollection<SongPerformer> PerformerSongs { get; set; }
+}
---
+++ 06.EntityFramework/12.LINQExercise/MusicHub/Data/MusicHubDbContext.cs
@@ -0,0 +1,48 @@
+﻿namespace MusicHub.Data;
+
+using Microsoft.EntityFrameworkCore;
+using MusicHub.Data.Models;
+
+public class MusicHubDbContext : DbContext
+{
+    public MusicHubDbContext()
+    {
+    }
+
+    public MusicHubDbContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder
+                .UseSqlServer(Configuration.ConnectionString);
+        }
+    }
+
+    public DbSet<Album> Albums { get; set; } = null!;
+
+    public DbSet<Performer> Performers { get; set; } = null!;
+
+    public DbSet<Producer> Producers { get; set; } = null!;
+
+    public DbSet<Song> Songs { get; set; } = null!;
+
+    public DbSet<SongPerformer> SongsPerformers { get; set; } = null!;
+
+    public DbSet<Writer> Writers { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<SongPerformer>(entity =>
+        {
+            entity
+            .HasKey(sp => new { sp.SongId, sp.PerformerId });
+        });
+    }
+}
