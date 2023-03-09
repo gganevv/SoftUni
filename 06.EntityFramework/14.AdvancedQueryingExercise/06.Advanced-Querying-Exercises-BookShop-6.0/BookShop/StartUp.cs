8cce8a69428dd8ac7b5aef22b3db6c5cc9c3d704
Georgi <bulbrainn@gmail.com>
2023-03-09 20:02:38 +0200

    Added Age Restriction

--- 06.EntityFramework/14.AdvancedQueryingExercise/06.Advanced-Querying-Exercises-BookShop-6.0/BookShop/StartUp.cs
+++ 06.EntityFramework/14.AdvancedQueryingExercise/06.Advanced-Querying-Exercises-BookShop-6.0/BookShop/StartUp.cs
@@ -1,16 +1,44 @@
-﻿namespace BookShop
-{
-    using Data;
-    using Initializer;
+﻿namespace BookShop;
 
-    public class StartUp
+using BookShop.Models.Enums;
+using Data;
+using Initializer;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+
+public class StartUp
+{
+    public static void Main()
     {
-        public static void Main()
+        using var db = new BookShopContext();
+        //DbInitializer.ResetDatabase(db);
+
+        //02. Age Restriction
+        string input = Console.ReadLine();
+        Console.WriteLine(GetBooksByAgeRestriction(db, input));
+
+    }
+
+    public static string GetBooksByAgeRestriction(BookShopContext context, string ageRestriction)
+    {
+        StringBuilder sb = new StringBuilder();
+        List<string> bookTitles = new List<string>();
+
+        if(Enum.TryParse(ageRestriction, true, out AgeRestriction ageR))
         {
-            using var db = new BookShopContext();
-            DbInitializer.ResetDatabase(db);
+            bookTitles = context.Books
+            .Where(b => b.AgeRestriction == ageR)
+            .Select(b => b.Title)
+            .OrderBy(b => b)
+            .ToList();
         }
+
+        
+
+        foreach (var book in bookTitles)
+        {
+            sb.AppendLine(book);
+        }
+        return sb.ToString().TrimEnd();
     }
 }
-
-
