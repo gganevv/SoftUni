77362844e7036d3a67ef130f5fd7eacd526b497a
Georgi <bulbrainn@gmail.com>
2023-03-17 22:12:02 +0200

    Added Import Suppliers

--- 06.EntityFramework/18.JSONProcessingExercise/CarDealer/CarDealerProfile.cs
+++ 06.EntityFramework/18.JSONProcessingExercise/CarDealer/CarDealerProfile.cs
@@ -1,11 +1,17 @@
 ﻿namespace CarDealer;
 
 using AutoMapper;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
 
 public class CarDealerProfile : Profile
 {
     public CarDealerProfile()
     {
+        //Supplier
+        this.CreateMap<ImportSupplierDto, Supplier>()
+            .ForMember(d => d.IsImporter, opt => opt.MapFrom(s => s.IsImporter))
+            .ForMember(d => d.Name, opt => opt.MapFrom(s => s.Name));
 
     }
 }
--- 06.EntityFramework/18.JSONProcessingExercise/CarDealer/StartUp.cs
+++ 06.EntityFramework/18.JSONProcessingExercise/CarDealer/StartUp.cs
@@ -1,9 +1,46 @@
-﻿namespace CarDealer;
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer;
 
 public class StartUp
 {
     public static void Main()
     {
+        CarDealerContext context = new CarDealerContext();
 
+        //09. Import Suppliers
+        string inputJson = File.ReadAllText("../../../Datasets/suppliers.json");
+        Console.WriteLine(ImportSuppliers(context, inputJson));
+    }
+
+    public static string ImportSuppliers(CarDealerContext context, string inputJson)
+    {
+        IMapper mapper = CreateMapper();
+
+        ImportSupplierDto[] importSupplierDtos = JsonConvert.DeserializeObject<ImportSupplierDto[]>(inputJson);
+
+        HashSet<Supplier> suppliers = new HashSet<Supplier>();
+        foreach (var importSupplierDto in importSupplierDtos)
+        {
+            Supplier supplier = mapper.Map<Supplier>(importSupplierDto);
+            suppliers.Add(supplier);
+        }
+
+        context.AddRange(suppliers);
+        context.SaveChanges();
+
+        return $"Successfully imported {suppliers.Count}.";
+    }
+
+    private static IMapper CreateMapper()
+    {
+        return new Mapper(new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<CarDealerProfile>();
+        }));
     }
 }
