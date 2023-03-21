4d6a73e0586041a003da2273986383fa284b69c3
Georgi <bulbrainn@gmail.com>
2023-03-21 21:48:07 +0200

    Added Import Suppliers

--- 06.EntityFramework/20.XMLProcessingExercise/CarDealer/CarDealerProfile.cs
+++ 06.EntityFramework/20.XMLProcessingExercise/CarDealer/CarDealerProfile.cs
@@ -2,10 +2,13 @@
 
 using AutoMapper;
 
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+
 public class CarDealerProfile : Profile
 {
     public CarDealerProfile()
     {
-
+        this.CreateMap<ImportSupplierDto, Supplier>();
     }
 }
--- 06.EntityFramework/20.XMLProcessingExercise/CarDealer/StartUp.cs
+++ 06.EntityFramework/20.XMLProcessingExercise/CarDealer/StartUp.cs
@@ -2,11 +2,41 @@
 
 using AutoMapper;
 
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using CarDealer.Utilities;
+
 public class StartUp
 {
     public static void Main()
     {
+        CarDealerContext context = new CarDealerContext();
 
+        //09. Import Suppliers
+        string inputXml = File.ReadAllText("../../../Datasets/suppliers.xml");
+        Console.WriteLine(ImportSuppliers(context, inputXml));
+    }
+
+    //09. Import Suppliers
+    public static string ImportSuppliers(CarDealerContext context, string inputXml)
+    {
+        XmlHelper xmlHelper = new XmlHelper();
+        IMapper mapper = CreateMapper();
+
+        ImportSupplierDto[] importSupplierDtos = xmlHelper.Deserialize<ImportSupplierDto[]>(inputXml, "Suppliers");
+        HashSet<Supplier> suppliers = new HashSet<Supplier>();
+
+        foreach (var supplierDto in importSupplierDtos)
+        {
+            Supplier supplier = mapper.Map<Supplier>(supplierDto);
+            suppliers.Add(supplier);
+        }
+
+        context.Suppliers.AddRange(suppliers);
+        context.SaveChanges();
+
+        return $"Successfully imported {suppliers.Count}";
     }
 
     private static IMapper CreateMapper()
