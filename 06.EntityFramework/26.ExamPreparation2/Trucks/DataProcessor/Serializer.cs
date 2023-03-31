acdd59c117264124379e1228783808791049e6b3
Georgi <bulbrainn@gmail.com>
2023-03-31 19:47:55 +0300

    Added Export Clients with Most Trucks

--- 06.EntityFramework/26.ExamPreparation2/Trucks/DataProcessor/Serializer.cs
+++ 06.EntityFramework/26.ExamPreparation2/Trucks/DataProcessor/Serializer.cs
@@ -1,6 +1,8 @@
 ﻿namespace Trucks.DataProcessor;
 
 using Data;
+using Newtonsoft.Json;
+using Trucks.Data.Models.Enums;
 
 public class Serializer
 {
@@ -11,6 +13,31 @@ public class Serializer
 
     public static string ExportClientsWithMostTrucks(TrucksContext context, int capacity)
     {
-        throw new NotImplementedException();
+        var trucks = context.Clients
+            .Where(c => c.ClientsTrucks.Any(t => t.Truck.CargoCapacity >= capacity))
+            .Select(c => new
+            {
+                Name = c.Name,
+                Trucks = c.ClientsTrucks
+                    .Where(c => c.Truck.TankCapacity >= capacity)
+                    .OrderBy(t => t.Truck.MakeType)
+                    .ThenByDescending(t => t.Truck.CargoCapacity)
+                    .Select(t => new
+                    {
+                        TruckRegistrationNumber = t.Truck.RegistrationNumber,
+                        VinNumber = t.Truck.VinNumber,
+                        TankCapacity = t.Truck.TankCapacity,
+                        CargoCapacity = t.Truck.CargoCapacity,
+                        CategoryType = t.Truck.CategoryType.ToString(),
+                        MakeType = t.Truck.MakeType.ToString()
+                    })
+                    .ToArray()
+            })
+            .OrderByDescending(c => c.Trucks.Count())
+            .ThenBy(c => c.Name)
+            .Take(10)
+            .ToArray();
+
+        return JsonConvert.SerializeObject(trucks, Formatting.Indented);
     }
 }
