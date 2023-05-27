8b77d1f004145425f7316950c8304baa60d25705
Georgi <bulbrainn@gmail.com>
2023-05-27 14:41:41 +0300

    Added Chronometer

---
+++ 07.ASPNETFundamentals/05.StateManagementAndAsynchronousProcessing/StateManagementAndAsynchronousProcessing/10.Chronometer/Chronometer.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+
+namespace _10.Chrono;
+
+public class Chronometer : IChronometer
+{
+    private Stopwatch stopWatch;
+    private List<string> laps;
+
+    public Chronometer()
+    {
+        stopWatch = new Stopwatch();
+        laps = new List<string>();
+    }
+
+    public string GetTime => stopWatch.Elapsed.ToString(@"mm\:ss\.ffff");
+
+    public List<string> Laps => laps;
+
+    public string Lap()
+    {
+        string result = GetTime;
+        laps.Add(result);
+        return result;
+    }
+
+    public void Reset()
+    {
+        stopWatch.Restart();
+        laps.Clear();
+    }
+
+    public void Start()
+    {
+        stopWatch.Start();
+    }
+
+    public void Stop()
+    {
+        stopWatch.Stop();
+    }
+}
