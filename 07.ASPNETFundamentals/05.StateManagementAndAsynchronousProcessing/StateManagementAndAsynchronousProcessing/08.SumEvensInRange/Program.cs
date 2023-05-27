c93bcdaecb47ebc22a0611b3694cd912aa8c23b6
Georgi <bulbrainn@gmail.com>
2023-05-27 14:40:46 +0300

    Added Sum Evens In Range

---
+++ 07.ASPNETFundamentals/05.StateManagementAndAsynchronousProcessing/StateManagementAndAsynchronousProcessing/08.SumEvensInRange/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _08
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var command = Console.ReadLine();
+                if (command == "show")
+                {
+                    var result = SumAsync();
+                    Console.WriteLine(result);
+                }
+            }
+        }
+
+        private static long SumAsync()
+        {
+            return Task.Run(() =>
+            {
+                long sum = 0;
+                for (int i = 1; i < 10000; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        sum += i;
+                    }
+                }
+                return sum;
+            }).Result;
+        }
+    }
+}
