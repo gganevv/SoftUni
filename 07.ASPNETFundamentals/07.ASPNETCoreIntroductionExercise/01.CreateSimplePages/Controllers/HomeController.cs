7ba8bc6e34e751bd82c9c7d4e07255b7128ece05
Georgi <bulbrainn@gmail.com>
2023-05-28 23:37:01 +0300

    Added Home Controller Pages

---
+++ 07.ASPNETFundamentals/07.ASPNETCoreIntroductionExercise/01.CreateSimplePages/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using _01.CreateSimplePages.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _01.CreateSimplePages.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Hello World!";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 07.ASPNETFundamentals/07.ASPNETCoreIntroductionExercise/01.CreateSimplePages/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _01.CreateSimplePages.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
