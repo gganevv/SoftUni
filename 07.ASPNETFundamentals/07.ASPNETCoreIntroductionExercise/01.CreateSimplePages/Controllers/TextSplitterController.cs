a2b56e878d73cab847483e29137ffad50c458298
Georgi <bulbrainn@gmail.com>
2023-05-30 23:54:51 +0300

    Added Controller and Model

---
+++ 07.ASPNETFundamentals/07.ASPNETCoreIntroductionExercise/01.CreateSimplePages/Controllers/TextSplitterController.cs
@@ -0,0 +1,26 @@
+﻿using _01.CreateSimplePages.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _01.CreateSimplePages.Controllers
+{
+    public class TextSplitterController : Controller
+    {
+        public IActionResult Index(TextViewModel model)
+        {
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Split(TextViewModel model)
+        {
+            var splitTextArray = model
+                .Text
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            model.SplitText = string.Join(Environment.NewLine, splitTextArray);
+
+            return RedirectToAction("Index", model);
+        }
+    }
+}
---
+++ 07.ASPNETFundamentals/07.ASPNETCoreIntroductionExercise/01.CreateSimplePages/Models/TextViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace _01.CreateSimplePages.Models
+{
+    public class TextViewModel
+    {
+        public string Text { get; set; } = null!;
+
+        public string SplitText { get; set; } = null!;
+    }
+}
