a57f89fa077fc0402ef27d396df8b794a8c97455
Georgi <bulbrainn@gmail.com>
2023-05-30 23:25:04 +0300

    Added Chat Controllers and Models

---
+++ 07.ASPNETFundamentals/07.ASPNETCoreIntroductionExercise/01.CreateSimplePages/Controllers/ChatController.cs
@@ -0,0 +1,42 @@
+﻿using _01.CreateSimplePages.Models.Message;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _01.CreateSimplePages.Controllers
+{
+    public class ChatController : Controller
+    {
+        private static List<KeyValuePair<string, string>> messeges = new List<KeyValuePair<string, string>>();
+
+        public IActionResult Show()
+        {
+            if (messeges.Count() < 1)
+            {
+                return View(new ChatViewModel());
+            }
+
+            var chatModel = new ChatViewModel()
+            {
+                Messages = messeges
+                .Select(m => new MessageViewModel()
+                {
+                    Sender = m.Key,
+                    MessageText = m.Value
+                })
+                .ToList()
+            };
+
+            return View(chatModel);
+        }
+
+        [HttpPost]
+        public IActionResult Send(ChatViewModel chat)
+        {
+            var newMessage = chat.CurrentMessage;
+
+            messeges.Add(new KeyValuePair<string, string>
+                (newMessage.Sender, newMessage.MessageText));
+
+            return RedirectToAction("Show");
+        }
+    }
+}
---
+++ 07.ASPNETFundamentals/07.ASPNETCoreIntroductionExercise/01.CreateSimplePages/Models/Message/ChatViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace _01.CreateSimplePages.Models.Message
+{
+    public class ChatViewModel
+    {
+        public MessageViewModel CurrentMessage { get; set; } = null!;
+
+        public List<MessageViewModel> Messages { get; set; } = null!;
+    }
+}
