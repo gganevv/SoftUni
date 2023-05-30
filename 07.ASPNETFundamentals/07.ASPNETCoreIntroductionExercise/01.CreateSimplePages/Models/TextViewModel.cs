ce06cf85f1d19bd38baa96d2211b0ab0cd2d1ebe
Georgi <bulbrainn@gmail.com>
2023-05-31 00:12:13 +0300

    Added Validation

--- 07.ASPNETFundamentals/07.ASPNETCoreIntroductionExercise/01.CreateSimplePages/Models/TextViewModel.cs
+++ 07.ASPNETFundamentals/07.ASPNETCoreIntroductionExercise/01.CreateSimplePages/Models/TextViewModel.cs
@@ -1,7 +1,11 @@
-﻿namespace _01.CreateSimplePages.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _01.CreateSimplePages.Models
 {
     public class TextViewModel
     {
+        [Required]
+        [StringLength(30, MinimumLength = 2)]
         public string Text { get; set; } = null!;
 
         public string SplitText { get; set; } = null!;
