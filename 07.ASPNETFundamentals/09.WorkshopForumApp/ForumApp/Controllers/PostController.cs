d2810108688418f0d5d0a310a5c146cb7d220c3f
Georgi <bulbrainn@gmail.com>
2023-06-06 00:59:31 +0300

    Added CRUD operations

---
+++ 07.ASPNETFundamentals/09.WorkshopForumApp/ForumApp/Controllers/PostController.cs
@@ -0,0 +1,37 @@
+﻿using ForumApp.Data;
+using ForumApp.Models.Post;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForumApp.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly ForumAppDbContext Data;
+
+        public PostController(ForumAppDbContext data)
+        {
+            this.Data = data;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> All()
+        {
+            var posts = await Data
+                .Posts
+                .Select(p => new PostViewModel()
+                {
+                    Id = p.Id,
+                    Title = p.Title,
+                    Content = p.Content
+                })
+                .ToListAsync();
+
+            return View(posts);
+        }
+    }
+}
