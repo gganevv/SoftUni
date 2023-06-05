10b88cc8e9b9e2e1c9c6d455d893bc3c293db5e2
Georgi <bulbrainn@gmail.com>
2023-06-06 00:27:51 +0300

    Added Seeding the database

--- 07.ASPNETFundamentals/09.WorkshopForumApp/ForumApp/Data/ForumAppDbContext.cs
+++ 07.ASPNETFundamentals/09.WorkshopForumApp/ForumApp/Data/ForumAppDbContext.cs
@@ -5,6 +5,12 @@ namespace ForumApp.Data
 {
     public class ForumAppDbContext : DbContext
     {
+        private Post FirstPost { get; set; }
+
+        private Post SecondPost { get; set; }
+
+        private Post ThirdPost { get; set; }
+
         public ForumAppDbContext(DbContextOptions<ForumAppDbContext> options) 
             : base(options)
         {
@@ -15,10 +21,37 @@ namespace ForumApp.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            SeedPosts();
+
             modelBuilder
-                .Entity<Post>();
+                .Entity<Post>()
+                .HasData(FirstPost, SecondPost, ThirdPost);
 
             base.OnModelCreating(modelBuilder);
+        }
+
+        private void SeedPosts()
+        {
+            FirstPost = new Post()
+            {
+                Id = 1,
+                Title = "My first post",
+                Content = "My first post will be about performing CRUD operations in MVC. It's so much fun!"
+            };
+
+            SecondPost = new Post()
+            {
+                Id = 2,
+                Title = "My second post",
+                Content = "This is my second post. CRUD operations in MVC are getting more and more interesting!"
+            };
+
+            ThirdPost = new Post()
+            {
+                Id = 3,
+                Title = "My third post",
+                Content = "Hello there! I'm getting better and better with the CRUD operations in MVC. Stay tuned!"
+            }
         }
     }
 }
