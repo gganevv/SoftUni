9396d75e86e8e970cb3efd6f7a95ef391fda73b2
Georgi <bulbrainn@gmail.com>
2023-06-10 22:18:27 +0300

    Added Entities

---
+++ 07.ASPNETFundamentals/11.WokshopTaskBoardApp/TaskBoardApp/Data/DataConstants.cs
@@ -0,0 +1,20 @@
+﻿namespace TaskBoardApp.Data
+{
+    public static class DataConstants
+    {
+        public static class Task
+        {
+            public const int TaskMinTitle = 5;
+            public const int TaskMaxTitle = 70;
+
+            public const int TaskMinDescription = 10;
+            public const int TaskMaxDescription = 1000;
+        }
+
+        public static class Board
+        {
+            public const int BoardMinName = 3;
+            public const int BoardMaxName = 30;
+        }
+    }
+}
---
+++ 07.ASPNETFundamentals/11.WokshopTaskBoardApp/TaskBoardApp/Data/Models/Board.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static TaskBoardApp.Data.DataConstants.Board;
+
+namespace TaskBoardApp.Data.Models
+{
+    public class Board
+    {
+        public Board()
+        {
+            this.Tasks = new HashSet<Task>();
+        }
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(BoardMaxName)]
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<Task> Tasks { get; set; } 
+    }
+}
