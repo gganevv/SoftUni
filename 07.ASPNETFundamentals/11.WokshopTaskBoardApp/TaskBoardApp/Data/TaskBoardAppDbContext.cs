0d5a91ae29d3f04235ad7b8e39a7249bdefc2e61
Georgi <bulbrainn@gmail.com>
2023-06-10 22:27:42 +0300

    Added DbContext

---
+++ 07.ASPNETFundamentals/11.WokshopTaskBoardApp/TaskBoardApp/Data/TaskBoardAppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TaskBoardApp.Data.Models;
+
+namespace TaskBoardApp.Data
+{
+    public class TaskBoardAppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public TaskBoardAppDbContext(DbContextOptions<TaskBoardAppDbContext> options)
+            : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public DbSet<Board> Boards { get; set; } = null!;
+
+        public DbSet<Models.Task> Tasks { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder
+                .Entity<Models.Task>()
+                .HasOne(t => t.Board)
+                .WithMany(b => b.Tasks)
+                .HasForeignKey(t =>t.BoardId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
