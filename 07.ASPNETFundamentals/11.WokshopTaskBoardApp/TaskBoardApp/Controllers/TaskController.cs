48d100552c9948b89d77471c64fa89933b2245a9
Georgi <bulbrainn@gmail.com>
2023-06-12 21:47:17 +0300

    Added CRUD operations pages

--- 07.ASPNETFundamentals/11.WokshopTaskBoardApp/TaskBoardApp/Controllers/TaskController.cs
+++ 07.ASPNETFundamentals/11.WokshopTaskBoardApp/TaskBoardApp/Controllers/TaskController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 using System.Security.Claims;
 using TaskBoardApp.Data;
 using TaskBoardApp.Models.Task;
@@ -55,6 +57,136 @@ namespace TaskBoardApp.Controllers
 
             var boards = Data.Boards;
 
+            return RedirectToAction("All", "Board");
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var task = await Data
+                .Tasks
+                .Where(t => t.Id == id)
+                .Select(t => new TaskDetailsViewModel()
+                {
+                    Id = t.Id,
+                    Title = t.Title,
+                    Description = t.Description,
+                    CreatedOn = t.CreatedOn.ToString("dd/MM/yyyy HH:mm"),
+                    Board = t.Board.Name,
+                    Owner = t.Owner.UserName
+                })
+                .FirstOrDefaultAsync();
+
+            if (task == null)
+            {
+                return BadRequest();
+            }
+
+            return View(task);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var task = await Data.Tasks.FindAsync(id);
+
+            if (task == null)
+            {
+                return BadRequest();
+            }
+
+            string currentUserId = GetUserId();
+            if (currentUserId != task.OwnerId)
+            {
+                return Unauthorized();
+            }
+
+            TaskFormModel taskModel = new TaskFormModel()
+            {
+                Title = task.Title,
+                Description = task.Description,
+                BoardId = task.BoardId,
+                Boards = GetBoards()
+            };
+
+            return View(taskModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, TaskFormModel taskModel)
+        {
+            var task = await Data.Tasks.FindAsync(id);
+
+            if (task == null)
+            {
+                return BadRequest();
+            }
+
+            string currentUserId = GetUserId();
+            if (currentUserId != task.OwnerId)
+            {
+                return Unauthorized();
+            }
+
+            if (!GetBoards().Any(b => b.Id == taskModel.BoardId))
+            {
+                ModelState.AddModelError(nameof(taskModel.BoardId), "Board does not exist.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                taskModel.Boards = GetBoards();
+
+                return View(taskModel);
+            }
+
+            task.Title = taskModel.Title;
+            task.Description = taskModel.Description;
+            task.BoardId = taskModel.BoardId;
+
+            await Data.SaveChangesAsync();
+            return RedirectToAction("All", "Board");
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var task = await Data.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return BadRequest();
+            }
+
+            string currentUserId = GetUserId();
+            if (currentUserId != task.OwnerId)
+            {
+                return Unauthorized();
+            }
+
+            TaskViewModel taskModel = new TaskViewModel()
+            {
+                Id = task.Id,
+                Title = task.Title,
+                Description = task.Description
+            };
+
+            return View(taskModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(TaskViewModel taskModel)
+        {
+            var task = await Data.Tasks.FindAsync(taskModel.Id);
+            if (task == null)
+            {
+                return BadRequest();
+            }
+
+            string currentUserId = GetUserId();
+            if (currentUserId != task.OwnerId)
+            {
+                return Unauthorized();
+            }
+
+            Data.Tasks.Remove(task);
+            await Data.SaveChangesAsync();
             return RedirectToAction("All", "Board");
         }
 
