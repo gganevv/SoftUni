a2b137aa50315b42bd7ad8bfafbe508948f5452e
Georgi <bulbrainn@gmail.com>
2023-07-04 22:45:43 +0300

    Modified HomeController

--- 08.ASP.NETAdvanced/04.WorkshopProjectFundamentals/HouseRentingSystem/Controllers/HomeController.cs
+++ 08.ASP.NETAdvanced/04.WorkshopProjectFundamentals/HouseRentingSystem/Controllers/HomeController.cs
@@ -6,27 +6,18 @@ namespace HouseRentingSystem.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        public IActionResult Index() 
+            => View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel 
+            { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
