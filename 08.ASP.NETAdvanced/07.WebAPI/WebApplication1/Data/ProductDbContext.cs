f21dd64f8c00693758b8042101e5b0497c39c423
Georgi <bulbrainn@gmail.com>
2023-07-15 15:31:13 +0300

    Created Database

---
+++ 08.ASP.NETAdvanced/07.WebAPI/WebApplication1/Data/ProductDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductsApi.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext
+            (DbContextOptions<ProductDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+
+        public DbSet<Product> Products { get; init; }
+    }
+}
--- 08.ASP.NETAdvanced/07.WebAPI/WebApplication1/Program.cs
+++ 08.ASP.NETAdvanced/07.WebAPI/WebApplication1/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ProductsApi.Data;
+
 namespace WebApplication1
 {
     public class Program
@@ -6,16 +9,19 @@ namespace WebApplication1
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<ProductDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
